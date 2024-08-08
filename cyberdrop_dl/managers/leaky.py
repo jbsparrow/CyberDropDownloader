8013da397e9be25f9269164cea8813bf35624423
datawhores <datawhores@users.noreply.github.com>
2024-08-08 03:38:34 -0500

    fix issue with leaky bucket class

--- cyberdrop_dl/managers/leaky.py
+++ cyberdrop_dl/managers/leaky.py
@@ -3,7 +3,7 @@ from aiolimiter import AsyncLimiter
 class LeakyBucket( AsyncLimiter):
     def __init__(self,manager):
         self.download_speed_limit = manager.config_manager.global_settings_data['Rate_Limiting_Options']['download_speed_limit']
-        super().__init__(self.download_speed_limit*1024,rate=1)
+        super().__init__(self.download_speed_limit*1024,1)
     async def acquire(self, amount: float = 1):
         if self.download_speed_limit<=0:
             return
