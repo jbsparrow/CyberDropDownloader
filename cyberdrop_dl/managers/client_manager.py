44a2fbe013cc610f40643da3a8db3a0cf6f3e2c2
Restyled.io <commits@restyled.io>
2024-09-02 17:07:24 +0000

    Restyled by black

--- cyberdrop_dl/managers/client_manager.py
+++ cyberdrop_dl/managers/client_manager.py
@@ -26,27 +26,44 @@ class ClientManager:
     def __init__(self, manager: Manager):
         self.manager = manager
         self.connection_timeout = manager.config_manager.global_settings_data[
-            'Rate_Limiting_Options']['connection_timeout']
+            "Rate_Limiting_Options"
+        ]["connection_timeout"]
         self.read_timeout = manager.config_manager.global_settings_data[
-            'Rate_Limiting_Options']['read_timeout']
+            "Rate_Limiting_Options"
+        ]["read_timeout"]
         self.rate_limit = manager.config_manager.global_settings_data[
-            'Rate_Limiting_Options']['rate_limit']
+            "Rate_Limiting_Options"
+        ]["rate_limit"]
 
         self.download_delay = manager.config_manager.global_settings_data[
-            'Rate_Limiting_Options']['download_delay']
-        self.user_agent = manager.config_manager.global_settings_data['General']['user_agent']
-        self.verify_ssl = not manager.config_manager.global_settings_data[
-            'General']['allow_insecure_connections']
+            "Rate_Limiting_Options"
+        ]["download_delay"]
+        self.user_agent = manager.config_manager.global_settings_data["General"][
+            "user_agent"
+        ]
+        self.verify_ssl = not manager.config_manager.global_settings_data["General"][
+            "allow_insecure_connections"
+        ]
         self.simultaneous_per_domain = manager.config_manager.global_settings_data[
-            'Rate_Limiting_Options']['max_simultaneous_downloads_per_domain']
+            "Rate_Limiting_Options"
+        ]["max_simultaneous_downloads_per_domain"]
 
-        self.ssl_context = ssl.create_default_context(
-            cafile=certifi.where()) if self.verify_ssl else False
+        self.ssl_context = (
+            ssl.create_default_context(cafile=certifi.where())
+            if self.verify_ssl
+            else False
+        )
         self.cookies = aiohttp.CookieJar(quote_cookie=False)
-        self.proxy = manager.config_manager.global_settings_data['General'][
-            'proxy'] if not manager.args_manager.proxy else manager.args_manager.proxy
-        self.flaresolverr = manager.config_manager.global_settings_data['General'][
-            'flaresolverr'] if not manager.args_manager.flaresolverr else manager.args_manager.flaresolverr
+        self.proxy = (
+            manager.config_manager.global_settings_data["General"]["proxy"]
+            if not manager.args_manager.proxy
+            else manager.args_manager.proxy
+        )
+        self.flaresolverr = (
+            manager.config_manager.global_settings_data["General"]["flaresolverr"]
+            if not manager.args_manager.flaresolverr
+            else manager.args_manager.flaresolverr
+        )
 
         self.domain_rate_limits = {
             "bunkrr": AsyncLimiter(5, 1),
@@ -54,16 +71,26 @@ class ClientManager:
             "coomer": AsyncLimiter(1, 1),
             "kemono": AsyncLimiter(1, 1),
             "pixeldrain": AsyncLimiter(10, 1),
-            "other": AsyncLimiter(25, 1)
+            "other": AsyncLimiter(25, 1),
         }
 
-        self.download_spacer = {'bunkr': 0.5, 'bunkrr': 0.5, 'cyberdrop': 0,
-                                'cyberfile': 0, "pixeldrain": 0, "coomer": 0.5, "kemono": 0.5}
+        self.download_spacer = {
+            "bunkr": 0.5,
+            "bunkrr": 0.5,
+            "cyberdrop": 0,
+            "cyberfile": 0,
+            "pixeldrain": 0,
+            "coomer": 0.5,
+            "kemono": 0.5,
+        }
 
         self.global_rate_limiter = AsyncLimiter(self.rate_limit, 1)
         self.session_limit = asyncio.Semaphore(50)
         self.download_session_limit = asyncio.Semaphore(
-            self.manager.config_manager.global_settings_data['Rate_Limiting_Options']['max_simultaneous_downloads'])
+            self.manager.config_manager.global_settings_data["Rate_Limiting_Options"][
+                "max_simultaneous_downloads"
+            ]
+        )
 
         self.scraper_session = ScraperClient(self)
         self.downloader_session = DownloadClient(manager, self)
@@ -85,21 +112,29 @@ class ClientManager:
 
     """~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"""
 
-    async def check_http_status(self, response: ClientResponse, download: bool = False) -> None:
+    async def check_http_status(
+        self, response: ClientResponse, download: bool = False
+    ) -> None:
         """Checks the HTTP status code and raises an exception if it's not acceptable"""
         status = response.status
         headers = response.headers
 
         if download:
-            if headers.get('Content-Length') == "322509" and headers.get('Content-Type') == "video/mp4":
+            if (
+                headers.get("Content-Length") == "322509"
+                and headers.get("Content-Type") == "video/mp4"
+            ):
                 raise DownloadFailure(
-                    status="Bunkr Maintenance", message="Bunkr under maintenance")
-            if headers.get('ETag') == '"d835884373f4d6c8f24742ceabe74946"':
+                    status="Bunkr Maintenance", message="Bunkr under maintenance"
+                )
+            if headers.get("ETag") == '"d835884373f4d6c8f24742ceabe74946"':
                 raise DownloadFailure(
-                    status=HTTPStatus.NOT_FOUND, message="Imgur image has been removed")
-            if headers.get('ETag') == '"65b7753c-528a"':
+                    status=HTTPStatus.NOT_FOUND, message="Imgur image has been removed"
+                )
+            if headers.get("ETag") == '"65b7753c-528a"':
                 raise DownloadFailure(
-                    status=HTTPStatus.NOT_FOUND, message="SC Scrape Image")
+                    status=HTTPStatus.NOT_FOUND, message="SC Scrape Image"
+                )
 
         if HTTPStatus.OK <= status < HTTPStatus.BAD_REQUEST:
             return
@@ -116,8 +151,7 @@ class ClientManager:
             try:
                 JSON_Resp = await response.json()
                 if "status" in JSON_Resp:
-                    raise ScrapeFailure(
-                        JSON_Resp['status'], JSON_Resp['data']['error'])
+                    raise ScrapeFailure(JSON_Resp["status"], JSON_Resp["data"]["error"])
             except ContentTypeError:
                 pass
 
@@ -128,15 +162,17 @@ class ClientManager:
 
         response_text = await response.text()
         if "<title>DDoS-Guard</title>" in response_text:
-            raise DDOSGuardFailure(status="DDOS-Guard",
-                                   message="DDoS-Guard detected")
+            raise DDOSGuardFailure(status="DDOS-Guard", message="DDoS-Guard detected")
 
-        if not headers.get('Content-Type'):
+        if not headers.get("Content-Type"):
             raise DownloadFailure(
-                status=CustomHTTPStatus.IM_A_TEAPOT, message="No content-type in response header")
+                status=CustomHTTPStatus.IM_A_TEAPOT,
+                message="No content-type in response header",
+            )
 
         raise DownloadFailure(
-            status=status, message=f"HTTP status code {status}: {phrase}")
+            status=status, message=f"HTTP status code {status}: {phrase}"
+        )
 
     async def check_bucket(self, size):
         await self._leaky_bucket.acquire(size)
