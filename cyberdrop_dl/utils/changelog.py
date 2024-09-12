5ad006aaee6c1e3a3c4255f81046e40f9872407b
datawhores <datawhores@users.noreply.github.com>
2024-09-12 15:13:09 -0500

    increment progres

--- cyberdrop_dl/utils/changelog.py
+++ cyberdrop_dl/utils/changelog.py
@@ -14,4 +14,26 @@ D\bDE\bES\bSC\bCR\bRI\bIP\bPT\bTI\bIO\bON\bN
 \t\t- Fix improper path handling of the `sort_cdl_only` config option.
 
 \tFor more details, visit the wiki: https://script-ware.gitbook.io
+
+
+C\bCH\bHA\bAN\bNG\bGE\bEL\bLO\bOG\bG
+\tVersion 5.5.0
+
+D\bDE\bES\bSC\bCR\bRI\bIP\bPT\bTI\bIO\bON\bN
+\tThis update introduces the following changes:
+\t\t1. Finalizes new sorting feature
+\t\t2. add scanning directory for sorting
+\t\t3. adds progress bar for sorting
+
+
+
+\tDetails:
+\t\t- skips need to scan db if sort_cdl_only is false
+\t\t- progress bar for current progress of sorting files,incremented for each folder
+\t\t- allow for setting a different folder to scan that is independent of the download folder
+\tFor more details, visit the wiki: https://script-ware.gitbook.io
+
+
+
+
 """
