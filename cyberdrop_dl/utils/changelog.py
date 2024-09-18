8a9d96ae29ec53e7c756bbfeaa4ab7a079450de9
Jacob <admin@script-ware.net>
2024-09-18 17:02:07 -0400

    Update changelog

--- cyberdrop_dl/utils/changelog.py
+++ cyberdrop_dl/utils/changelog.py
@@ -3,6 +3,22 @@
 ------------------------------------------------------------
 
 C\bCH\bHA\bAN\bNG\bGE\bEL\bLO\bOG\bG
+\tVersion 5.6.12
+
+
+D\bDE\bES\bSC\bCR\bRI\bIP\bPT\bTI\bIO\bON\bN
+\tThis update introduces the following changes:
+\t\t1. Per-config authentication settings
+
+\tDetails:
+\t\t- If an authentication.yaml file is placed within your config directory, it will be used instead of the global authentication values.
+
+
+\tFor more details, visit the wiki: https://script-ware.gitbook.io
+
+------------------------------------------------------------
+
+C\bCH\bHA\bAN\bNG\bGE\bEL\bLO\bOG\bG
 \tVersion 5.6.11
 
 
