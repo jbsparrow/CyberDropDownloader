4037d5f2b2fa77f7ce4302aca05931a510e9e765
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2024-10-04 23:52:44 -0500

    add: Toonily MaxChildren support

--- cyberdrop_dl/scraper/crawlers/toonily_crawler.py
+++ cyberdrop_dl/scraper/crawlers/toonily_crawler.py
@@ -8,8 +8,9 @@ from aiolimiter import AsyncLimiter
 from yarl import URL
 
 from cyberdrop_dl.scraper.crawler import Crawler
-from cyberdrop_dl.utils.dataclasses.url_objects import ScrapeItem
+from cyberdrop_dl.utils.dataclasses.url_objects import ScrapeItem, FILE_HOST_ALBUM, FILE_HOST_PROFILE
 from cyberdrop_dl.utils.utilities import error_handling_wrapper, get_filename_and_ext
+from cyberdrop_dl.clients.errors import ScrapeItemMaxChildrenReached
 
 if TYPE_CHECKING:
     from cyberdrop_dl.managers.manager import Manager
@@ -38,9 +39,17 @@ class ToonilyCrawler(Crawler):
 
     @error_handling_wrapper
     async def series(self, scrape_item: ScrapeItem) -> None:
-        """Scrapes an album"""
+        """Scrapes a series"""
         async with self.request_limiter:
             soup = await self.client.get_BS4(self.domain, scrape_item.url)
+
+        self.type = FILE_HOST_PROFILE
+        scrape_item.children = scrape_item.children_limit = 0
+
+        try:
+            scrape_item.children_limit = self.manager.config_manager.settings_data['Download_Options']['maximum_number_of_children'][scrape_item.type]
+        except (IndexError, TypeError):
+            pass
 
         chapters = soup.select("li[class*=wp-manga-chapter] a")
         for chapter in chapters:
@@ -53,12 +62,23 @@ class ToonilyCrawler(Crawler):
                 chapter_path = URL(chapter_path)
             new_scrape_item = await self.create_scrape_item(scrape_item, chapter_path, "", True , add_parent = scrape_item.url)
             self.manager.task_group.create_task(self.run(new_scrape_item))
+            if scrape_item.children_limit:
+                if scrape_item.children >= scrape_item.children_limit:
+                    raise ScrapeItemMaxChildrenReached(scrape_item)
 
     @error_handling_wrapper
     async def chapter(self, scrape_item: ScrapeItem) -> None:
         """Scrapes an image"""
         async with self.request_limiter:
             soup = await self.client.get_BS4(self.domain, scrape_item.url)
+
+        scrape_item.type = FILE_HOST_ALBUM
+        scrape_item.children = scrape_item.children_limit = 0
+        
+        try:
+            scrape_item.children_limit = self.manager.config_manager.settings_data['Download_Options']['maximum_number_of_children'][scrape_item.type]
+        except (IndexError, TypeError):
+            pass
 
         title_parts = soup.select_one("title").get_text().split(" - ")
         series_name = title_parts[0]
@@ -87,6 +107,9 @@ class ToonilyCrawler(Crawler):
 
             filename, ext = await get_filename_and_ext(link.name)
             await self.handle_file(link, scrape_item, filename, ext)
+            if scrape_item.children_limit:
+                if scrape_item.children >= scrape_item.children_limit:
+                    raise ScrapeItemMaxChildrenReached(scrape_item)
 
     @error_handling_wrapper
     async def handle_direct_link(self, scrape_item: ScrapeItem) -> None:
