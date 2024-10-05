973ed390e0f506df9e2d675a281f94ee8cfdbb21
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2024-10-04 23:18:51 -0500

    add: Redgifs MaxChildren support

--- cyberdrop_dl/scraper/crawlers/redgifs_crawler.py
+++ cyberdrop_dl/scraper/crawlers/redgifs_crawler.py
@@ -6,8 +6,9 @@ from aiolimiter import AsyncLimiter
 from yarl import URL
 
 from cyberdrop_dl.scraper.crawler import Crawler
-from cyberdrop_dl.utils.dataclasses.url_objects import ScrapeItem
+from cyberdrop_dl.utils.dataclasses.url_objects import ScrapeItem, FILE_HOST_PROFILE
 from cyberdrop_dl.utils.utilities import error_handling_wrapper, get_filename_and_ext
+from cyberdrop_dl.clients.errors import ScrapeItemMaxChildrenReached
 
 if TYPE_CHECKING:
     from cyberdrop_dl.managers.manager import Manager
@@ -45,6 +46,14 @@ class RedGifsCrawler(Crawler):
 
         page = 1
         total_pages = 1
+
+        self.type = FILE_HOST_PROFILE
+        scrape_item.children = scrape_item.children_limit = 0
+
+        try:
+            scrape_item.children_limit = self.manager.config_manager.settings_data['Download_Options']['maximum_number_of_children'][scrape_item.type]
+        except (IndexError, TypeError):
+            pass
         while page <= total_pages:
             async with self.request_limiter:
                 JSON_Resp = await self.client.get_json(self.domain,
@@ -65,6 +74,9 @@ class RedGifsCrawler(Crawler):
                 filename, ext = await get_filename_and_ext(link.name)
                 new_scrape_item = await self.create_scrape_item(scrape_item, link, title, True, date,add_parent = scrape_item.url)
                 await self.handle_file(link, new_scrape_item, filename, ext)
+                if scrape_item.children_limit:
+                    if scrape_item.children >= scrape_item.children_limit:
+                        raise ScrapeItemMaxChildrenReached(scrape_item)
             page += 1
 
     @error_handling_wrapper
