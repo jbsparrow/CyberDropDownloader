f3c38979f8a872f42b53675d99f2cf9d66743c0c
Jacob <admin@script-ware.net>
2024-10-10 12:23:14 -0400

    Update table_definitions.py

--- cyberdrop_dl/utils/database/table_definitions.py
+++ cyberdrop_dl/utils/database/table_definitions.py
@@ -1,27 +1,27 @@
 create_history = """CREATE TABLE IF NOT EXISTS media (domain TEXT,
-                                               url_path TEXT,
-                                               referer TEXT,
-                                               album_id TEXT,
-                                               download_path TEXT,
-                                               download_filename TEXT,
-                                               original_filename TEXT,
-                                               completed INTEGER NOT NULL,
-                                               created_at TIMESTAMP,
-                                               completed_at TIMESTAMP,
-                                               PRIMARY KEY (domain, url_path, original_filename)
-                                               );"""
+                                              url_path TEXT,
+                                              referer TEXT,
+                                              album_id TEXT,
+                                              download_path TEXT,
+                                              download_filename TEXT,
+                                              original_filename TEXT,
+                                              completed INTEGER NOT NULL,
+                                              created_at TIMESTAMP,
+                                              completed_at TIMESTAMP,
+                                              PRIMARY KEY (domain, url_path, original_filename)
+                                              );"""
 
 create_fixed_history = """CREATE TABLE IF NOT EXISTS media_copy (domain TEXT,
-                                               url_path TEXT,
-                                               referer TEXT,
-                                               album_id TEXT,
-                                               download_path TEXT,
-                                               download_filename TEXT,
-                                               original_filename TEXT,
-                                               file_size INT,
-                                               completed INTEGER NOT NULL,
-                                               PRIMARY KEY (domain, url_path, original_filename)
-                                               );"""
+                                              url_path TEXT,
+                                              referer TEXT,
+                                              album_id TEXT,
+                                              download_path TEXT,
+                                              download_filename TEXT,
+                                              original_filename TEXT,
+                                              file_size INT,
+                                              completed INTEGER NOT NULL,
+                                              PRIMARY KEY (domain, url_path, original_filename)
+                                              );"""
 
 create_temp = """CREATE TABLE IF NOT EXISTS temp (downloaded_filename TEXT);"""
 
