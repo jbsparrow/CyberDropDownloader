a0d2ad40db973018c1e57a461e11e248186cddd6
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2024-10-13 21:24:51 -0500

    refactor: use general cookie (celebforum)

--- cyberdrop_dl/scraper/crawlers/celebforum_crawler.py
+++ cyberdrop_dl/scraper/crawlers/celebforum_crawler.py
@@ -19,6 +19,7 @@ class CelebForumCrawler(Crawler):
         super().__init__(manager, "celebforum", "CelebForum")
         self.primary_base_domain = URL("https://celebforum.to")
         self.logged_in = False
+        self.login_attempts = 0
         self.request_limiter = AsyncLimiter(10, 1)
 
         self.title_selector = "h1[class=p-title-value]"
@@ -52,13 +53,18 @@ class CelebForumCrawler(Crawler):
         """Determines where to send the scrape item based on the url"""
         task_id = await self.scraping_progress.add_task(scrape_item.url)
 
-        if not self.logged_in:
+        if not self.logged_in and self.login_attempts == 0:
             login_url = self.primary_base_domain / "login"
-            session_cookie = self.manager.config_manager.authentication_data['Forums']['celebforum_xf_user_cookie']
+            host_cookies = self.client.client_manager.cookies._cookies.get((self.primary_base_domain.host,""), {})
+            session_cookie = host_cookies.get('xf_user').value if 'xf_user' in host_cookies else None
+            if not session_cookie:
+                session_cookie = self.manager.config_manager.authentication_data['Forums']['celebforum_xf_user_cookie']
+
             username = self.manager.config_manager.authentication_data['Forums']['celebforum_username']
             password = self.manager.config_manager.authentication_data['Forums']['celebforum_password']
             wait_time = 5
 
+            self.login_attempts += 1
             await self.forum_login(login_url, session_cookie, username, password, wait_time)
 
         if self.logged_in:
