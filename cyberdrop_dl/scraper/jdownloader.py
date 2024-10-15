2a78de2d7d949cc019c38a7cffb0d4ac60c08b27
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2024-10-15 11:02:53 -0500

    feat: use custom download_dir and autostart for Jdownloader

--- cyberdrop_dl/scraper/jdownloader.py
+++ cyberdrop_dl/scraper/jdownloader.py
@@ -5,6 +5,7 @@ from dataclasses import field
 from typing import TYPE_CHECKING
 
 from myjdapi import myjdapi
+from pathlib import Path
 
 from cyberdrop_dl.clients.errors import JDownloaderFailure
 from cyberdrop_dl.managers.manager import Manager
@@ -22,28 +23,33 @@ class JDownloader:
         self.jdownloader_device = manager.config_manager.authentication_data['JDownloader']['jdownloader_device']
         self.jdownloader_username = manager.config_manager.authentication_data['JDownloader']['jdownloader_username']
         self.jdownloader_password = manager.config_manager.authentication_data['JDownloader']['jdownloader_password']
-        self.download_directory = manager.path_manager.download_dir
+        self.jdownloader_download_dir = manager.config_manager.settings_data['Runtime_Options']['jdownloader_download_dir']
+        self.jdownloader_autostart = manager.config_manager.settings_data['Runtime_Options']['jdownloader_autostart']
+        if not self.jdownloader_download_dir:
+            self.jdownloader_download_dir = manager.path_manager.download_dir
+        self.jdownloader_download_dir = Path(self.jdownloader_download_dir)
         self.jdownloader_agent = field(init=False)
 
     async def jdownloader_setup(self) -> None:
         """Setup function for JDownloader"""
         try:
-            if not self.jdownloader_username or not self.jdownloader_password or not self.jdownloader_device:
+            if not all (self.jdownloader_username, self.jdownloader_password, self.jdownloader_device):
                 raise JDownloaderFailure("JDownloader credentials were not provided.")
             jd = myjdapi.Myjdapi()
             jd.set_app_key("CYBERDROP-DL")
             jd.connect(self.jdownloader_username, self.jdownloader_password)
             self.jdownloader_agent = jd.get_device(self.jdownloader_device)
+            return
+
         except (myjdapi.MYJDApiException, JDownloaderFailure) as e:
-            await log("Failed JDownloader setup", 40)
-            await log(e.message, 40)
-            self.enabled = False
-            time.sleep(20)
+            msg = e.message
+
         except myjdapi.MYJDDeviceNotFoundException as e:
-            await log("Failed JDownloader setup", 40)
-            await log(str(e), 40)
-            self.enabled = False
-            time.sleep(20)
+            msg = str(e)
+
+        await log(f"Failed JDownloader setup\n{msg}", 40)
+        self.enabled = False
+        time.sleep(20)
 
     async def direct_unsupported_to_jdownloader(self, url: URL, title: str) -> None:
         """Sends links to JDownloader"""
@@ -51,13 +57,12 @@ class JDownloader:
             assert url.host is not None
             assert self.jdownloader_agent is not None
             self.jdownloader_agent.linkgrabber.add_links([{
-                "autostart": False,
+                "autostart": self.jdownloader_autostart ,
                 "links": str(url),
                 "packageName": title if title else "Cyberdrop-DL",
-                "destinationFolder": str(self.download_directory.absolute()),
+                "destinationFolder": str(self.jdownloader_download_dir.resolve()),
                 "overwritePackagizerRules": True
             }])
 
         except (JDownloaderFailure, AssertionError) as e:
-            await log(f"Failed to send {url} to JDownloader", 40)
-            await log(e.message, 40)
+            await log(f"Failed to send {url} to JDownloader\n{e.message}", 40)
