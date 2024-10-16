7447930122045f2df917f823916710b0055a9ee2
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2024-10-16 17:35:45 -0500

    add playlist support
    - handle private videos
    - change custom_filename
    - format to reduce name collitions.

--- cyberdrop_dl/scraper/crawlers/tokyomotion_crawler.py
+++ cyberdrop_dl/scraper/crawlers/tokyomotion_crawler.py
@@ -20,8 +20,10 @@ class TokioMotionCrawler(Crawler):
         super().__init__(manager, "tokyomotion", "Tokyomotion")
         self.primary_base_domain = URL("https://www.tokyomotion.net")
         self.request_limiter = AsyncLimiter(10, 1)
-        self.page_selector = 'a.prevnext'
+        self.next_page_selector = 'a.prevnext'
         self.title_selector = 'meta[property="og:title"]'
+        self.next_page_attribute = "href"
+        self.video_selector = 'a[href^="/video/"]'
 
     """~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"""
 
@@ -52,7 +54,7 @@ class TokioMotionCrawler(Crawler):
 
     @error_handling_wrapper
     async def video(self, scrape_item: ScrapeItem) -> None:
-        """Scrapes an video"""
+        """Scrapes a video"""
         if await self.check_complete_from_referer(scrape_item):
             return
         async with self.request_limiter:
@@ -62,7 +64,9 @@ class TokioMotionCrawler(Crawler):
             srcHD = soup.select_one('source[title="HD"]')
             src = (srcHD or srcSD).get('src')
             link = URL(src)
-        except AttributeError as e:
+        except AttributeError:
+            if "This is a private video" in soup.text:
+                raise ScrapeFailure('Private Video', f"Private video: {scrape_item.url}")
             raise ScrapeFailure(404, f"Could not find video source for {scrape_item.url}")
         
         title = soup.select_one('title').text.rsplit(" - TOKYO Motion")[0].strip()
@@ -70,7 +74,7 @@ class TokioMotionCrawler(Crawler):
         # NOTE: hardcoding the extension to prevent quering the final server URL
         # final server URL is always diferent so it can not be saved to db.
         filename, ext = scrape_item.url.parts[2], '.mp4'
-        custom_file_name, _ = await get_filename_and_ext(title + ext)
+        custom_file_name, _ = await get_filename_and_ext(f"{title} [{filename}]{ext}")
         await self.handle_file(link, scrape_item, filename, ext, custom_file_name)
 
     @error_handling_wrapper
@@ -95,5 +99,37 @@ class TokioMotionCrawler(Crawler):
     
     @error_handling_wrapper
     async def playlist(self, scrape_item: ScrapeItem) -> None:
-        """Scrapes an album"""
-        raise NotImplementedError
+        """Scrapes a video playlist"""
+        title = 'favorites' if 'favorite' in scrape_item.url.parts else "videos" 
+        user = scrape_item.url.parts[2]
+        page_url = scrape_item.url
+        if user not in scrape_item.parent_title.split('/'):
+            await scrape_item.add_to_parent_title(scrape_item.url.parts[2])
+
+        while True:
+            async with self.request_limiter:
+                soup: BeautifulSoup = await self.client.get_BS4(self.domain, page_url)
+
+            videos = soup.select(self.video_selector)
+            
+            for video in videos:
+                link = video.get('href')
+                if not link:
+                    continue
+
+                if link.startswith("/"):
+                    link = self.primary_base_domain / link[1:]
+
+                link = URL(link)
+                new_scrape_item = await self.create_scrape_item(scrape_item, link, new_title_part=title, add_parent = scrape_item.url)
+                await self.video(new_scrape_item)
+
+            next_page = soup.select_one(self.next_page_selector)
+            if next_page :
+                page_url = next_page.get(self.next_page_attribute)
+                if page_url:
+                    if page_url.startswith("/"):
+                        page_url = self.primary_base_domain / page_url[1:]
+                    page_url = URL(page_url)
+                    continue
+            break
