b2e333981cce3a658dabbc52666c181fc3f72255
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2024-10-18 20:56:30 -0500

    fix custom url path saved to db

--- cyberdrop_dl/scraper/crawlers/realdebrid_crawler.py
+++ cyberdrop_dl/scraper/crawlers/realdebrid_crawler.py
@@ -6,7 +6,7 @@ from yarl import URL
 
 from cyberdrop_dl.scraper.crawler import Crawler
 from cyberdrop_dl.utils.dataclasses.url_objects import ScrapeItem
-from cyberdrop_dl.utils.utilities import get_filename_and_ext, error_handling_wrapper, log
+from cyberdrop_dl.utils.utilities import get_filename_and_ext, error_handling_wrapper, log, log_debug
 from cyberdrop_dl.managers.realdebrid_manager import RATE_LIMIT
 
 if TYPE_CHECKING:
@@ -33,8 +33,8 @@ class RealDebridCrawler(Crawler):
     @error_handling_wrapper
     async def folder(self, scrape_item: ScrapeItem) -> None:
         """Scrapes a folder"""
-        await log ('scraping folder with RealDebrid',10)
         original_url = scrape_item.url
+        await log (f'scraping folder with RealDebrid: {original_url}',10)
         folder_id = await self.manager.real_debrid_manager.guess_folder(original_url)
         scrape_item.album_id = folder_id
         scrape_item.part_of_album = True
@@ -53,24 +53,31 @@ class RealDebridCrawler(Crawler):
     @error_handling_wrapper
     async def file(self, scrape_item: ScrapeItem) -> None:
         """Scrapes a file"""
-        await log ('scraping file with RealDebrid',10)
         original_url = scrape_item.url
         password = original_url.query.get('password','')
         async with self.request_limiter:
             debrid_link = await self.manager.real_debrid_manager.unrestrict_link(original_url, password)
-
-        await log (f"{debrid_link=}",10)
         
         if await self.check_complete_from_referer(scrape_item):
             return
+        
+        await log (f'scraping file with RealDebrid: {original_url}',10)
+        await log_debug(f'original url: {original_url}  -  debrid_url: {debrid_link}',10)
 
         if not scrape_item.part_of_album:
             title = await self.create_title(f"files [{original_url.host.lower()}]", None, None)
             await scrape_item.add_to_parent_title(title)
 
+        # Some hosts use query params or fragment as id or password (ex: mega.nz)
+        # This save the query and fragment as parts of the URL path since DB lookups only use url_path 
         link = self.primary_base_domain / original_url.host.lower() / original_url.path[1:] / debrid_link.name
-        link = scrape_item.url.with_query(original_url.query).with_fragment(original_url.fragment.split('/')[0])
-        await log (f"{link=}",10)
+        if original_url.query:
+            query_params_list = [item for pair in original_url.query.items() for item in pair]
+            link = link / 'query' / "/".join(query_params_list)
+
+        if original_url.fragment:
+            link = link / 'frag'/ original_url.fragment
+
         filename, ext = await get_filename_and_ext(debrid_link.name)
         await self.handle_file(link, scrape_item, filename, ext, debrid_link)
 
