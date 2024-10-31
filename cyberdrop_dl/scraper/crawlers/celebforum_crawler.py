a43ba5549203569e25b32b4d29ecbd8b7c6e90fa
Jacob <admin@script-ware.net>
2024-10-31 15:46:48 -0400

    Update celebforum_crawler.py

--- cyberdrop_dl/scraper/crawlers/celebforum_crawler.py
+++ cyberdrop_dl/scraper/crawlers/celebforum_crawler.py
@@ -85,7 +85,7 @@ class CelebForumCrawler(Crawler):
         current_post_number = 0
         while True:
             async with self.request_limiter:
-                soup: BeautifulSoup = await self.client.get_BS4(self.domain, thread_url, origin = scrape_item)
+                soup: BeautifulSoup = await self.client.get_BS4(self.domain, thread_url, origin=scrape_item)
 
             title_block = soup.select_one(self.title_selector)
             for elem in title_block.find_all(self.title_trash_selector):
@@ -103,7 +103,9 @@ class CelebForumCrawler(Crawler):
 
                 if scrape_post:
                     date = int(post.select_one(self.post_date_selector).get(self.post_date_attribute))
-                    new_scrape_item = await self.create_scrape_item(scrape_item, thread_url, title, False, None, date, add_parent= thread_url.joinpath(f"post-{current_post_number}"))
+                    new_scrape_item = await self.create_scrape_item(scrape_item, thread_url, title, False, None, date,
+                                                                    add_parent=thread_url.joinpath(
+                                                                        f"post-{current_post_number}"))
 
                     for elem in post.find_all(self.quotes_selector):
                         elem.decompose()
