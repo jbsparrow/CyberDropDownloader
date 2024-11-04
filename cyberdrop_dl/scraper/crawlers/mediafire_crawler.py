839407e8c0dbb223c61185c618245a5b954b5544
Jacob <admin@script-ware.net>
2024-11-04 15:54:28 -0500

    Update mediafire_crawler.py

--- cyberdrop_dl/scraper/crawlers/mediafire_crawler.py
+++ cyberdrop_dl/scraper/crawlers/mediafire_crawler.py
@@ -91,7 +91,8 @@ class MediaFireCrawler(Crawler):
 
     """~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"""
 
-    async def parse_datetime(self, date: str) -> int:
+    @staticmethod
+    async def parse_datetime(date: str) -> int:
         """Parses a datetime string into a unix timestamp"""
         date = datetime.datetime.strptime(date, "%Y-%m-%d %H:%M:%S")
         return calendar.timegm(date.timetuple())
