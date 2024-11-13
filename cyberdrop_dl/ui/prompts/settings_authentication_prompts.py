1c194c0123ed48ffe888e8726edc3c75241f425b
datawhores <datawhores@users.noreply.github.com>
2024-11-13 12:29:56 -0600

    remove s

--- cyberdrop_dl/ui/prompts/settings_authentication_prompts.py
+++ cyberdrop_dl/ui/prompts/settings_authentication_prompts.py
@@ -161,7 +161,7 @@ def edit_forum_authentication_values_prompt(manager: Manager) -> None:
                 vi_mode=manager.vi_mode,
             ).execute()
             celebforum_password = inquirer.text(
-                message="Enter your CelebForum Passwords:",
+                message="Enter your CelebForum Password:",
                 default=manager.config_manager.authentication_data["Forums"]["celebforum_password"],
                 vi_mode=manager.vi_mode,
             ).execute()
