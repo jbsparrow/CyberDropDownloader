cd72553767e8353df0d5b73d37f04e7204d71479
datawhores <datawhores@users.noreply.github.com>
2024-11-13 13:55:32 -0600

    use supported hosts

--- cyberdrop_dl/utils/args/browser_cookie_extraction.py
+++ cyberdrop_dl/utils/args/browser_cookie_extraction.py
@@ -85,7 +85,7 @@ def get_cookies_from_browser(manager: Manager, browsers: str = None) -> None:
     if isinstance(browsers,str):
         browsers=re.split(r'[ ,]+', browsers)
     all_sites=set(SupportedDomains.supported_forums)
-    user_sites= manager.config_manager.settings_data["Browser_Cookies"]["sites"] or SupportedDomains.supported_forums
+    user_sites= manager.config_manager.settings_data["Browser_Cookies"]["sites"] or SupportedDomains.supported_hosts
     if isinstance(user_sites,str):
         user_sites=re.split(r'[ ,]+', user_sites)
     for domain in user_sites:
