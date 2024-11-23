6dc3c8c1a06e1d205ab4290beaba0ea338d339e4
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2024-11-22 22:07:28 -0500

    refactor: add yaml custom representers

--- cyberdrop_dl/managers/yaml_manager.py
+++ cyberdrop_dl/managers/yaml_manager.py
@@ -1,14 +1,25 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+from enum import IntEnum, StrEnum
+from pathlib import Path, PurePath
 
 import yaml
 from pydantic import BaseModel
+from yarl import URL
 
 from cyberdrop_dl.clients.errors import InvalidYamlError
 
-if TYPE_CHECKING:
-    from pathlib import Path
+# from cyberdrop_dl.utils.constants import BROWSERS
+
+
+def save_as_str(dumper: yaml.Dumper, value):
+    return dumper.represent_str(str(value))
+
+
+yaml.add_multi_representer(PurePath, save_as_str)
+yaml.add_multi_representer(StrEnum, save_as_str)
+yaml.add_multi_representer(IntEnum, int)
+yaml.add_representer(URL, save_as_str)
 
 
 class YamlManager:
@@ -30,7 +41,7 @@ class YamlManager:
                 file.touch()
         try:
             with file.open(encoding="utf8") as yaml_file:
-                yaml_values = yaml.load(yaml_file.read(), Loader=yaml.FullLoader)
+                yaml_values = yaml.safe_load(yaml_file.read())
                 return yaml_values if yaml_values else {}
         except yaml.constructor.ConstructorError as e:
             raise InvalidYamlError(file, e) from None
