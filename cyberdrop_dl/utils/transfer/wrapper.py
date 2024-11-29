b3dbc10ffc41033e3d83ec2d15d28e4c0e72e6ce
datawhores <datawhores@users.noreply.github.com>
2024-11-29 17:13:48 -0600

    fix: increment i

--- cyberdrop_dl/utils/transfer/wrapper.py
+++ cyberdrop_dl/utils/transfer/wrapper.py
@@ -8,7 +8,9 @@ def db_transfer_context(db_file):
     while True:
         new_file=Path(f"{db_file}i")
         if new_file.exists():
+            i=i+1
             continue
+
         copy2(db_file,new_file)
         break
     conn = sqlite3.connect(db_file)
