f68ac54580e5472d69d15a2d2c2b2e98126cfce4
datawhores <datawhores@users.noreply.github.com>
2024-12-01 17:37:31 -0600

    fix: log backup syntax

--- cyberdrop_dl/utils/transfer/backup.py
+++ cyberdrop_dl/utils/transfer/backup.py
@@ -4,5 +4,5 @@ from shutil import copy2
 
 
 def db_backup(db_file):
-    new_file = Path(db_file.parent, f"cyberdrop_v5_{datetime.now().strftime("%Y%m%d_%H%M%S")}.bak.db")
+    new_file = Path(db_file.parent, f"cyberdrop_v5_{datetime.now().strftime('%Y%m%d_%H%M%S')}.bak.db")
     copy2(db_file, new_file)
