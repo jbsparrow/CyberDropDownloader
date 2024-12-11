199743e3b9c11d4e5a213d9237bb68a3d83cc323
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2024-12-11 15:11:25 -0500

    refactor: use custom `handle_link_confirmation` for F95Zone

--- cyberdrop_dl/scraper/crawlers/f95zone_crawler.py
+++ cyberdrop_dl/scraper/crawlers/f95zone_crawler.py
@@ -8,6 +8,7 @@ from .xenforo_crawler import PostSelectors, Selector, XenforoCrawler, XenforoSel
 
 if TYPE_CHECKING:
     from cyberdrop_dl.managers.manager import Manager
+    from cyberdrop_dl.utils.data_enums_classes.url_objects import ScrapeItem
 
 
 class F95ZoneCrawler(XenforoCrawler):
@@ -20,3 +21,16 @@ class F95ZoneCrawler(XenforoCrawler):
 
     def __init__(self, manager: Manager) -> None:
         super().__init__(manager, "f95zone", "F95Zone")
+
+    async def handle_link_confirmation(self, link: URL, *, origin: ScrapeItem | None = None) -> URL | None:
+        """Override to handle protected link confirmation."""
+        async with self.request_limiter:
+            await self.client.get_soup(self.domain, link)
+        async with self.request_limiter:
+            JSON_Resp = await self.client.post_data(
+                self.domain, link, data={"xhr": "1", "download": "1"}, origin=origin
+            )
+
+        if JSON_Resp["status"] == "ok":
+            return URL(JSON_Resp["msg"])
+        return
