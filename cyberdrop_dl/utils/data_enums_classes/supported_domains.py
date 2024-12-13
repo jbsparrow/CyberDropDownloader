90cccc7c542bddf42f60f2af047437a4b13f361b
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2024-12-13 16:14:11 -0500

    refactor: update supported_domains logic

--- cyberdrop_dl/utils/data_enums_classes/supported_domains.py
+++ cyberdrop_dl/utils/data_enums_classes/supported_domains.py
@@ -4,7 +4,7 @@ from typing import TYPE_CHECKING
 
 from cyberdrop_dl import __version__ as current_version
 from cyberdrop_dl.scraper import ALL_CRAWLERS, DEBUG_CRAWLERS
-from cyberdrop_dl.scraper.crawlers.xxxbunker_crawler import XXXBunkerCrawler
+from cyberdrop_dl.scraper.crawlers.xenforo_crawler import XenforoCrawler
 from cyberdrop_dl.utils.constants import PRERELEASE_TAGS
 
 if TYPE_CHECKING:
@@ -15,7 +15,7 @@ is_testing = next((tag for tag in PRERELEASE_TAGS if tag in current_version), Fa
 if not is_testing:
     crawlers -= DEBUG_CRAWLERS
 
-forum_crawlers = [crawler for crawler in crawlers if issubclass(crawler, XXXBunkerCrawler)]
+forum_crawlers = [crawler for crawler in crawlers if issubclass(crawler, XenforoCrawler)]
 website_crawlers = crawlers - forum_crawlers
 
 
@@ -26,9 +26,8 @@ def get_supported_sites_from(crawlers: set[type[Crawler]]) -> dict[str, str]:
             support_sites_dict[crawler.domain] = crawler.primary_base_domain.host
             continue
 
-        for domains in crawler.SUPPORTED_SITES.values():
-            for domain in domains:
-                support_sites_dict[domain] = domain
+        for site, domains in crawler.SUPPORTED_SITES.items():
+            support_sites_dict[site] = domains[0]
 
 
 SUPPORTED_FORUMS = get_supported_sites_from(forum_crawlers)
