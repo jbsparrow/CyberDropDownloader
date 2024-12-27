5d227ee16dbeb8143bab288892f7bbca62026b88
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2024-12-27 17:52:53 -0500

    chroe: fix ruff issues from rebase

--- cyberdrop_dl/main.py
+++ cyberdrop_dl/main.py
@@ -18,7 +18,6 @@ from cyberdrop_dl.clients.errors import InvalidYamlError
 from cyberdrop_dl.managers.manager import Manager
 from cyberdrop_dl.scraper.scraper import ScrapeMapper
 from cyberdrop_dl.ui.program_ui import ProgramUI
-from cyberdrop_dl.ui.prompts.user_prompts import get_cookies_from_browsers
 from cyberdrop_dl.utils.logger import RedactedConsole, log, log_spacer, log_with_color, print_to_console
 from cyberdrop_dl.utils.sorting import Sorter
 from cyberdrop_dl.utils.utilities import (
