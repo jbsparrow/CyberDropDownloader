ffaa776cf9fb7cd411f7fef66bdcd3e5bf9755f4
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2024-12-29 00:33:40 -0500

    test: split test cases into individual functions

--- tests/test_apprise.py
+++ tests/test_apprise.py
@@ -14,6 +14,20 @@ from tests.fake_classes.managers import FakeCacheManager
 TEST_FILES_PATH = Path("tests/test_files/apprise")
 FAKE_MANAGER = Manager()
 FAKE_MANAGER.cache_manager = FakeCacheManager(FAKE_MANAGER)
+FAKE_MANAGER.config_manager = ConfigManager(FAKE_MANAGER)
+
+URL_FAIL = "mailto://test_user:test_email@gmail.com"
+URL_SUCCESS = os.environ.get("APPRISE_TEST_EMAIL_URL", "")
+URL_SUCCESS_ATTACH_LOGS = f"attach_logs={URL_SUCCESS}"
+
+
+@dataclass
+class AppriseTestCase:
+    urls: list[str]
+    result: NotificationResult
+    include: list[str] = field(default_factory=list)
+    exclude: list[str] = field(default_factory=list)
+    file: Path | None = None
 
 
 def test_get_apprise_urls():
@@ -59,56 +73,71 @@ def test_get_apprise_urls():
         assert got == expected, f"Parsed URL: {got.raw_url}, Expected URL: {expected.raw_url}"
 
 
-async def test_send_apprise_notifications():
-    @dataclass
-    class AppriseTestCase:
-        include: list[str]
-        urls: list[str]
-        result: NotificationResult
-        exclude: list[str] = field(default_factory=list)
-        file: Path | None = None
+async def send_notification(test_case: AppriseTestCase):
+    assert URL_SUCCESS, "Email URL should be set on enviroment"
+    FAKE_MANAGER.config_manager.apprise_urls = []
+    if test_case.urls and any(test_case.urls):
+        FAKE_MANAGER.config_manager.apprise_urls = apprise.get_apprise_urls(FAKE_MANAGER, urls=test_case.urls)
+    FAKE_MANAGER.path_manager = PathManager(FAKE_MANAGER)
+    FAKE_MANAGER.path_manager.main_log = test_case.file or TEST_FILES_PATH / "valid_single_url.txt"
+    result, logs = await apprise.send_apprise_notifications(FAKE_MANAGER)
+    assert result.value == test_case.result.value, f"Result for this case should be {test_case.result.value}"
+    assert isinstance(logs, list), "Invalid return type for logs"
+    assert logs, "Logs can't be empty"
+    logs_as_str = "\n".join([line.msg for line in logs])
+    print(logs_as_str)
+    if test_case.include:
+        assert all(
+            match.casefold() in logs_as_str.casefold() for match in test_case.include
+        ), "Logs do not match expected pattern"
+    if test_case.exclude:
+        assert not any(match in logs_as_str for match in test_case.exclude), "Logs should not match exclude pattern"
 
-    FAKE_MANAGER.config_manager = ConfigManager(FAKE_MANAGER)
 
-    async def send_notification(test_case: AppriseTestCase):
-        FAKE_MANAGER.config_manager.apprise_urls = []
-        if test_case.urls and any(test_case.urls):
-            FAKE_MANAGER.config_manager.apprise_urls = apprise.get_apprise_urls(FAKE_MANAGER, urls=test_case.urls)
-        FAKE_MANAGER.path_manager = PathManager(FAKE_MANAGER)
-        FAKE_MANAGER.path_manager.main_log = test_case.file or TEST_FILES_PATH / "valid_single_url.txt"
-        result, logs = await apprise.send_apprise_notifications(FAKE_MANAGER)
-        assert result.value == test_case.result.value, f"Result for this case should be {test_case.result.value}"
-        assert isinstance(logs, list), "Invalid return type for logs"
-        assert logs, "Logs can't be empty"
-        logs_as_str = "\n".join([line.msg for line in logs])
-        print(logs_as_str)
-        if test_case.include:
-            assert all(
-                match.casefold() in logs_as_str.casefold() for match in test_case.include
-            ), "Logs do not match expected pattern"
-        if test_case.exclude:
-            assert not any(match in logs_as_str for match in test_case.exclude), "Logs should not match exclude pattern"
+async def test_failed_notification():
+    test_case = AppriseTestCase(urls=[URL_FAIL], result=NotificationResult.FAILED)
+    await send_notification(test_case)
 
-    url_fail = "mailto://test_user:test_email@gmail.com"
-    url_success = os.environ.get("APPRISE_TEST_EMAIL_URL")
-    url_success_attach_logs = f"attach_logs={url_success}"
 
-    assert url_success, "Email URL should be set on enviroment"
+async def test_successful_notification():
+    test_case = AppriseTestCase(
+        urls=[URL_SUCCESS],
+        result=NotificationResult.SUCCESS,
+        include=["Sent Email to"],
+        exclude=["Preparing Email attachment"],
+    )
+    await send_notification(test_case)
 
-    test_cases = [
-        [[], [url_fail], NotificationResult.FAILED],
-        [["Sent Email to"], [url_success_attach_logs, url_fail], NotificationResult.PARTIAL],
-        [[NotificationResult.NONE.value.plain], [""], NotificationResult.NONE],
-        [["Sent Email to"], [url_success], NotificationResult.SUCCESS, ["Preparing Email attachment"]],
-        [["Sent Email to", "Preparing Email attachment"], [url_success_attach_logs], NotificationResult.SUCCESS],
-        [
-            ["Sent Email to"],
-            [url_success_attach_logs],
-            NotificationResult.SUCCESS,
-            ["Preparing Email attachment"],
-            TEST_FILES_PATH / "file_that_does_exists.txt",
-        ],
-    ]
-    for test_case in test_cases:
-        case = AppriseTestCase(*test_case)
-        await send_notification(case)
+
+async def test_successful_notification_with_successful_attachment():
+    test_case = AppriseTestCase(
+        urls=[URL_SUCCESS_ATTACH_LOGS],
+        result=NotificationResult.SUCCESS,
+        include=["Sent Email to", "Preparing Email attachment"],
+    )
+    await send_notification(test_case)
+
+
+async def test_successful_notification_with_failed_attachment():
+    test_case = AppriseTestCase(
+        urls=[URL_SUCCESS_ATTACH_LOGS],
+        result=NotificationResult.SUCCESS,
+        include=["Sent Email to"],
+        exclude=["Preparing Email attachment"],
+        file=TEST_FILES_PATH / "file_that_does_exists.txt",
+    )
+    await send_notification(test_case)
+
+
+async def test_none_notification():
+    test_case = AppriseTestCase(
+        urls=[""], result=NotificationResult.NONE, include=[NotificationResult.NONE.value.plain]
+    )
+    await send_notification(test_case)
+
+
+async def test_partial_notification():
+    test_case = AppriseTestCase(
+        urls=[URL_SUCCESS_ATTACH_LOGS, URL_FAIL], result=NotificationResult.PARTIAL, include=["Sent Email to"]
+    )
+    await send_notification(test_case)
