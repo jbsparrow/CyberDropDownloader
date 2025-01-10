12a342e6a6d8e4c475d6c77456dc51df63850cdf
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2025-01-10 13:35:38 -0500

    refactor: handle album children limit

--- cyberdrop_dl/scraper/crawlers/saint_crawler.py
+++ cyberdrop_dl/scraper/crawlers/saint_crawler.py
@@ -9,6 +9,7 @@ from yarl import URL
 
 from cyberdrop_dl.clients.errors import ScrapeError
 from cyberdrop_dl.scraper.crawler import Crawler, create_task_id
+from cyberdrop_dl.utils.data_enums_classes.url_objects import FILE_HOST_ALBUM, ScrapeItem
 from cyberdrop_dl.utils.utilities import error_handling_wrapper, get_filename_and_ext
 
 if TYPE_CHECKING:
@@ -17,7 +18,6 @@ if TYPE_CHECKING:
     from bs4 import BeautifulSoup
 
     from cyberdrop_dl.managers.manager import Manager
-    from cyberdrop_dl.utils.data_enums_classes.url_objects import ScrapeItem
 
 
 class SaintCrawler(Crawler):
@@ -48,6 +48,7 @@ class SaintCrawler(Crawler):
         results = await self.get_album_results(album_id)
         scrape_item.album_id = album_id
         scrape_item.part_of_album = True
+        scrape_item.set_type(FILE_HOST_ALBUM, self.manager)
 
         async with self.request_limiter:
             soup: BeautifulSoup = await self.client.get_soup(self.domain, scrape_item.url, origin=scrape_item)
@@ -66,6 +67,7 @@ class SaintCrawler(Crawler):
             filename, ext = get_filename_and_ext(link.name)
             if not self.check_album_results(link, results):
                 await self.handle_file(link, scrape_item, filename, ext)
+            scrape_item.add_children()
 
     @error_handling_wrapper
     async def embed(self, scrape_item: ScrapeItem) -> None:
