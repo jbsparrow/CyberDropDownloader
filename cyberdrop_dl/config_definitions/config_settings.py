2525d32371fb12c6897b2497bf0a995c543e5ea0
datawhores <datawhores@users.noreply.github.com>
2025-01-13 21:10:31 -0600

    fix: pydantic import of custom types

--- cyberdrop_dl/config_definitions/config_settings.py
+++ cyberdrop_dl/config_definitions/config_settings.py
@@ -1,16 +1,17 @@
-from datetime import timedelta
 from logging import DEBUG
 from pathlib import Path
 from typing import Literal
+from datetime import timedelta
 
 from pydantic import BaseModel, ByteSize, Field, NonNegativeInt, PositiveInt, field_serializer, field_validator
 
-from cyberdrop_dl.config_definitions.pydantic.validators import parse_duration_to_timedelta
 from cyberdrop_dl.utils.constants import APP_STORAGE, BROWSERS, DOWNLOAD_STORAGE
 from cyberdrop_dl.utils.data_enums_classes.hash import Hashing
 from cyberdrop_dl.utils.data_enums_classes.supported_domains import SUPPORTED_SITES_DOMAINS
 
 from .pydantic.custom_types import AliasModel, HttpAppriseURL, NonEmptyStr
+from cyberdrop_dl.config_definitions.pydantic.validators import parse_duration_to_timedelta
+
 
 
 class DownloadOptions(BaseModel):
@@ -23,6 +24,7 @@ class DownloadOptions(BaseModel):
     remove_generated_id_from_filenames: bool = False
     scrape_single_forum_post: bool = False
     separate_posts: bool = False
+    separate_posts_format: NonEmptyStr = "{default}"
     skip_download_mark_completed: bool = False
     skip_referer_seen_before: bool = False
     maximum_number_of_children: list[NonNegativeInt] = []
@@ -49,7 +51,6 @@ class Logs(AliasModel):
     download_error_urls: Path = Field(Path("Download_Error_URLs.csv"), validation_alias="download_error_urls_filename")
     scrape_error_urls: Path = Field(Path("Scrape_Error_URLs.csv"), validation_alias="scrape_error_urls_filename")
     rotate_logs: bool = False
-    logs_expire_after: timedelta | None = None
     log_line_width: PositiveInt = Field(default=240, ge=50)
     logs_expire_after: timedelta|None=None
 
@@ -119,8 +120,8 @@ class IgnoreOptions(BaseModel):
 
 class RuntimeOptions(BaseModel):
     ignore_history: bool = False
-    log_level: int = DEBUG
-    console_log_level: int = 100
+    log_level: NonNegativeInt = DEBUG
+    console_log_level: NonNegativeInt = 100
     skip_check_for_partial_files: bool = False
     skip_check_for_empty_folders: bool = False
     delete_partial_files: bool = False
@@ -129,6 +130,7 @@ class RuntimeOptions(BaseModel):
     jdownloader_download_dir: Path | None = None
     jdownloader_autostart: bool = False
     jdownloader_whitelist: list[NonEmptyStr] = []
+    deep_scrape: bool = False
 
     @field_validator("jdownloader_download_dir", mode="before")
     @classmethod
