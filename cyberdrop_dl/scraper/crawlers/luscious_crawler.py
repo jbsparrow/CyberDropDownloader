d153d4ec0e7a7554516a770b2936a0d1d44b40d3
Jacob <admin@script-ware.net>
2025-01-22 11:42:42 -0500

    Add support for luscious album search

--- cyberdrop_dl/scraper/crawlers/luscious_crawler.py
+++ cyberdrop_dl/scraper/crawlers/luscious_crawler.py
@@ -26,6 +26,7 @@ class LusciousCrawler(Crawler):
             "AlbumGet": "\n    query AlbumGet($id: ID!) {\n  album {\n    get(id: $id) {\n      ... on Album {\n        ...AlbumStandard\n      }\n      ... on MutationError {\n        errors {\n          code\n          message\n        }\n      }\n    }\n  }\n}\n    \n    fragment AlbumStandard on Album {\n  __typename\n  id\n  title\n  labels\n  description\n  created\n  modified\n  like_status\n  number_of_favorites\n  number_of_dislikes\n  moderation_status\n  marked_for_deletion\n  marked_for_processing\n  number_of_pictures\n  number_of_animated_pictures\n  number_of_duplicates\n  slug\n  is_manga\n  url\n  download_url\n  permissions\n  created_by {\n    id\n    url\n    name\n    display_name\n    user_title\n    avatar_url\n  }\n  content {\n    id\n    title\n    url\n  }\n  language {\n    id\n    title\n    url\n  }\n  tags {\n    category\n    text\n    url\n    count\n  }\n  genres {\n    id\n    title\n    slug\n    url\n  }\n  audiences {\n    id\n    title\n    url\n  }\n  is_featured\n  featured_date\n  featured_by {\n    id\n    url\n    name\n    display_name\n    user_title\n    avatar_url\n  }\n}\n    ",
             "AlbumListOwnPictures": "\n    query AlbumListOwnPictures($input: PictureListInput!) {\n    picture {\n        list(input: $input) {\n            info {\n                ...FacetCollectionInfo\n            }\n            items {\n                ...PictureStandardWithoutAlbum\n            }\n        }\n    }\n}\n\nfragment FacetCollectionInfo on FacetCollectionInfo {\n    page\n    has_next_page\n    has_previous_page\n    total_items\n    total_pages\n    items_per_page\n    url_complete\n    url_filters_only\n}\n\nfragment PictureStandardWithoutAlbum on Picture {\n    __typename\n    id\n    title\n    created\n    like_status\n    number_of_comments\n    number_of_favorites\n    status\n    width\n    height\n    resolution\n    aspect_ratio\n    url_to_original\n    url_to_video\n    is_animated\n    position\n    tags {\n        id\n        category\n        text\n        url\n    }\n    permissions\n    url\n    thumbnails {\n        width\n        height\n        size\n        url\n    }\n}\n    ",
             "PictureListInsideAlbum": "\n    query PictureListInsideAlbum($input: PictureListInput!) {\n  picture {\n    list(input: $input) {\n      info {\n        ...FacetCollectionInfo\n      }\n      items {\n        __typename\n        id\n        title\n        description\n        created\n        like_status\n        number_of_comments\n        number_of_favorites\n        moderation_status\n        width\n        height\n        resolution\n        aspect_ratio\n        url_to_original\n        url_to_video\n        is_animated\n        position\n        permissions\n        url\n        tags {\n          category\n          text\n          url\n        }\n        thumbnails {\n          width\n          height\n          size\n          url\n        }\n      }\n    }\n  }\n}\n    \n    fragment FacetCollectionInfo on FacetCollectionInfo {\n  page\n  has_next_page\n  has_previous_page\n  total_items\n  total_pages\n  items_per_page\n  url_complete\n}\n    ",
+            "AlbumListWithPeek": "query AlbumListWithPeek($input: AlbumListInput!) {\n    album {\n        list(input: $input) {\n            info {\n                ...FacetCollectionInfo\n            }\n            items {\n                ...AlbumMinimal\n                peek_thumbnails {\n                    width\n                    height\n                    size\n                    url\n                }\n            }\n        }\n    }\n}\n\nfragment FacetCollectionInfo on FacetCollectionInfo {\n    page\n    has_next_page\n    has_previous_page\n    total_items\n    total_pages\n    items_per_page\n    url_complete\n    url_filters_only\n}\n\nfragment AlbumMinimal on Album {\n    __typename\n    id\n    title\n    labels\n    description\n    created\n    modified\n    number_of_favorites\n    number_of_pictures\n    slug\n    is_manga\n    url\n    download_url\n    cover {\n        width\n        height\n        size\n        url\n    }\n    content {\n        id\n        title\n        url\n    }\n    language {\n        id\n        title\n        url\n    }\n    tags {\n        id\n        category\n        text\n        url\n        count\n    }\n    genres {\n        id\n        title\n        slug\n        url\n    }\n    audiences {\n        id\n        title\n        url\n    }\n}"
         }
 
     """~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"""
@@ -37,15 +38,16 @@ class LusciousCrawler(Crawler):
         if "albums" not in scrape_item.url.parts or "read" in scrape_item.url.parts:
             log(f"Scrape Failed: Unknown URL Path for {scrape_item.url}", 40)
             return
+        if scrape_item.url.parts[-1] == "list":
+            await self.search(scrape_item)
         await self.album(scrape_item)
 
     async def create_graphql_query(self, operation: str, scrape_item: ScrapeItem, page: int = 1) -> str:
         """Creates a graphql query."""
+        query = scrape_item.url.query
         album_id = scrape_item.album_id
         data = {"id": "1", "operationName": operation, "query": self.graphql_queries[operation]}
         if operation == "PictureListInsideAlbum":
-            query = scrape_item.url.query
-
             sorting = query.get("sorting", "position")
             only_animated = query.get("only_animated", "false")
 
@@ -63,6 +65,16 @@ class LusciousCrawler(Crawler):
             }
         elif operation == "AlbumGet":
             data["variables"] = {"id": f"{album_id}"}
+        elif operation == "AlbumListWithPeek":
+            sorting = query.get("display", "date_newest")
+
+            data["variables"] = {
+                "input": {
+                    "display": sorting,
+                    "filters": [{"name": i, "value": v} for i, v in query.items() if i not in ("page", "display")],
+                    "page": page,
+                }
+            }
         return dump_json(data)
 
     async def album_pager(self, scrape_item: ScrapeItem) -> AsyncGenerator[dict]:
@@ -117,3 +129,40 @@ class LusciousCrawler(Crawler):
                 if not self.check_album_results(link, results):
                     await self.handle_file(link, scrape_item, filename, ext)
                 scrape_item.add_children()
+
+
+    async def search_pager(self, scrape_item: ScrapeItem) -> AsyncGenerator[dict]:
+        """Generator for search pages."""
+        page = int(scrape_item.url.query.get("page", 1))
+        while True:
+            query = await self.create_graphql_query("AlbumListWithPeek", scrape_item, page)
+            async with self.request_limiter:
+                json_data = await self.client.post_data(
+                    self.domain,
+                    self.graphql_url.with_query({"operationName": "AlbumListWithPeek"}),
+                    data=query,
+                    headers_inc={"Content-Type": "application/json"},
+                    origin=scrape_item,
+                )
+            has_next_page = json_data["data"]["album"]["list"]["info"]["has_next_page"]
+            yield json_data
+            if has_next_page:
+                page += 1
+                continue
+            break
+
+    @error_handling_wrapper
+    async def search(self, scrape_item: ScrapeItem) -> None:
+        query = scrape_item.url.query.get("q", "")
+        if not query:
+            log(f"Scrape Failed: No search query provided for {scrape_item.url}", 40)
+            return
+        
+        async for json_data in self.search_pager(scrape_item):
+            for item in json_data["data"]["album"]["list"]["items"]:
+                album_url = self.primary_base_domain / item["url"].replace("/albums/", "albums/")
+                new_scrape_item = self.create_scrape_item(
+                    parent_scrape_item=scrape_item,
+                    url=album_url,
+                )
+                await self.album(new_scrape_item)
