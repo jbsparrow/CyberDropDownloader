255b79577af7d90a1e627cdecf5032c8fe461fcf
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2025-01-24 11:33:12 -0500

    refactor: update crawler to new design
    Everything changed

--- cyberdrop_dl/scraper/crawlers/imageban_crawler.py
+++ cyberdrop_dl/scraper/crawlers/imageban_crawler.py
@@ -1,7 +1,5 @@
 from __future__ import annotations
 
-import calendar
-import datetime
 from typing import TYPE_CHECKING
 
 from yarl import URL
@@ -9,6 +7,7 @@ from yarl import URL
 from cyberdrop_dl.clients.errors import ScrapeError
 from cyberdrop_dl.scraper.crawler import Crawler, create_task_id
 from cyberdrop_dl.utils.data_enums_classes.url_objects import FILE_HOST_ALBUM, ScrapeItem
+from cyberdrop_dl.utils.logger import log
 from cyberdrop_dl.utils.utilities import error_handling_wrapper, get_filename_and_ext
 
 if TYPE_CHECKING:
@@ -31,96 +30,14 @@ class ImageBamCrawler(Crawler):
     @create_task_id
     async def fetch(self, scrape_item: ScrapeItem) -> None:
         """Determines where to send the scrape item based on the url."""
-        if "a" in scrape_item.url.parts:
-            await self.album(scrape_item)
-        elif "c" in scrape_item.url.parts:
-            await self.compilation(scrape_item)
-        elif "show" in scrape_item.url.parts:
-            await self.image(scrape_item)
-        elif "view" in scrape_item.url.parts:
-            await self.view(scrape_item)
-        else:
-            await self.handle_direct(scrape_item)
+        if self.is_cdn(scrape_item.url):
+            scrape_item.url = self.get_view_url(scrape_item.url)
 
-    @error_handling_wrapper
-    async def album(self, scrape_item: ScrapeItem) -> None:
-        """Scrapes a gallery."""
-        async with self.request_limiter:
-            soup: BeautifulSoup = await self.client.get_soup(self.domain, scrape_item.url, origin=scrape_item)
-
-        scrape_item.album_id = scrape_item.url.parts[2]
-        scrape_item.part_of_album = True
-
-        title = self.create_title(
-            soup.select_one("title").get_text().replace("Просмотр альбома: ", ""), scrape_item.album_id
-        )
-        content_block = soup.select_one('div[class="row text-center"]')
-        images = content_block.select("a")
-        scrape_item.set_type(FILE_HOST_ALBUM, self.manager)
-
-        for image in images:
-            link_str: str = image.get("href")
-            if not link_str or "javascript:void(0)" in link_str:
-                continue
-
-            link = self.parse_url(link_str, scrape_item.url.with_path("/"))
-            new_scrape_item = self.create_scrape_item(scrape_item, link, title, add_parent=scrape_item.url)
-            self.manager.task_group.create_task(self.run(new_scrape_item))
-            scrape_item.add_children()
-
-        next_page = soup.select_one('a[class*="page-link next"]')
-        if next_page:
-            link_str: str = next_page.get("href")
-            link = self.parse_url(link_str, scrape_item.url.with_path("/"))
-            new_scrape_item = self.create_scrape_item(scrape_item, link)
-            self.manager.task_group.create_task(self.run(new_scrape_item))
-
-    @error_handling_wrapper
-    async def compilation(self, scrape_item: ScrapeItem) -> None:
-        """Scrapes a compilation."""
-        async with self.request_limiter:
-            soup: BeautifulSoup = await self.client.get_soup(self.domain, scrape_item.url, origin=scrape_item)
-
-        title = self.create_title(soup.select_one("blockquote").get_text(), scrape_item.url.parts[2])
-        scrape_item.add_to_parent_title(title)
-        content_block = soup.select("div[class=container-fluid]")[-1]
-        images = content_block.select("img")
-        scrape_item.set_type(FILE_HOST_ALBUM, self.manager)
-        date = self.parse_datetime("-".join(scrape_item.url.parts[2:5]))
-
-        for image in images:
-            link_str: str = image.get("src")
-            link = self.parse_url(link_str)
-            new_scrape_item = self.create_scrape_item(scrape_item, scrape_item.url, possible_datetime=date)
-            filename, ext = get_filename_and_ext(link.name)
-            await self.handle_file(link, new_scrape_item, filename, ext)
-            scrape_item.add_children()
-
-    @error_handling_wrapper
-    async def image(self, scrape_item: ScrapeItem) -> None:
-        """Scrapes an image."""
-        if await self.check_complete_from_referer(scrape_item):
+        if "view" not in scrape_item.url.parts:
+            log(f"Scrape Failed: Unknown URL path: {scrape_item.url}", 40)
             return
 
-        async with self.request_limiter:
-            soup: BeautifulSoup = await self.client.get_soup(self.domain, scrape_item.url, origin=scrape_item)
-
-        image = soup.select_one("img[id=img_main]")
-        if not image:
-            raise ScrapeError(422, origin=scrape_item)
-
-        date = self.parse_datetime("-".join(scrape_item.url.parts[2:5]))
-        new_scrape_item = self.create_scrape_item(scrape_item, scrape_item.url, possible_datetime=date)
-        link_str: str = image.get("src")
-        link = self.parse_url(link_str)
-        filename, ext = get_filename_and_ext(link.name)
-        await self.handle_file(link, new_scrape_item, filename, ext)
-
-    @error_handling_wrapper
-    async def handle_direct(self, scrape_item: ScrapeItem) -> None:
-        """Scrapes an image."""
-        filename, ext = get_filename_and_ext(scrape_item.url.name)
-        await self.handle_file(scrape_item.url, scrape_item, filename, ext)
+        await self.view(scrape_item)
 
     @error_handling_wrapper
     async def view(self, scrape_item: ScrapeItem) -> None:
@@ -128,17 +45,71 @@ class ImageBamCrawler(Crawler):
             soup: BeautifulSoup = await self.client.get_soup(self.domain, scrape_item.url, origin=scrape_item)
 
         if "Share this gallery" in soup.text:
-            return await self.album(scrape_item)
-        await self.image(scrape_item)
+            return await self.gallery(scrape_item, soup)
+
+        await self.image(scrape_item, soup)
+
+    @error_handling_wrapper
+    async def gallery(self, scrape_item: ScrapeItem, soup: BeautifulSoup | None = None) -> None:
+        if not soup:
+            async with self.request_limiter:
+                soup: BeautifulSoup = await self.client.get_soup(self.domain, scrape_item.url, origin=scrape_item)
+        gallery_name = soup.select_one("a#gallery-name").get_text()
+        gallery_id = scrape_item.url.name
+        title = self.create_title(gallery_name, gallery_id)
+        scrape_item.part_of_album = True
+        scrape_item.album_id = gallery_id
+        scrape_item.set_type(FILE_HOST_ALBUM, self.manager)
+        scrape_item.add_to_parent_title(title)
+        results = await self.get_album_results(gallery_id)
+
+        images = soup.select("ul.images a.thumbnail")
+        for image in images:
+            link_str: str = image.get("href")
+            link = self.parse_url(link_str)
+            new_scrape_item = self.create_scrape_item(scrape_item, link)
+            if not self.check_album_results(link, results):
+                await self.image(new_scrape_item)
+            scrape_item.add_children()
+
+    @error_handling_wrapper
+    async def image(self, scrape_item: ScrapeItem, soup: BeautifulSoup | None = None) -> None:
+        """Scrapes an image."""
+        if await self.check_complete_from_referer(scrape_item):
+            return
+
+        if not soup:
+            async with self.request_limiter:
+                soup: BeautifulSoup = await self.client.get_soup(self.domain, scrape_item.url, origin=scrape_item)
+
+        image_tag = soup.select_one("img.main-image")
+        if not image_tag:
+            raise ScrapeError(422, origin=scrape_item)
+
+        from_gallery = soup.select_one("div.view-navigation a:has(i.fas.fa-reply)")
+        if from_gallery and not scrape_item.album_id:
+            gallery_url_str: str = from_gallery.get("href")
+            gallery_url = self.parse_url(gallery_url_str)
+            gallery_id = gallery_url.name
+            scrape_item.album_id = gallery_id
+
+        title: str = image_tag.get("alt")
+        link_str: str = image_tag.get("src")
+        link = self.parse_url(link_str)
+        filename, ext = get_filename_and_ext(link.name)
+        custom_filename, _ = get_filename_and_ext(title)
+        await self.handle_file(link, scrape_item, filename, ext, custom_filename=custom_filename)
+
+    """~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"""
+
+    def is_cdn(self, url: URL) -> bool:
+        bare_host = url.host.strip(".com")
+        return "imageban" in url.host.split(".") and "." in bare_host
+
+    def get_view_url(self, url: URL) -> URL:
+        view_id = url.name.rsplit("_", 1)[0]
+        return self.primary_base_domain / "view" / view_id
 
     def set_cookies(self) -> None:
         """Set cookies to bypass confirmation."""
         self.client.client_manager.cookies.update_cookies({"nsfw_inter": "1"}, self.primary_base_domain)
-
-    """~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"""
-
-    @staticmethod
-    def parse_datetime(date: str) -> int:
-        """Parses a datetime string into a unix timestamp."""
-        parsed_date = datetime.datetime.strptime(date, "%Y-%m-%d")
-        return calendar.timegm(parsed_date.timetuple())
