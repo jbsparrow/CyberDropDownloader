e55474e5e41396e226bee91c7054297e8d46c049
Jacob <admin@script-ware.net>
2025-01-23 21:53:17 -0500

    Ruff fixes

--- cyberdrop_dl/scraper/crawlers/tiktok_crawler.py
+++ cyberdrop_dl/scraper/crawlers/tiktok_crawler.py
@@ -1,24 +1,18 @@
 from __future__ import annotations
 
-import calendar
-import datetime
 from typing import TYPE_CHECKING
 
 from aiolimiter import AsyncLimiter
-from bs4 import BeautifulSoup
 from yarl import URL
 
-from cyberdrop_dl.clients.errors import NoExtensionError, ScrapeError
-from cyberdrop_dl.utils.utilities import log
 from cyberdrop_dl.scraper.crawler import Crawler, create_task_id
 from cyberdrop_dl.utils.data_enums_classes.url_objects import FILE_HOST_ALBUM, ScrapeItem
-from cyberdrop_dl.utils.utilities import error_handling_wrapper, get_filename_and_ext
+from cyberdrop_dl.utils.utilities import error_handling_wrapper, get_filename_and_ext, log
 
 if TYPE_CHECKING:
-    from bs4 import BeautifulSoup
+    from collections.abc import AsyncGenerator
 
     from cyberdrop_dl.managers.manager import Manager
-    from typing import AsyncGenerator
 
 
 class TikTokCrawler(Crawler):
@@ -33,23 +27,24 @@ class TikTokCrawler(Crawler):
     async def fetch(self, scrape_item: ScrapeItem) -> None:
         """Determines where to send the scrape item based on the URL."""
         # await self.test(scrape_item)
-        
+
         if "@" in scrape_item.url.parts[1]:
             await self.profile(scrape_item)
         # else:
         #     await self.video(scrape_item)
-
 
     async def profile_post_pager(self, scrape_item: ScrapeItem) -> AsyncGenerator[dict]:
         """Generator for profile posts."""
         username = scrape_item.url.parts[1][1:]
         cursor = "0"
         while True:
-            posts_api_url = (self.api_url / "user" / "posts").with_query({
-                "cursor": cursor,
-                "unique_id": username,
-                "count": "50",
-            })
+            posts_api_url = (self.api_url / "user" / "posts").with_query(
+                {
+                    "cursor": cursor,
+                    "unique_id": username,
+                    "count": "50",
+                }
+            )
             async with self.request_limiter:
                 json_data = await self.client.get_json(self.primary_base_domain, posts_api_url, origin=scrape_item)
             has_next_page = json_data["data"]["hasMore"]
@@ -64,7 +59,6 @@ class TikTokCrawler(Crawler):
                 continue
             break
 
-
     @error_handling_wrapper
     async def test(self, scrape_item: ScrapeItem) -> None:
         """Tests the TikTok crawler."""
@@ -76,7 +70,9 @@ class TikTokCrawler(Crawler):
         """Handles an image carousel post."""
         post_id = post["video_id"]
         title = post["title"] if post["title"] else f"Post {post_id}"
-        new_scrape_item = self.create_scrape_item(scrape_item, scrape_item.url, title, True, post_id, post["create_time"])
+        new_scrape_item = self.create_scrape_item(
+            scrape_item, scrape_item.url, title, True, post_id, post["create_time"]
+        )
         for image in post["images"]:
             image_url = URL(image)
             filename, ext = get_filename_and_ext(image_url.name)
@@ -91,13 +87,13 @@ class TikTokCrawler(Crawler):
         async for json_data in self.profile_post_pager(scrape_item):
             for item in json_data["data"]["videos"]:
                 post_url = URL(item["play"])
-                if len(item.get('images', [])) > 0:
+                if len(item.get("images", [])) > 0:
                     await self.handle_image_post(scrape_item, item)
                     continue
-                if str(post_url).endswith('mp3'):
+                if str(post_url).endswith("mp3"):
                     continue
                 filename, ext = f'{item["video_id"]}.mp4', "mp4"
-                date = item['create_time']
+                date = item["create_time"]
 
                 new_scrape_item = self.create_scrape_item(scrape_item, post_url, "", True, scrape_item.album_id, date)
                 await self.handle_file(post_url, new_scrape_item, filename, ext)
@@ -113,4 +109,3 @@ class TikTokCrawler(Crawler):
     #         video_url = scrape_item.url
     #         filename, ext = get_filename_and_ext(video_url.name)
     #         await self.handle_file(video_url, scrape_item, filename, ext)
-
