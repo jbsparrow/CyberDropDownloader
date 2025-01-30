f19223e28202882032cddfdc352dc96059b1ed7b
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2025-01-30 11:49:12 -0500

    refactor: use `error_handling_wrapper` for `handle_internal_link`

--- cyberdrop_dl/scraper/crawlers/xenforo_crawler.py
+++ cyberdrop_dl/scraper/crawlers/xenforo_crawler.py
@@ -315,18 +315,16 @@ class XenforoCrawler(Crawler):
         if not link or link == self.primary_base_domain:
             return
         assert link.host
-        try:
-            if self.is_attachment(link):
-                return await self.handle_internal_link(link, scrape_item)
-            if self.primary_base_domain.host in link.host:  # type: ignore
-                origin = scrape_item.parents[0]
-                return log(f"Skipping nested thread URL {link} found on {origin}", 10)
-            new_scrape_item = self.create_scrape_item(scrape_item, link)
-            new_scrape_item.set_type(None, self.manager)
-            self.handle_external_links(new_scrape_item)
-        except TypeError:
-            log(f"Scrape Failed: encountered while handling {link}", 40)
+        if self.is_attachment(link):
+            return await self.handle_internal_link(link, scrape_item)
+        if self.primary_base_domain.host in link.host:  # type: ignore
+            origin = scrape_item.parents[0]
+            return log(f"Skipping nested thread URL {link} found on {origin}", 10)
+        new_scrape_item = self.create_scrape_item(scrape_item, link)
+        new_scrape_item.set_type(None, self.manager)
+        self.handle_external_links(new_scrape_item)
 
+    @error_handling_wrapper
     async def handle_internal_link(self, link: URL, scrape_item: ScrapeItem) -> None:
         """Handles internal links."""
         filename, ext = get_filename_and_ext(link.name, True)
