73adb63f61821d92c16971b08f9208e6f8dc8162
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2025-02-03 09:12:39 -0500

    refactor: use `CachedSession`

--- cyberdrop_dl/clients/request_client.py
+++ cyberdrop_dl/clients/request_client.py
@@ -4,6 +4,7 @@ from functools import wraps
 from typing import TYPE_CHECKING
 
 import aiohttp
+from aiohttp_client_cache.session import CachedSession
 
 from cyberdrop_dl.utils import constants
 from cyberdrop_dl.utils.logger import log
@@ -45,17 +46,23 @@ class Client:
         trace_config.on_request_end.append(on_request_end)
         self.trace_configs.append(trace_config)
 
+    @property
+    def request_params(self) -> dict:
+        params = {"headers": self.headers, "ssl": self.client_manager.ssl_context, "proxy": self.client_manager.proxy}
+        return params
+
 
 def create_session(func: Callable) -> Callable:
     """Wrapper handles client session creation to pass cookies."""
 
     @wraps(func)
     async def wrapper(self: Client, *args, **kwargs):
-        async with aiohttp.ClientSession(
+        async with CachedSession(
             headers=self.headers,
             cookie_jar=self.client_manager.cookies,
             timeout=self.client_manager.timeout,
             trace_configs=self.trace_configs,
+            cache=self.client_manager.manager.cache_manager.request_cache,
         ) as client:
             kwargs["client_session"] = client
             return await func(self, *args, **kwargs)
