27f7358e9fcca110456ad86a6bd2cd13a8bfe9db
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2025-02-13 07:59:38 -0500

    feat: add support for playlists

--- cyberdrop_dl/scraper/crawlers/spankbang_crawler.py
+++ cyberdrop_dl/scraper/crawlers/spankbang_crawler.py
@@ -11,6 +11,7 @@ from yarl import URL
 
 from cyberdrop_dl.clients.errors import ScrapeError
 from cyberdrop_dl.scraper.crawler import Crawler, create_task_id
+from cyberdrop_dl.utils.data_enums_classes.url_objects import FILE_HOST_ALBUM, ScrapeItem
 from cyberdrop_dl.utils.logger import log_debug
 from cyberdrop_dl.utils.utilities import error_handling_wrapper
 
@@ -23,6 +24,7 @@ DEFAULT_QUALITY = "main"
 RESOLUTIONS = ["4k", "1080p", "720p", "480p", "320p", "240p"]  # best to worst
 
 VIDEO_REMOVED_SELECTOR = "[id='video_removed'], [class*='video_removed']"
+PLAYLIST_ITEM_SELECTOR = "div.video-list > div.video-item > a"
 
 
 class SpankBangCrawler(Crawler):
@@ -47,7 +49,28 @@ class SpankBangCrawler(Crawler):
 
     @error_handling_wrapper
     async def playlist(self, scrape_item: ScrapeItem) -> None:
-        raise NotImplementedError
+        """Scrapes a playlist. Max 100 items per playlist"""
+        playlist_id = scrape_item.url.parts[1]
+        # TODO: add playlist pagination
+        async with self.request_limiter:
+            soup = get_test_soup()
+
+        scrape_item.part_of_album = True
+        scrape_item.set_type(FILE_HOST_ALBUM, self.manager)
+        scrape_item.album_id = playlist_id
+        title = soup.select_one("title").text.rsplit("Playlist -")[0].strip()  # type: ignore
+        title = self.create_title(title, playlist_id)
+        scrape_item.add_to_parent_title(title)
+
+        results = await self.get_album_results(playlist_id)
+        videos = soup.select(PLAYLIST_ITEM_SELECTOR)
+        for video in videos:
+            link_str: str = video.get("href")  # type: ignore
+            link = self.parse_url(link_str)
+            if not self.check_album_results(link, results):
+                new_scrape_item = self.create_scrape_item(scrape_item, link, add_parent=scrape_item.url)
+                self.manager.task_group.create_task(self.run(new_scrape_item))
+            scrape_item.add_children()
 
     @error_handling_wrapper
     async def video(self, scrape_item: ScrapeItem) -> None:
@@ -159,9 +182,9 @@ def parse_datetime(date: str) -> int:
 
 
 def get_test_soup() -> BeautifulSoup:
-    file_html = Path("spankbang.htm").read_bytes()
+    file_html = Path("spankbang_playlist.htm").read_bytes()
     return BeautifulSoup(file_html, "html.parser")
 
 
 def is_playlist(url: URL) -> bool:
-    return "playlist" in url.parts and "-" in url.parts[1]
+    return "playlist" in url.parts and "-" not in url.parts[1]
