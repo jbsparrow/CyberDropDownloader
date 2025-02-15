dfb4a8378626360d2d2a17d383943c73fa5b6c83
Jacob <admin@script-ware.net>
2025-02-15 15:47:11 -0500

    Fix celebforum image downloads
    Download full quality images from celebforum instead of thumbnails.

--- cyberdrop_dl/scraper/crawlers/celebforum_crawler.py
+++ cyberdrop_dl/scraper/crawlers/celebforum_crawler.py
@@ -14,9 +14,18 @@ class CelebForumCrawler(XenforoCrawler):
     primary_base_domain = URL("https://celebforum.to")
     post_selectors = PostSelectors(
         date=Selector("time", "data-time"),
+        images=Selector("a[class*=js-lbImage]", "href"),
     )
     selectors = XenforoSelectors(posts=post_selectors)
     domain = "celebforum"
 
     def __init__(self, manager: Manager) -> None:
         super().__init__(manager, self.domain, "CelebForum")
+
+    def filter_link(self, link):
+        if link.host == self.primary_base_domain.host:
+            if all(part in link.parts for part in ["data", "attachments"]): # Thumbnails
+                return None
+            if all(part in link.parts for part in ["data", "assets"]): # Placeholder content for insufficient rank
+                return None
+        return link
