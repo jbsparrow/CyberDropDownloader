250090d4c3e0743a61e9b7a85b78076909bd77f2
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2025-02-21 05:31:17 -0500

    refactor: replace some keys

--- cyberdrop_dl/utils/dumper.py
+++ cyberdrop_dl/utils/dumper.py
@@ -15,6 +15,10 @@ if TYPE_CHECKING:
     from cyberdrop_dl.utils.data_enums_classes.url_objects import MediaItem
 
 
+KEYS_TO_REMOVE = "file_lock_reference_name", "task_id"
+KEYS_TO_REPLACE = {"current_attempt": "attempts"}
+
+
 class Dumper:
     def __init__(self, manager: Manager) -> None:
         self.manager = manager
@@ -35,7 +39,10 @@ def convert_to_dict(media_item: MediaItem) -> dict:
     item = asdict(media_item)
     if date:
         item["datetime"] = datetime.datetime.fromtimestamp(date)
-    return item
+    for key, new_key in KEYS_TO_REPLACE.items():
+        item[new_key] = item[key]
+        del item[key]
+    return {k: v for k, v in item.items() if k not in KEYS_TO_REMOVE}
 
 
 async def dump_jsonl(data: Generator[dict], file: Path) -> None:
