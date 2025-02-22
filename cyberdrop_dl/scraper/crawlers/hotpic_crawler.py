fafb24cd738123660ff6c5fbdaaef6045873da37
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2025-02-21 19:57:39 -0500

    feat: download direct images (hotpic)
    Thumbnails are convert to full res images before download

--- cyberdrop_dl/scraper/crawlers/hotpic_crawler.py
+++ cyberdrop_dl/scraper/crawlers/hotpic_crawler.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from pathlib import Path
 from typing import TYPE_CHECKING
 
 from yarl import URL
@@ -26,9 +27,11 @@ class HotPicCrawler(Crawler):
     async def fetch(self, scrape_item: ScrapeItem) -> None:
         """Determines where to send the scrape item based on the url."""
         if "album" in scrape_item.url.parts:
-            await self.album(scrape_item)
+            return await self.album(scrape_item)
         elif "i" in scrape_item.url.parts:
-            await self.image(scrape_item)
+            return await self.image(scrape_item)
+        elif "uploads" in scrape_item.url.parts:
+            return await self.handle_direct_link(scrape_item)
         else:
             raise ValueError
 
@@ -39,14 +42,14 @@ class HotPicCrawler(Crawler):
             soup: BeautifulSoup = await self.client.get_soup(self.domain, scrape_item.url, origin=scrape_item)
 
         scrape_item.album_id = scrape_item.url.parts[2]
-        title = self.create_title(soup.select_one("title").text.rsplit(" - ")[0], scrape_item.album_id)
+        title = self.create_title(soup.title.text.rsplit(" - ")[0], scrape_item.album_id)  # type: ignore
         scrape_item.add_to_parent_title(title)
         scrape_item.part_of_album = True
         scrape_item.set_type(FILE_HOST_ALBUM, self.manager)
 
         files = soup.select("a[class*=spotlight]")
         for file in files:
-            link_str: str = file.get("href")
+            link_str: str = file.get("href")  # type: ignore
             link = self.parse_url(link_str)
             filename, ext = get_filename_and_ext(link.name)
             await self.handle_file(link, scrape_item, filename, ext)
@@ -61,7 +64,28 @@ class HotPicCrawler(Crawler):
         async with self.request_limiter:
             soup: BeautifulSoup = await self.client.get_soup(self.domain, scrape_item.url, origin=scrape_item)
 
-        link_str: str = soup.select_one("img[id*=main-image]").get("src")
+        link_str: str = soup.select_one("img[id*=main-image]").get("src")  # type: ignore
         link = self.parse_url(link_str)
         filename, ext = get_filename_and_ext(link.name)
         await self.handle_file(link, scrape_item, filename, ext)
+
+    @error_handling_wrapper
+    async def handle_direct_link(self, scrape_item: ScrapeItem) -> None:
+        link = thumbnail_to_img(scrape_item.url)
+        filename, ext = get_filename_and_ext(link.name)
+        await self.handle_file(link, scrape_item, filename, ext)
+
+
+def thumbnail_to_img(url: URL) -> URL:
+    if "thumb" not in url.parts:
+        return url
+
+    url = with_suffix_encoded(url, ".jpg")
+    new_parts = [p for p in url.parts if p not in ("/", "thumb")]
+    new_path = "/".join(new_parts)
+    return url.with_path(new_path)
+
+
+def with_suffix_encoded(url: URL, suffix: str) -> URL:
+    name = Path(url.raw_name).with_suffix(suffix)
+    return url.parent.joinpath(str(name), encoded=True).with_query(url.query).with_fragment(url.fragment)
