da4cd3f114e73e5090ccadd70167777bb59c5e29
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2025-02-23 08:55:31 -0500

    fix: stream URL check (bunkr)

--- cyberdrop_dl/scraper/crawlers/bunkrr_crawler.py
+++ cyberdrop_dl/scraper/crawlers/bunkrr_crawler.py
@@ -243,6 +243,10 @@ class BunkrrCrawler(Crawler):
 
 def is_stream_redirect(url: URL) -> bool:
     assert url.host
+    first_subdomain = url.host.split(".")[0]
+    _, _, number = first_subdomain.partition("cdn")
+    if number.isdigit():
+        return True
     return any(part in url.host for part in ("cdn12", "cdn-")) or url.host == "cdn.bunkr.ru"
 
 
