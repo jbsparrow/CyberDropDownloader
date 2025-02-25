12a126cf41949e3391836236b5358dfad8ab7f24
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2025-02-24 21:29:14 -0500

    fix: unsupported forum filename parsing

--- cyberdrop_dl/utils/utilities.py
+++ cyberdrop_dl/utils/utilities.py
@@ -123,7 +123,7 @@ def get_filename_and_ext(filename: str, forum: bool = False) -> tuple[str, str]:
     if not filename_as_path.suffix:
         raise NoExtensionError
     ext_no_dot = filename_as_path.suffix.split(".")[1]
-    if ext_no_dot.isdigit() and forum:
+    if ext_no_dot.isdigit() and forum and "-" in filename:
         name, ext = filename_as_path.name.rsplit("-", 1)
         ext = ext.rsplit(".")[0]
         filename_as_path = Path(f"{name}.{ext}")
