a0ef5bc3ed16eed28d7c04b8ea668a96e1ebe7b1
datawhores <67020411+datawhores@users.noreply.github.com>
2025-03-06 13:13:00 -0600

    Update filters.py

--- cyberdrop_dl/scraper/filters.py
+++ cyberdrop_dl/scraper/filters.py
@@ -8,7 +8,8 @@ from bs4 import BeautifulSoup
 from yarl import URL
 
 from cyberdrop_dl.clients.errors import NoExtensionError
-from cyberdrop_dl.utils.constants import DISABLE_CACHE, FILE_FORMATS
+from cyberdrop_dl.utils.constants import FILE_FORMATS
+import cyberdrop_dl.utils.constants as constants
 from cyberdrop_dl.utils.utilities import get_filename_and_ext
 
 if TYPE_CHECKING:
@@ -98,7 +99,7 @@ async def get_return_value(url: URL | str) -> bool | None:
 
 async def filter_fn(response: ClientResponse) -> bool:
     """Filter function for aiohttp_client_cache"""
-    if DISABLE_CACHE:
+    if constants.DISABLE_CACHE:
         return False
     HTTP_404_LIKE_STATUS = {HTTPStatus.NOT_FOUND, HTTPStatus.GONE, HTTPStatus.UNAVAILABLE_FOR_LEGAL_REASONS}
 
