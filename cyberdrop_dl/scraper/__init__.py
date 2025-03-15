3ef7f2c189becc3a08a5417c4d5c629c4ce6e123
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2025-03-15 11:56:56 -0500

    chore: ruff fixes

--- cyberdrop_dl/scraper/__init__.py
+++ cyberdrop_dl/scraper/__init__.py
@@ -22,8 +22,8 @@ from cyberdrop_dl.scraper.crawlers.eporner_crawler import EpornerCrawler
 from cyberdrop_dl.scraper.crawlers.erome_crawler import EromeCrawler
 from cyberdrop_dl.scraper.crawlers.f95zone_crawler import F95ZoneCrawler
 from cyberdrop_dl.scraper.crawlers.fapello_crawler import FapelloCrawler
-from cyberdrop_dl.scraper.crawlers.generic_crawler import GenericCrawler
 from cyberdrop_dl.scraper.crawlers.fileditch_crawler import FileditchCrawler
+from cyberdrop_dl.scraper.crawlers.generic_crawler import GenericCrawler
 from cyberdrop_dl.scraper.crawlers.gofile_crawler import GoFileCrawler
 from cyberdrop_dl.scraper.crawlers.google_drive_crawler import GoogleDriveCrawler
 from cyberdrop_dl.scraper.crawlers.hotpic_crawler import HotPicCrawler
