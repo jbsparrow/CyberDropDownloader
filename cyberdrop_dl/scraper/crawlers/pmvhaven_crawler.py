0308543c033fe8b48423bf5f248971935cb82d52
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2025-03-19 11:48:59 -0500

    feat: add collections support
    also use API for everything

--- cyberdrop_dl/scraper/crawlers/pmvhaven_crawler.py
+++ cyberdrop_dl/scraper/crawlers/pmvhaven_crawler.py
@@ -14,6 +14,8 @@ from cyberdrop_dl.utils.logger import log_debug
 from cyberdrop_dl.utils.utilities import error_handling_wrapper, get_filename_and_ext
 
 if TYPE_CHECKING:
+    from collections.abc import AsyncGenerator
+
     from bs4 import BeautifulSoup
 
     from cyberdrop_dl.managers.manager import Manager
@@ -21,10 +23,12 @@ if TYPE_CHECKING:
 
 
 JS_VIDEO_INFO_SELECTOR = "script#__NUXT_DATA__"
+API_ENTRYPOINT = URL("https://pmvhaven.com/api/v2/")
+PRIMARY_BASE_DOMAIN = URL("https://pmvhaven.com")
 
 
 class PMVHavenCrawler(Crawler):
-    primary_base_domain = URL("https://pmvhaven.com")
+    primary_base_domain = PRIMARY_BASE_DOMAIN
 
     def __init__(self, manager: Manager) -> None:
         super().__init__(manager, "pmvhaven", "PMVHaven")
@@ -32,18 +36,165 @@ class PMVHavenCrawler(Crawler):
     @create_task_id
     async def fetch(self, scrape_item: ScrapeItem) -> None:
         if "video" in scrape_item.url.parts:
-            return await self.video(scrape_item)
+            return await self.video_from_api(scrape_item)
+        if "star" in scrape_item.url.parts:
+            return await self.model(scrape_item)
+        if "tags" in scrape_item.url.parts:
+            return await self.tag(scrape_item)
+        if "music" in scrape_item.url.parts:
+            return await self.music(scrape_item)
+        if "search" in scrape_item.url.parts:
+            return await self.search(scrape_item)
+        if "category" in scrape_item.url.parts:
+            return await self.category(scrape_item)
+        if "profile" in scrape_item.url.parts:
+            return await self.profile(scrape_item)
+        if "playlist" in scrape_item.url.parts:
+            return await self.playlist(scrape_item)
         raise ValueError
 
+    async def profile(self, scrape_item: ScrapeItem) -> None:
+        username = scrape_item.url.name
+        data = {"profile": username, "mode": "GetUser"}
+        api_url = API_ENTRYPOINT / "profileInput"
+        title = self.create_title(username)
+        scrape_item.setup_as_album(title)
+
+        # Videos
+        add_data = {"mode": "GetMoreProfileVideos", "user": username}
+        new_scrape_item = scrape_item.create_new(scrape_item.url, new_title_part="Videos")
+        async for json_resp in self.api_pager(api_url, add_data):
+            await self.iter_videos(new_scrape_item, json_resp["data"])
+
+        # Favorites
+        add_data = {"mode": "GetMoreFavoritedVideos", "user": username, "search": None, "date": "Date", "sort": "Sort"}
+        new_scrape_item = scrape_item.create_new(scrape_item.url, new_title_part="Favorites")
+        async for json_resp in self.api_pager(api_url, add_data):
+            await self.iter_videos(new_scrape_item, json_resp["data"])
+
+        # Playlist
+        # TODO: add pagination support for user playlists
+        async with self.request_limiter:
+            json_resp: dict = await self.client.post_data(self.domain, api_url, data=data)
+
+        user_info: dict[str, dict] = json_resp["data"]
+        for playlist in user_info["playlists"]:
+            playlist_id = playlist["_id"]
+            link = PRIMARY_BASE_DOMAIN / "playlist" / playlist_id
+            new_scrape_item = scrape_item.create_child(link, new_title_part="Playlists")
+            self.manager.task_group.create_task(self.run(new_scrape_item))
+            scrape_item.add_children()
+
+    async def playlist(self, scrape_item: ScrapeItem) -> None:
+        playlist_id = scrape_item.url.name
+        add_data = {"mode": "loadPlaylistVideos", "playlist": playlist_id}
+        api_url = API_ENTRYPOINT / "playlists"
+        title: str = ""
+        async for json_resp in self.api_pager(api_url, add_data, check_key="videos"):
+            if not title:
+                playlist_name: str = json_resp["playlist"]["name"]
+                title = self.create_title(playlist_name, playlist_id)
+                scrape_item.setup_as_album(title, album_id=playlist_id)
+            await self.iter_videos(scrape_item, json_resp["videos"])
+
+    async def model(self, scrape_item: ScrapeItem) -> None:
+        model_name = scrape_item.url.name
+        add_data = {"mode": "SearchStar", "star": model_name, "profile": None}
+        api_url = API_ENTRYPOINT / "search"
+        title: str = ""
+        async for json_resp in self.api_pager(api_url, add_data):
+            if not title:
+                title = self.create_title(model_name)
+                scrape_item.setup_as_album(title)
+
+            await self.iter_videos(scrape_item, json_resp["data"])
+
+    async def creator(self, scrape_item: ScrapeItem) -> None:
+        creator_name = scrape_item.url.name
+        add_data = {"mode": "SearchCreator", "creator": creator_name, "profile": None}
+        api_url = API_ENTRYPOINT / "search"
+        title: str = ""
+        async for json_resp in self.api_pager(api_url, add_data):
+            if not title:
+                title = self.create_title(creator_name)
+                scrape_item.setup_as_album(title)
+
+            await self.iter_videos(scrape_item, json_resp["data"])
+
+    async def music(self, scrape_item: ScrapeItem) -> None:
+        search_value = scrape_item.url.name
+        add_data = {"mode": "SearchMusic", "music": search_value, "profile": None}
+        api_url = API_ENTRYPOINT / "search"
+        title: str = ""
+        async for json_resp in self.api_pager(api_url, add_data):
+            if not title:
+                title = self.create_title(search_value)
+                scrape_item.setup_as_album(title)
+
+            await self.iter_videos(scrape_item, json_resp["data"])
+
+    async def search(self, scrape_item: ScrapeItem) -> None:
+        search_value = scrape_item.url.name
+        add_data = {"mode": "DefaultSearch", "data": search_value, "profile": None}
+        api_url = API_ENTRYPOINT / "search"
+        title: str = ""
+        async for json_resp in self.api_pager(api_url, add_data):
+            if not title:
+                title = self.create_title(search_value)
+                scrape_item.setup_as_album(title)
+
+            await self.iter_videos(scrape_item, json_resp["data"])
+
+    async def category(self, scrape_item: ScrapeItem) -> None:
+        category_name = scrape_item.url.name
+        add_data = {"mode": category_name, "profile": None}
+        api_url = API_ENTRYPOINT / "search"
+        title: str = ""
+        async for json_resp in self.api_pager(api_url, add_data):
+            if not title:
+                title = self.create_title(category_name)
+                scrape_item.setup_as_album(title)
+
+            await self.iter_videos(scrape_item, json_resp["data"])
+
+    async def tag(self, scrape_item: ScrapeItem) -> None:
+        tag_name = scrape_item.url.name
+        add_data = {"mode": "SearchMoreTag", "tag": tag_name, "profile": None}
+        api_url = API_ENTRYPOINT / "search"
+        title: str = ""
+        async for json_resp in self.api_pager(api_url, add_data):
+            if not title:
+                title = self.create_title(tag_name)
+                scrape_item.setup_as_album(title)
+
+            await self.iter_videos(scrape_item, json_resp["data"])
+
     @error_handling_wrapper
-    async def video(self, scrape_item: ScrapeItem) -> None:
+    async def video_from_api(self, scrape_item: ScrapeItem):
+        video_id: str = scrape_item.url.name.rsplit("_", 1)[-1]
+        data = {"video": video_id, "mode": "InitVideo", "view": True}
+        async with self.request_limiter:
+            json_resp: dict = await self.client.post_data(self.domain, API_ENTRYPOINT, data=data)
+
+        video_info: dict | None = json_resp.get("video") or {}
+        await self.process_video_info(scrape_item, video_info)
+
+    @error_handling_wrapper
+    async def video_from_soup(self, scrape_item: ScrapeItem) -> None:
         if await self.check_complete_from_referer(scrape_item):
             return
 
         async with self.request_limiter:
-            soup: BeautifulSoup = await self.client.get_soup(self.domain, scrape_item.url, origin=scrape_item)
+            soup: BeautifulSoup = await self.client.get_soup(self.domain, scrape_item.url)
 
-        video_info = get_video_info(soup)
+        video_info = get_video_info_from_js(soup)
+        await self.process_video_info(scrape_item, video_info)
+
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    @error_handling_wrapper
+    async def process_video_info(self, scrape_item: ScrapeItem, video_info: dict) -> None:
+        log_debug(json.dumps(video_info, indent=4))
         link_str: str = video_info.get("url") or ""
         if not link_str:
             raise ScrapeError(422, message="No video source found")
@@ -61,8 +212,39 @@ class PMVHavenCrawler(Crawler):
         custom_filename, _ = get_filename_and_ext(f"{title} [{video_id}][{resolution}]{ext}")
         await self.handle_file(link, scrape_item, filename, ext, custom_filename=custom_filename)
 
+    async def iter_videos(self, scrape_item: ScrapeItem, videos: list[dict]) -> None:
+        for video in videos:
+            link = create_canonical_video_url(video)
+            new_scrape_item = scrape_item.create_child(link)
+            await self.process_video_info(new_scrape_item, video)
+            scrape_item.add_children()
 
-def get_video_info(soup: BeautifulSoup) -> dict:
+    async def api_pager(self, api_url: URL, add_data: dict, *, check_key: str = "data") -> AsyncGenerator[dict]:
+        """Generator of API pages."""
+        page: int = 1
+        while True:
+            data = {"index": page} | add_data
+            async with self.request_limiter:
+                json_resp: dict = await self.client.post_data(self.domain, api_url, data=data)
+
+            has_videos = bool(json_resp[check_key])
+            if not has_videos:
+                break
+            yield json_resp
+            page += 1
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+def create_canonical_video_url(video_info: dict) -> URL:
+    title: str = video_info.get("title") or video_info["uploadTitle"]
+    video_id: str = video_info["_id"]
+    path = f"{title}_{video_id}"
+    return PRIMARY_BASE_DOMAIN / "video" / path
+
+
+def get_video_info_from_js(soup: BeautifulSoup) -> dict:
     info_js_script = soup.select_one(JS_VIDEO_INFO_SELECTOR)
     js_text = info_js_script.text if info_js_script else None
     if not js_text:
@@ -78,8 +260,6 @@ def get_video_info(soup: BeautifulSoup) -> dict:
             break
     for name, index in indices.items():
         video_properties[name] = info_dict["data"][index]
-
-    log_debug(json.dumps(video_properties, indent=4))
     return video_properties
 
 
