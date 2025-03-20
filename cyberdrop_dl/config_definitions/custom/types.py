4a0c76614d51c035982a3a1f6e6a332e3bfc2c7b
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2025-03-20 13:12:59 -0500

    refactor: update types

--- cyberdrop_dl/config_definitions/custom/types.py
+++ cyberdrop_dl/config_definitions/custom/types.py
@@ -12,7 +12,6 @@ from pydantic import (
     BeforeValidator,
     ByteSize,
     ConfigDict,
-    HttpUrl,
     NonNegativeInt,
     PlainSerializer,
     PlainValidator,
@@ -22,39 +21,32 @@ from pydantic import (
     model_serializer,
     model_validator,
 )
+from pydantic import HttpUrl as PydanticHttpUrl
 
 from .converters import change_path_suffix, convert_byte_size_to_str, convert_to_yarl
 from .validators import parse_apprise_url, parse_falsy_as_none, parse_list
 
+# ~~~~~ Strings ~~~~~~~
 StrSerializer = PlainSerializer(str, return_type=str, when_used="json-unless-none")
-
-
-ByteSizeSerilized = Annotated[ByteSize, PlainSerializer(convert_byte_size_to_str, return_type=str)]
-HttpURL = Annotated[HttpUrl, AfterValidator(convert_to_yarl), StrSerializer]
-ListNonNegativeInt = Annotated[list[NonNegativeInt], BeforeValidator(parse_list)]
-
 NonEmptyStr = Annotated[str, StringConstraints(min_length=1, strip_whitespace=True)]
 NonEmptyStrOrNone = Annotated[NonEmptyStr | None, BeforeValidator(parse_falsy_as_none)]
 ListNonEmptyStr = Annotated[list[NonEmptyStr], BeforeValidator(parse_list)]
 
+# ~~~~~ Paths ~~~~~~~
 PathOrNone = Annotated[Path | None, BeforeValidator(parse_falsy_as_none)]
 LogPath = Annotated[Path, AfterValidator(partial(change_path_suffix, suffix=".csv"))]
 MainLogPath = Annotated[LogPath, AfterValidator(partial(change_path_suffix, suffix=".log"))]
 
+# ~~~~~ URLs ~~~~~~~
+URLSerilized = Annotated[yarl.URL, StrSerializer]
+HttpStr = Annotated[URLSerilized, PlainValidator(str)]  # URL in type hints, str at runtime
+HttpStrURL = Annotated[HttpStr, AfterValidator(convert_to_yarl)]  # URL with str validation
+HttpUrl = Annotated[URLSerilized, PlainValidator(PydanticHttpUrl)]  # URL in type hints, pydantic.HttpUrl at runtime
+HttpURL = Annotated[HttpUrl, AfterValidator(convert_to_yarl)]  # URL with pydantic.HttpUrl validation
 
-YarlURLSerilized = Annotated[yarl.URL, StrSerializer]
-
-# A yarl.URL in type hints that is actually a str at runtime, to use as base for custom URL parsers
-CustomHttpStrURL = Annotated[YarlURLSerilized, PlainValidator(str)]
-
-# A yarl.URL in type hints that is actually a pydantic.HttpUrl at runtime
-CustomHttpURL = Annotated[YarlURLSerilized, PlainValidator(HttpUrl)]
-
-# An actual yarl.URL in type hints and runtime but uses str for validation.
-ParsedHttpStrURL = Annotated[CustomHttpStrURL, AfterValidator(convert_to_yarl)]
-
-# An actual yarl.URL in type hints and runtime but uses pydantic.HttpUrl for validation.
-ParsedHttpURL = Annotated[CustomHttpURL, AfterValidator(convert_to_yarl)]
+# ~~~~~ Others ~~~~~~~
+ByteSizeSerilized = Annotated[ByteSize, PlainSerializer(convert_byte_size_to_str, return_type=str)]
+ListNonNegativeInt = Annotated[list[NonNegativeInt], BeforeValidator(parse_list)]
 
 
 class AliasModel(BaseModel):
@@ -67,7 +59,7 @@ class FrozenModel(BaseModel):
 
 class AppriseURLModel(FrozenModel):
     url: Secret[AnyUrl]
-    tags: set[str]
+    tags: set[str] = set()
 
     @model_serializer()
     def serialize(self, info: SerializationInfo):
@@ -85,3 +77,7 @@ class AppriseURLModel(FrozenModel):
 
 class HttpAppriseURL(AppriseURLModel):
     url: Secret[HttpURL]
+
+
+# DEPRECATED
+# HttpURL = Annotated[HttpUrl, AfterValidator(convert_to_yarl), StrSerializer]
