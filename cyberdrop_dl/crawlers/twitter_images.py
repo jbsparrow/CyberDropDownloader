265077398eb80bb9d0c8240bf64eea2240fdb71c
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2025-03-23 23:07:56 -0500

    refactor: try to download images from any url (twing)

--- cyberdrop_dl/crawlers/twitter_images.py
+++ cyberdrop_dl/crawlers/twitter_images.py
@@ -5,16 +5,18 @@ from typing import TYPE_CHECKING
 from yarl import URL
 
 from cyberdrop_dl.crawlers.crawler import Crawler, create_task_id
-from cyberdrop_dl.utils.utilities import get_filename_and_ext
 
 if TYPE_CHECKING:
     from cyberdrop_dl.managers.manager import Manager
     from cyberdrop_dl.utils.data_enums_classes.url_objects import ScrapeItem
 
 
+CDN_HOST = "pbs.twimg.com"
+PRIMARY_BASE_DOMAIN = URL("https://twimg.com/")
+
+
 class TwimgCrawler(Crawler):
-    primary_base_domain = URL("https://twimg.com/")
-    cdn_base_domain = URL("https://pbs.twimg.com/")
+    primary_base_domain = PRIMARY_BASE_DOMAIN
 
     def __init__(self, manager: Manager) -> None:
         super().__init__(manager, "twimg", "TwitterImages")
@@ -24,8 +26,6 @@ class TwimgCrawler(Crawler):
     @create_task_id
     async def fetch(self, scrape_item: ScrapeItem) -> None:
         """Determines where to send the scrape item based on the url."""
-        if "media" not in scrape_item.url.parts:
-            raise ValueError
         await self.photo(scrape_item)
 
     async def photo(self, scrape_item: ScrapeItem) -> None:
@@ -33,7 +33,7 @@ class TwimgCrawler(Crawler):
 
         See: https://developer.x.com/en/docs/x-api/v1/data-dictionary/object-model/entities#photo_format
         """
-        scrape_item.url = scrape_item.url.with_host(self.cdn_base_domain.host).with_query(None)
+        scrape_item.url = scrape_item.url.with_host(CDN_HOST)
         link = scrape_item.url.with_query(format="jpg", name="large")
-        filename, ext = get_filename_and_ext(f"{link.name}.jpg")
+        filename, ext = self.get_filename_and_ext(link.name, assume_ext=".jpg")
         await self.handle_file(link, scrape_item, filename, ext)
