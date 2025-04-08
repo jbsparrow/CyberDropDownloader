4142927580886bb339b442f23b67903cf42a0c65
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2025-04-08 18:04:30 -0500

    refactor: fix formatting of log messages

--- cyberdrop_dl/managers/storage_manager.py
+++ cyberdrop_dl/managers/storage_manager.py
@@ -1,12 +1,13 @@
 from __future__ import annotations
 
 import asyncio
+import itertools
 from collections import defaultdict
 from datetime import timedelta
 from functools import lru_cache
 from pathlib import Path
 from types import MappingProxyType
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Any
 
 import psutil
 from psutil._common import sdiskpart
@@ -37,11 +38,11 @@ class StorageManager:
         self._nt_network_drives: dict[Path, sdiskpart] = {}
         self._loop = asyncio.create_task(self._check_free_space_loop())
 
-    def get_used_mounts_stats(self, simplified: bool = True) -> dict:
+    def get_used_mounts_stats(self, simplified: bool = True) -> dict[Path, dict[str, Any]] | str:
         """Returns a dict with the infomartion + free space of every used mount.
 
         If simplified is `True` (the default), all the information is flatten as a single string and mounts are converted to `str` (for logging)"""
-        mounts = {}
+        mounts: dict[Path, dict[str, Any]] = {}
         partitions = get_available_partitions() | self._nt_network_drives
         for mount in self._used_mounts:
             free_space = ByteSize(self._free_space[mount])
@@ -52,7 +53,7 @@ class StorageManager:
             mounts[mount] = data
 
         if simplified:
-            return {str(key): str(value) for key, value in mounts.items()}
+            return as_text({key: as_text(value, "", indent=1) for key, value in mounts.items()})
         return mounts
 
     async def check_free_space(self, media_item: MediaItem) -> None:
@@ -91,16 +92,18 @@ class StorageManager:
             pass
 
     async def _has_sufficient_space(self, folder: Path) -> bool:
-        """Checks if there is enough free space to download to this folder"""
+        """Checks if there is enough free space to download to this folder
+
+        `folder` should be an absolute path"""
 
         async def nt_network_drive_check():
             """Calls is_network_drive to cache the result and prevent blocking on future calls"""
             if not psutil.WINDOWS:
                 return
 
-            folder_drive = Path(folder.drive)
+            folder_drive = drive_as_path(folder.drive)
             async with self._mount_addition_locks[folder_drive]:
-                if folder_drive in self._nt_network_drives:
+                if folder_drive in itertools.chain(self._nt_network_drives, get_available_mountpoints()):
                     return
                 if await asyncio.to_thread(is_network_drive, folder.drive):
                     nw_drive = sdiskpart(folder.drive, folder.drive, "network_drive", "")
@@ -117,8 +120,8 @@ class StorageManager:
                 result = await asyncio.to_thread(psutil.disk_usage, str(mount))
                 self._free_space[mount] = result.free
                 self._used_mounts.add(mount)
-                log(f"A new mountpoint ({mount}) will be used for '{folder}'")
-                log({"Storage status": self.get_used_mounts_stats()})
+                log(f"A new mountpoint ('{mount!s}') will be used for '{folder}'")
+                log(f"Storage status:\n {self.get_used_mounts_stats()}")
 
         return self._free_space[mount] > self.manager.config_manager.global_settings_data.general.required_free_space
 
@@ -141,7 +144,7 @@ class StorageManager:
                 for mount, result in zip(used_mounts, results, strict=True):
                     self._free_space[mount] = result.free
                 if last_check % self._log_period == 0:
-                    log_debug({"Storage status": self.get_used_mounts_stats()})
+                    log_debug(f"Storage status:\n {self.get_used_mounts_stats()}")
             self._updated.set()
             await asyncio.sleep(self._period)
 
@@ -154,17 +157,18 @@ def get_mount_point(folder: Path) -> Path | None:
     assert folder.is_absolute()
     mounts = get_available_mountpoints()
     possible_mountpoints = [mount for mount in mounts if mount in folder.parents or mount == folder]
+    folder_drive = drive_as_path(folder.drive)
     if not possible_mountpoints and psutil.WINDOWS:
         # path may be a network share. See: https://github.com/jbsparrow/CyberDropDownloader/issues/860
         # is_network_drive MUST be pre-cached to prevent blocking
         if is_network_drive(folder.drive):
-            possible_mountpoints = [Path(folder.drive)]
+            possible_mountpoints = [folder_drive]
 
     if not possible_mountpoints:
         # Mount point for this path does not exists
         # This will only happend on Windows, ex: an USB drive (`D:`) that is not currently available (AKA disconnected)
         # On Unix there's always at least 1 mountpoint, root (`/`)
-        msg = f"No available mountpoint found for '{folder}'\n -> {folder.drive = } - {folder.drive = } - {folder.parents[-1] = }"
+        msg = f"No available mountpoint found for '{folder}'\n -> drive = '{folder_drive}' , last_parent = '{folder.parents[-1]}'"
         log(msg, 40)
         return
 
@@ -179,10 +183,9 @@ def get_mount_point(folder: Path) -> Path | None:
 @lru_cache
 def get_available_partitions() -> MappingProxyType[Path, sdiskpart]:
     """NOTE: This function is cached which means it always returns the partitions available at startup"""
-
     # all=True is required to make sure it works on most platforms. See: https://github.com/giampaolo/psutil/issues/2191
-    partitions = psutil.disk_partitions(all=True)
-    return MappingProxyType({Path(p.mountpoint): p for p in partitions})
+    # Calling resolve on a mapped network drive returns its UNC path
+    return MappingProxyType({Path(p.mountpoint).resolve(): p for p in psutil.disk_partitions(all=True)})
 
 
 @lru_cache
@@ -192,8 +195,8 @@ def get_available_mountpoints() -> tuple[Path, ...]:
 
 
 @lru_cache
-def is_network_drive(path_drive: str) -> bool:
-    """Determines if the drive is a Windows network drive (UNC or mapped drive) and exists.
+def is_network_drive(drive: str) -> bool:
+    """Determines if the drive is a Windows network drive (UNC or unknown mapped drive) and exists.
 
     `path_drive` MUST be the output of `pathlib.Path.drive` and the original path instance MUST be absolute
 
@@ -201,14 +204,34 @@ def is_network_drive(path_drive: str) -> bool:
     if not psutil.WINDOWS:
         return False
 
-    known_mounts = [p.mountpoint for p in get_available_partitions().values()]
-    is_unc_path = path_drive.startswith("\\\\")
-    is_mapped_drive = ":" in path_drive
-    is_unknown = is_mapped_drive and f"{path_drive.upper()}\\" not in known_mounts
+    # In reality, a network drive be a mapped drive because all paths are resolved at startup
+    # Calling resolve on a mapped network drive returns its UNC path
+    drive_path = drive_as_path(drive)
+    is_unc_path = drive.startswith("\\\\")
+    known_mounts = get_available_partitions().keys()
+    is_unknown = drive_path not in known_mounts
+    known_mounts = ", ".join(f"'{mount}'" for mount in known_mounts)
 
-    msg = f"checking new possible network_drive: {path_drive = } {is_unc_path  = } {is_unknown} {known_mounts!s}"
+    msg = f"Checking new possible network_drive: \n{as_text(locals(), quote='')}"
     log_debug(msg)
+
     if is_unc_path or is_unknown:
-        return Path(path_drive).is_dir()
+        return drive_path.is_dir()
 
     return False
+
+
+def as_text(a_dict: dict[Any, Any], sep: str = "\n", quote: str = "'", indent: int = 4) -> str:
+    """Custom function to dump simple dicts (1 level deep, no dict or list as values) as a string.
+
+    Reason: using `json.dumps` changes the paths separator on Windows becuase it will always try to create a valid json"""
+    text = ""
+    padding = " " * indent
+    for key, value in a_dict.items():
+        text += f"{padding}{quote}{key!s}{quote}: {quote}{value!s}{quote}{sep}"
+    return text
+
+
+def drive_as_path(drive: str) -> Path:
+    is_mapped_drive = ":" in drive and len(drive) == 2
+    return Path(f"{drive}/" if is_mapped_drive else drive)
