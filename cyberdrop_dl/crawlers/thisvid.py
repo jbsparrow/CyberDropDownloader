dfe990d8eec129b7f29705376bc6a26369859c72
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2025-04-09 18:01:55 -0500

    refactor: user `yarl.URL` instead of `urllib`

--- cyberdrop_dl/crawlers/thisvid.py
+++ cyberdrop_dl/crawlers/thisvid.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
 
 import re
-import urllib
 from typing import TYPE_CHECKING, NamedTuple
 
 from aiolimiter import AsyncLimiter
@@ -9,7 +8,7 @@ from yarl import URL
 
 from cyberdrop_dl.clients.errors import ScrapeError
 from cyberdrop_dl.crawlers.crawler import Crawler, create_task_id
-from cyberdrop_dl.utils.utilities import error_handling_wrapper, get_filename_and_ext
+from cyberdrop_dl.utils.utilities import error_handling_wrapper
 
 if TYPE_CHECKING:
     from bs4 import BeautifulSoup
@@ -17,7 +16,7 @@ if TYPE_CHECKING:
     from cyberdrop_dl.managers.manager import Manager
     from cyberdrop_dl.utils.data_enums_classes.url_objects import ScrapeItem
 
-
+# Selectors
 UNAUTHORIZED_SELECTOR = "div.video-holder:contains('This video is a private video')"
 JS_SELECTOR = "div.video-holder > script:contains('var flashvars')"
 USER_NAME_SELECTOR = "div.headline > h2"
@@ -26,6 +25,8 @@ PRIVATE_VIDEOS_SELECTOR = "div#list_videos_private_videos_items"
 FAVOURITE_VIDEOS_SELECTOR = "div#list_videos_favourite_videos_items"
 COMMON_VIDEOS_TITLE_SELECTOR = "div#list_videos_common_videos_list"
 VIDEOS_SELECTOR = "a.tumbpu"
+
+# Regex
 VIDEO_RESOLUTION_PATTERN = re.compile(r"video_url_text:\s*'([^']+)'")
 VIDEO_INFO_PATTTERN = re.compile(
     r"video_id:\s*'(?P<video_id>[^']+)'[^}]*?"
@@ -33,10 +34,12 @@ VIDEO_INFO_PATTTERN = re.compile(
     r"video_url:\s*'(?P<video_url>[^']+)'[^}]*?"
 )
 
+HASH_LENGTH = 32
+
 
 class Video(NamedTuple):
     id: str
-    url: str
+    url: URL
     res: str
 
 
@@ -113,15 +116,14 @@ class ThisVidCrawler(Crawler):
         if soup.select_one(UNAUTHORIZED_SELECTOR):
             raise ScrapeError(401)
         script = soup.select_one(JS_SELECTOR)
-        if script is None:
+        if not script:
             raise ScrapeError(404)
 
         video = get_video_info(script.text)
-        link = self.parse_url(video.url)
         title: str = soup.select_one("title").text.split("- ThisVid.com")[0].strip()  # type: ignore
-        filename, ext = self.get_filename_and_ext(link.name)
-        custom_filename, _ = get_filename_and_ext(f"{title} [{video.id}] [{video.res}]{ext}")
-        await self.handle_file(link, scrape_item, filename, ext, custom_filename=custom_filename)
+        filename, ext = self.get_filename_and_ext(video.url.name)
+        custom_filename, _ = self.get_filename_and_ext(f"{title} [{video.id}] [{video.res}]{ext}")
+        await self.handle_file(video.url, scrape_item, filename, ext, custom_filename=custom_filename)
 
 
 """~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"""
@@ -131,24 +133,25 @@ def get_video_info(flashvars: str) -> Video:
     if (match_id := VIDEO_INFO_PATTTERN.search(flashvars)) and (
         match_res := VIDEO_RESOLUTION_PATTERN.search(flashvars)
     ):
-        video_id = match_id.group("video_id")
-        video_url = kvs_get_real_url(match_id.group("video_url"), match_id.group("license_code"))
-        video_res = match_res.group(1)
-        return Video(video_id, video_url, video_res)
+        id, license_code, url = match_id.groups()
+        real_url = kvs_get_real_url(url, license_code)
+        resolution = match_res.group(1)
+        return Video(id, real_url, resolution)
     raise ScrapeError(404)
 
 
 # URL de-obfuscation code, borrowed from yt-dlp
 # https://github.com/yt-dlp/yt-dlp/blob/e1847535e28788414a25546a45bebcada2f34558/yt_dlp/extractor/generic.py
-def kvs_get_license_token(license_code: str):
-    license_code = license_code.replace("$", "")
-    license_values = [int(char) for char in license_code]
 
+
+def kvs_get_license_token(license_code: str) -> list[int]:
+    license_code = license_code.removeprefix("$")
+    license_values = [int(char) for char in license_code]
     modlicense = license_code.replace("0", "1")
-    center = len(modlicense) // 2
-    fronthalf = int(modlicense[: center + 1])
-    backhalf = int(modlicense[center:])
-    modlicense = str(4 * abs(fronthalf - backhalf))[: center + 1]
+    middle = len(modlicense) // 2
+    fronthalf = int(modlicense[: middle + 1])
+    backhalf = int(modlicense[middle:])
+    modlicense = str(4 * abs(fronthalf - backhalf))[: middle + 1]
 
     return [
         (license_values[index + offset] + current) % 10
@@ -157,16 +160,13 @@ def kvs_get_license_token(license_code: str):
     ]
 
 
-def kvs_get_real_url(video_url: str, license_code: str) -> str:
+def kvs_get_real_url(video_url: str, license_code: str) -> URL:
     if not video_url.startswith("function/0/"):
-        return video_url  # not obfuscated
+        return URL(video_url)  # not obfuscated
 
-    parsed = urllib.parse.urlparse(video_url[len("function/0/") :])
+    parsed_url = URL(video_url.removeprefix("function/0/"))
     license_token = kvs_get_license_token(license_code)
-    urlparts = parsed.path.split("/")
-
-    HASH_LENGTH = 32
-    hash_ = urlparts[3][:HASH_LENGTH]
+    hash, tail = parsed_url.parts[3][:HASH_LENGTH], parsed_url.parts[3][HASH_LENGTH:]
     indices = list(range(HASH_LENGTH))
 
     # Swap indices of hash according to the destination calculated from the license token
@@ -176,5 +176,8 @@ def kvs_get_real_url(video_url: str, license_code: str) -> str:
         dest = (src + accum) % HASH_LENGTH
         indices[src], indices[dest] = indices[dest], indices[src]
 
-    urlparts[3] = "".join(hash_[index] for index in indices) + urlparts[3][HASH_LENGTH:]
-    return urllib.parse.urlunparse(parsed._replace(path="/".join(urlparts)))
+    new_parts = list(parsed_url.parts)
+    if not parsed_url.name:
+        new_parts.pop(-1)
+    new_parts[3] = "".join(hash[index] for index in indices) + tail
+    return parsed_url.with_path("/".join(new_parts[1:]), keep_query=True, keep_fragment=True)
