60953222c947c7248c5afdd2ae9d67537379a0c1
Barbarella <barbarella6666666@protonmail.com>
2025-04-09 08:26:32 -0300

    refactor: extract only the information needed
    We don't need to use js_to_json

--- cyberdrop_dl/crawlers/thisvid.py
+++ cyberdrop_dl/crawlers/thisvid.py
@@ -1,7 +1,5 @@
 from __future__ import annotations
 
-import contextlib
-import json
 import re
 import urllib
 from typing import TYPE_CHECKING
@@ -31,6 +29,12 @@ FAVOURITE_VIDEOS_SELECTOR = "div#list_videos_favourite_videos_items"
 COMMON_VIDEOS_TITLE_SELECTOR = "div#list_videos_common_videos_list"
 NEXT_PAGE_SELECTOR = "li.pagination-next > a"
 VIDEOS_SELECTOR = "a.tumbpu"
+VIDEO_RESOLUTION_PATTERN = re.compile(r"video_url_text:\s*'([^']+)'")
+VIDEO_INFO_PATTTERN = re.compile(
+    r"video_id:\s*'(?P<video_id>[^']+)'[^}]*?"
+    r"license_code:\s*'(?P<license_code>[^']+)'[^}]*?"
+    r"video_url:\s*'(?P<video_url>[^']+)'[^}]*?",
+)
 
 
 class ThisVidCrawler(Crawler):
@@ -122,20 +126,32 @@ class ThisVidCrawler(Crawler):
         script = soup.select_one(JS_SELECTOR)
         if script is None:
             raise ScrapeError(404, origin=scrape_item)
-        flashvars: str = script.text[script.text.find("var flashvars") + 16 : script.text.find("kt_player")]
-        flashvars = flashvars[: flashvars.rfind(";")]
-        best_fmt = get_best_resolution(str(scrape_item.url), js_to_json(flashvars))
+        video_info = get_video_info(script.text)
+        if "video_url" not in video_info:
+            raise ScrapeError(404, origin=scrape_item)
         title: str = soup.select_one("title").text.split("- ThisVid.com")[0].strip()
-        filename, ext = get_filename_and_ext(best_fmt["url"])
-        video_url: URL = URL(best_fmt["url"])
-        custom_filename, _ = get_filename_and_ext(f"{title} [{video_url.parts[-3]}] [{best_fmt['format_id']}].{ext}")
+        filename, ext = get_filename_and_ext(video_info["video_url"])
+        video_url: URL = URL(video_info["video_url"])
+        custom_filename, _ = get_filename_and_ext(
+            f"{title} [{video_url.parts[-3]}] [{video_info['video_url_text']}].{ext}"
+        )
         await self.handle_file(video_url, scrape_item, filename, ext, custom_filename=custom_filename)
 
 
 """~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"""
 
 
-# Code borrowed from yt-dlp
+def get_video_info(flashvars: str) -> dict:
+    info = {"video_url_text": "Unknown"}
+    if m := VIDEO_INFO_PATTTERN.search(flashvars):
+        info["video_id"] = m.group("video_id")
+        info["video_url"] = kvs_get_real_url(m.group("video_url"), m.group("license_code"))
+    if m := VIDEO_RESOLUTION_PATTERN.search(flashvars):
+        info["video_url_text"] = m.group(1)
+    return info
+
+
+# URL de-obfuscation code, borrowed from yt-dlp
 # https://github.com/yt-dlp/yt-dlp/blob/e1847535e28788414a25546a45bebcada2f34558/yt_dlp/extractor/generic.py
 def kvs_get_license_token(license_code):
     license_code = license_code.replace("$", "")
@@ -175,155 +191,3 @@ def kvs_get_real_url(video_url, license_code):
 
     urlparts[3] = "".join(hash_[index] for index in indices) + urlparts[3][HASH_LENGTH:]
     return urllib.parse.urlunparse(parsed._replace(path="/".join(urlparts)))
-
-
-def parse_resolution(s, *, lenient=False):
-    if s is None:
-        return {}
-
-    if lenient:
-        mobj = re.search(r"(?P<w>\d+)\s*[xX×,]\s*(?P<h>\d+)", s)  # noqa: RUF001
-    else:
-        mobj = re.search(r"(?<![a-zA-Z0-9])(?P<w>\d+)\s*[xX×,]\s*(?P<h>\d+)(?![a-zA-Z0-9])", s)  # noqa: RUF001
-    if mobj:
-        return {
-            "width": int(mobj.group("w")),
-            "height": int(mobj.group("h")),
-        }
-
-    mobj = re.search(r"(?<![a-zA-Z0-9])(\d+)[pPiI](?![a-zA-Z0-9])", s)
-    if mobj:
-        return {"height": int(mobj.group(1))}
-
-    mobj = re.search(r"\b([48])[kK]\b", s)
-    if mobj:
-        return {"height": int(mobj.group(1)) * 540}
-
-    return {}
-
-
-def get_best_resolution(url, flashvars):
-    url_keys = list(filter(re.compile(r"^video_(?:url|alt_url\d*)$").match, flashvars.keys()))
-    formats = []
-    for key in url_keys:
-        if "/get_file/" not in flashvars[key]:
-            continue
-        format_id = flashvars.get(f"{key}_text", key)
-        formats.append(
-            {
-                "url": urllib.parse.urljoin(url, kvs_get_real_url(flashvars[key], flashvars["license_code"])),
-                "format_id": format_id,
-                "ext": "mp4",
-                **(parse_resolution(format_id) or parse_resolution(flashvars[key])),
-            }
-        )
-        if not formats[-1].get("height"):
-            formats[-1]["quality"] = 1
-    higher_res: int = 0
-    best_fmt: dict = None
-    for fmt in formats:
-        if fmt["format_id"] == "HQ":
-            return fmt
-        elif fmt["format_id"] == "video_url":
-            fmt["format_id"] = "Unknown"
-            return fmt
-        else:
-            res = int(fmt["format_id"].strip("p"))
-            if res > higher_res:
-                higher_res = res
-                best_fmt = fmt
-
-    return best_fmt
-
-
-def js_to_json(code, vars={}, *, strict=False):  # noqa: B006
-    # vars is a dict of var, val pairs to substitute
-    STRING_QUOTES = "'\"`"
-    STRING_RE = "|".join(rf"{q}(?:\\.|[^\\{q}])*{q}" for q in STRING_QUOTES)
-    COMMENT_RE = r"/\*(?:(?!\*/).)*?\*/|//[^\n]*\n"
-    SKIP_RE = rf"\s*(?:{COMMENT_RE})?\s*"
-    INTEGER_TABLE = (
-        (rf"(?s)^(0[xX][0-9a-fA-F]+){SKIP_RE}:?$", 16),
-        (rf"(?s)^(0+[0-7]+){SKIP_RE}:?$", 8),
-    )
-
-    def process_escape(match):
-        JSON_PASSTHROUGH_ESCAPES = R'"\bfnrtu'
-        escape = match.group(1) or match.group(2)
-
-        return (
-            Rf"\{escape}"
-            if escape in JSON_PASSTHROUGH_ESCAPES
-            else R"\u00"
-            if escape == "x"
-            else ""
-            if escape == "\n"
-            else escape
-        )
-
-    def template_substitute(match):
-        evaluated = js_to_json(match.group(1), vars, strict=strict)
-        if evaluated[0] == '"':
-            with contextlib.suppress(json.JSONDecodeError):
-                return json.loads(evaluated)
-        return evaluated
-
-    def fix_kv(m):
-        v = m.group(0)
-        if v in ("true", "false", "null"):
-            return v
-        elif v in ("undefined", "void 0"):
-            return "null"
-        elif v.startswith(("/*", "//", "!")) or v == ",":
-            return ""
-
-        if v[0] in STRING_QUOTES:
-            v = re.sub(r"(?s)\${([^}]+)}", template_substitute, v[1:-1]) if v[0] == "`" else v[1:-1]
-            escaped = re.sub(r'(?s)(")|\\(.)', process_escape, v)
-            return f'"{escaped}"'
-
-        for regex, base in INTEGER_TABLE:
-            im = re.match(regex, v)
-            if im:
-                i = int(im.group(1), base)
-                return f'"{i}":' if v.endswith(":") else str(i)
-
-        if v in vars:
-            try:
-                if not strict:
-                    json.loads(vars[v])
-            except json.JSONDecodeError:
-                return json.dumps(vars[v])
-            else:
-                return vars[v]
-
-        if not strict:
-            return f'"{v}"'
-
-        raise ValueError(f"Unknown value: {v}")
-
-    def create_map(mobj):
-        return json.dumps(dict(json.loads(js_to_json(mobj.group(1) or "[]", vars=vars))))
-
-    code = re.sub(r"(?:new\s+)?Array\((.*?)\)", r"[\g<1>]", code)
-    code = re.sub(r"new Map\((\[.*?\])?\)", create_map, code)
-    if not strict:
-        code = re.sub(rf"new Date\(({STRING_RE})\)", r"\g<1>", code)
-        code = re.sub(r"new \w+\((.*?)\)", lambda m: json.dumps(m.group(0)), code)
-        code = re.sub(r"parseInt\([^\d]+(\d+)[^\d]+\)", r"\1", code)
-        code = re.sub(r'\(function\([^)]*\)\s*\{[^}]*\}\s*\)\s*\(\s*(["\'][^)]*["\'])\s*\)', r"\1", code)
-
-    return json.loads(
-        re.sub(
-            rf"""(?sx)
-        {STRING_RE}|
-        {COMMENT_RE}|,(?={SKIP_RE}[\]}}])|
-        void\s0|(?:(?<![0-9])[eE]|[a-df-zA-DF-Z_$])[.a-zA-Z_$0-9]*|
-        \b(?:0[xX][0-9a-fA-F]+|0+[0-7]+)(?:{SKIP_RE}:)?|
-        [0-9]+(?={SKIP_RE}:)|
-        !+
-        """,
-            fix_kv,
-            code,
-        )
-    )
