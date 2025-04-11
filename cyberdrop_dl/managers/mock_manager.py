4edaf8e132dfdc0f7196b1aa8cf3e52d696cc8a7
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2025-04-10 19:21:20 -0500

    refactor: define `MockCallable`

--- cyberdrop_dl/managers/mock_manager.py
+++ cyberdrop_dl/managers/mock_manager.py
@@ -1,14 +1,27 @@
+from __future__ import annotations
+
 from typing import Any
 
 MOCK_MANAGER = None
 
 
+class MockCallable:
+    def __init__(self, return_obj: Any = None) -> None:
+        self.return_obj = return_obj
+
+    def __getitem__(self, parameters: Any) -> object: ...
+    def __or__(self, other: Any) -> MockCallable: ...
+    def __ror__(self, other: Any) -> MockCallable: ...
+    def __call__(self, *args, **kwargs):
+        return self.return_obj
+
+
 class Mock(Any):
-    def __init__(self, name: str):
+    def __init__(self, name: str, /) -> None:
         self._nested_attrs: dict[str, Mock] = {}
         self._mock_name = name
 
-    def __getattribute__(self, name: str) -> Any:
+    def __getattribute__(self, name: str, /) -> Any:
         if name == "manager" and MOCK_MANAGER is not None:
             return MOCK_MANAGER
         try:
@@ -18,18 +31,11 @@ class Mock(Any):
                 raise  # Avoid infinite recursion
             return self._nested_attrs.get(name, Mock(name))
 
-    def __call__(self, *_, **__):
-        return self
-
 
 class MockCacheManager(Mock):
-    def __init__(self):
+    def __init__(self) -> None:
+        self.get = self.save = MockCallable()
         super().__init__("cache_manager")
-
-    def __getattribute__(self, name: str):
-        if name in ("get", "save"):
-            return lambda _: None
-        return super().__getattribute__(name)
 
 
 class MockManager(Mock):
