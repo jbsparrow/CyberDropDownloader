a30a0f33031080a6cf742f162d4555eada6d7487
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2025-04-11 18:54:27 -0500

    refactor:  remove unused vars

--- cyberdrop_dl/utils/cookie_management.py
+++ cyberdrop_dl/utils/cookie_management.py
@@ -20,15 +20,12 @@ class CookieExtractor(NamedTuple):
     extract: Callable[..., CookieJar]
 
 
-COOKIE_EXTRACTORS: list[CookieExtractor] = [
-    CookieExtractor(func.__name__, func) for func in browser_cookie3.all_browsers
-]
+class UnsupportedBrowserError(browser_cookie3.BrowserCookieError): ...
+
+
+COOKIE_EXTRACTORS = [CookieExtractor(func.__name__, func) for func in browser_cookie3.all_browsers]
 COOKIE_ERROR_FOOTER = "\n\nNothing has been saved."
 CHROMIUM_BROWSERS = ["chrome", "chromium", "opera", "opera_gx", "brave", "edge", "vivaldi", "arc"]
-
-
-class UnsupportedBrowserError(browser_cookie3.BrowserCookieError):
-    pass
 
 
 def cookie_wrapper(func: Callable) -> Callable:
@@ -36,13 +33,12 @@ def cookie_wrapper(func: Callable) -> Callable:
 
     @wraps(func)
     def wrapper(*args, **kwargs) -> None:
-        msg = ""
         try:
             return func(*args, **kwargs)
         except PermissionError as e:
             msg = """We've encountered a Permissions Error. Please close all browsers and try again
                      If you are still having issues, make sure all browsers processes are closed in Task Manager"""
-            msg = dedent(msg) + f"\nERROR: {e!s}"
+            msg = f"{dedent(msg)}\nERROR: {e!s}"
 
         except (ValueError, UnsupportedBrowserError) as e:
             msg = f"ERROR: {e!s}"
@@ -50,10 +46,9 @@ def cookie_wrapper(func: Callable) -> Callable:
         except browser_cookie3.BrowserCookieError as e:
             msg = """Browser extraction ran into an error, the selected browser(s) may not be available on your system
                      If you are still having issues, make sure all browsers processes are closed in Task Manager."""
+            msg = f"{dedent(msg)}\nERROR: {e!s}"
 
-            msg = dedent(msg) + f"\nERROR: {e!s}"
-
-        raise browser_cookie3.BrowserCookieError(msg + COOKIE_ERROR_FOOTER)
+        raise browser_cookie3.BrowserCookieError(f"{msg}{COOKIE_ERROR_FOOTER}")
 
     return wrapper
 
@@ -67,10 +62,7 @@ def get_cookies_from_browsers(
     :param browsers: list of browsers to extract from. If `None`, config `browser_cookies.browsers` will be used
     :param domains: list of domains to filter cookies. If `None`, config `browser_cookies.sites` will be used
     :return: A set with all the domains that actually had cookies
-    :raises ValueError: If `browsers` or `domains` are empty lists, or none of the provided browsers is supported for extraction
-    :raises UnsupportedBrowserError: If there was a decrypt error while extracting cookies from a chromium browser
-    and the current OS is Windows
-    :raises BrowserCookieError: For any other kind of error while extracting cookies"""
+    :raises BrowserCookieError: If there's any error while extracting cookies"""
     if browsers == []:
         msg = "No browser selected"
         raise ValueError(msg)
