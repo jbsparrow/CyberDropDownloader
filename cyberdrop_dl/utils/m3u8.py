db72f92c986c2fff2f916157b9250c871774103f
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2025-04-15 13:14:21 -0500

    refactor: add additional methods

--- cyberdrop_dl/utils/m3u8.py
+++ cyberdrop_dl/utils/m3u8.py
@@ -23,13 +23,28 @@ class HlsSegment(NamedTuple):
 
 class M3U8_Playlist:  # noqa: N801
     def __init__(self, content: str, base_url: URL | None = None) -> None:
-        self.base_url = base_url
+        self._base_url = base_url
         self._content = content
         self._lines = content.splitlines()
         self._suffix = ".cdl_hsl"
         self._segments: tuple[HlsSegment, ...] = ()
 
+    def new(self, base_url: URL | None = None):
+        """Creates a new playst with the same content but a new base_url"""
+        return M3U8_Playlist(self._content, base_url)
+
+    @property
+    def base_url(self) -> URL | None:
+        return self._base_url
+
+    @base_url.setter
+    def set_base_url(self, url: URL) -> None:
+        if self._segments:
+            raise ValueError("Cannot set base. Segments were already generated. Call new() instead")
+        self._base_url = url
+
     def _gen_segments(self) -> Generator[HlsSegment]:
+        """NOTE: Calling this function directly won't update the internal segments. Acces the `segments` attribute instead"""
         if self._segments:
             yield from self._segments
             return
@@ -48,8 +63,8 @@ class M3U8_Playlist:  # noqa: N801
                 yield part
 
         def parse(part: str) -> URL:
-            if self.base_url:
-                return self.base_url / part
+            if self._base_url:
+                return self._base_url / part
             return parse_url(part)
 
         last_segment_part = get_last_part()
@@ -60,13 +75,16 @@ class M3U8_Playlist:  # noqa: N801
             name = f"{index:0{padding}d}{self._suffix}"
             yield HlsSegment(part, name, url)
 
-    async def async_gen_segments(self) -> AsyncGenerator[HlsSegment]:
+    async def _async_gen_segments(self) -> AsyncGenerator[HlsSegment]:
+        """NOTE: Calling this function directly won't update the internal segments, use `get_segments` instead"""
         for segment in self._gen_segments():
             yield segment
             await asyncio.sleep(0)
 
     @staticmethod
     def _clean_line(line: str) -> str | None:
+        # This is the only method that may need to be updated in the future to be more robust
+        # All other methods are final
         stripped_line = line.strip()
         if stripped_line.startswith("#"):
             # Handle audio playlist references
@@ -87,5 +105,5 @@ class M3U8_Playlist:  # noqa: N801
 
     async def get_segments(self) -> tuple[HlsSegment, ...]:
         if not self._segments:
-            self._segments = tuple([seg async for seg in self.async_gen_segments()])
+            self._segments = tuple([seg async for seg in self._async_gen_segments()])
         return self._segments
