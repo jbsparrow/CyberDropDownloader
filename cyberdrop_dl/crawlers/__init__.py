5b912b7d0f56ee1fae31af8fc6c5e570fe5dbb63
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2025-04-26 19:24:17 -0500

    feat: add pkmncards.com support (#897)
    * feat: add pkmncards.com support
    
    * feat: add set support
    
    * feat: add series support
    
    * refactor: clean up code
    
    * refactor: download higher quality png
    
    * fix: card set
    
    * docs: fix docstrings
    
    * fix: sets and cards
    
    * fix: always create card set from soup
    
    * refactor: simplify simple card
    
    * refactor: fix series
    
    * fix: series
    
    * refactor: move simple card handling to its own method
    
    * refactor: re-order methods
    
    * refactor: remove duplicate selector
    
    * refactor: use selectors class

--- cyberdrop_dl/crawlers/__init__.py
+++ cyberdrop_dl/crawlers/__init__.py
@@ -52,6 +52,7 @@ from .onedrive import OneDriveCrawler
 from .pimpandhost import PimpAndHostCrawler
 from .pixeldrain import PixelDrainCrawler
 from .pixhost import PixHostCrawler
+from .pkmncards import PkmncardsCrawler
 from .pmvhaven import PMVHavenCrawler
 from .pornpics import PornPicsCrawler
 from .postimg import PostImgCrawler
