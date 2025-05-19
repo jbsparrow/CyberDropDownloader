3c84fdbc2d3cb176fe08924dd198c374c530fb72
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2025-05-18 21:57:02 -0500

    test: use pytest temp dir for tests

--- tests/test_startup.py
+++ tests/test_startup.py
@@ -1,29 +1,35 @@
 import sys
+from pathlib import Path
 
 import pytest
 
-from cyberdrop_dl.main import main
+from cyberdrop_dl.main import run
 from cyberdrop_dl.ui.program_ui import ProgramUI
 
 
-def test_startup(monkeypatch: pytest.MonkeyPatch, capsys: pytest.CaptureFixture[str]):
+@pytest.fixture(autouse=True)
+def test_dir(tmp_path: Path, monkeypatch: pytest.MonkeyPatch) -> None:
+    monkeypatch.chdir(tmp_path)
+
+
+def test_startup(monkeypatch: pytest.MonkeyPatch, capsys: pytest.CaptureFixture[str]) -> None:
     # This is just to test that cyberdrop is able to run in the current python version
     msg = "main UI started successfully"
 
-    def main_ui(*_):
+    def main_ui(*_) -> None:
         print(msg)
 
     monkeypatch.setattr(sys, "argv", ["pytest", "--disable-cache"])
     monkeypatch.setattr(ProgramUI, "__init__", main_ui)
-    main(profiling=True, ask=False)
+    run()
     captured = capsys.readouterr()
     output = captured.out
     assert msg in output
 
 
-def test_async_startup(monkeypatch: pytest.MonkeyPatch, capsys: pytest.CaptureFixture[str]):
+def test_async_startup(monkeypatch: pytest.MonkeyPatch, capsys: pytest.CaptureFixture[str]) -> None:
     monkeypatch.setattr(sys, "argv", ["pytest", "--download"])
-    main(profiling=True, ask=False)
+    run()
     captured = capsys.readouterr()
     output = captured.out
     assert "Finished downloading. Enjoy :)" in output
