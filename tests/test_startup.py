4fa0edccfc7710e7a0f0612d68c5ad29472bce94
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2025-05-19 14:23:06 -0500

    test: add tmp_path to global fixtures

--- tests/test_startup.py
+++ tests/test_startup.py
@@ -1,26 +1,17 @@
-import sys
-from pathlib import Path
-
 import pytest
 
 from cyberdrop_dl.main import run
 from cyberdrop_dl.ui.program_ui import ProgramUI
 
 
-@pytest.fixture(autouse=True)
-def test_dir(tmp_path: Path, monkeypatch: pytest.MonkeyPatch) -> None:
-    monkeypatch.chdir(tmp_path)
-
-
-def test_startup(tmp_path: Path, monkeypatch: pytest.MonkeyPatch, capsys: pytest.CaptureFixture[str]) -> None:
+def test_startup(monkeypatch: pytest.MonkeyPatch, capsys: pytest.CaptureFixture[str]) -> None:
     # This is just to test that cyberdrop is able to run in the current python version
     msg = "main UI started successfully"
 
     def main_ui(*_) -> None:
         print(msg)
 
-    monkeypatch.setattr(sys, "argv", ["pytest", "--disable-cache"])
-    monkeypatch.chdir(tmp_path)
+    monkeypatch.setattr("sys.argv", ["pytest", "--disable-cache"])
     monkeypatch.setattr(ProgramUI, "__init__", main_ui)
     run()
     captured = capsys.readouterr()
@@ -28,9 +19,8 @@ def test_startup(tmp_path: Path, monkeypatch: pytest.MonkeyPatch, capsys: pytest
     assert msg in output
 
 
-def test_async_startup(tmp_path: Path, monkeypatch: pytest.MonkeyPatch, capsys: pytest.CaptureFixture[str]) -> None:
-    monkeypatch.setattr(sys, "argv", ["pytest", "--download"])
-    monkeypatch.chdir(tmp_path)
+def test_async_startup(monkeypatch: pytest.MonkeyPatch, capsys: pytest.CaptureFixture[str]) -> None:
+    monkeypatch.setattr("sys.argv", ["pytest", "--download"])
     run()
     captured = capsys.readouterr()
     output = captured.out
