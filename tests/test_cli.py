79627b3a444b57f9e36cb9c78ac19619334b0913
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2025-05-19 14:25:44 -0500

    test: update `test_help`

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -5,10 +5,13 @@ import pytest
 from cyberdrop_dl.main import run
 
 
-def test_help(monkeypatch: pytest.MonkeyPatch, capsys: pytest.CaptureFixture[str]):
+def test_help(monkeypatch: pytest.MonkeyPatch, capsys: pytest.CaptureFixture[str]) -> None:
     # This is just to test that cyberdrop is able to run in the current python version
     monkeypatch.setattr(sys, "argv", ["pytest", "--help"])
-    run()
+    try:
+        run()
+    except SystemExit:
+        pass
     captured = capsys.readouterr()
     output = captured.out
     assert "Bulk asynchronous downloader for multiple file hosts" in output
