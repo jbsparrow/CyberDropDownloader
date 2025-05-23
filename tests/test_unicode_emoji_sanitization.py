7ce2f2ba072a4dc375006199bc0c058e832c03c7
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2025-05-23 15:04:03 -0500

    test: add more tests cases

--- tests/test_unicode_emoji_sanitization.py
+++ tests/test_unicode_emoji_sanitization.py
@@ -1,12 +1,34 @@
-from cyberdrop_dl.utils.utilities import sanitize_unicode_emojis_and_symbols
+import os
+from pathlib import Path
+
+import pytest
+
+from cyberdrop_dl.utils.utilities import sanitize_filename, sanitize_unicode_emojis_and_symbols
+
+IS_WINDOWS = os.name == "nt"
+IS_MACOS = os.name == "darwin"
 
 
-def test_sanitize_macos_problematic_unicode_symbol():
-    assert sanitize_unicode_emojis_and_symbols("🫧Bubblz🫧") == "Bubblz"
+FOREIGN_LENGUAGE_STRINGS = ["これは単なるテストです", "это всего лишь тест", "این فقط یک آزمایش است", "هذا مجرد اختبار"]
+INVALID_UNICODE9_STRING = "🫧Bubblz🫧"
 
 
-def test_sanitization_must_keep_other_languages_chars():
-    assert sanitize_unicode_emojis_and_symbols("これは単なるテストです") == "これは単なるテストです"
-    assert sanitize_unicode_emojis_and_symbols("это всего лишь тест") == "это всего лишь тест"
-    assert sanitize_unicode_emojis_and_symbols("این فقط یک آزمایش است") == "این فقط یک آزمایش است"
-    assert sanitize_unicode_emojis_and_symbols("هذا مجرد اختبار") == "هذا مجرد اختبار"
+def test_sanitize_macos_problematic_unicode_symbol() -> None:
+    assert sanitize_unicode_emojis_and_symbols(INVALID_UNICODE9_STRING) == "Bubblz"
+
+
+@pytest.mark.parametrize("name", FOREIGN_LENGUAGE_STRINGS)
+def test_sanitization_must_keep_other_languages_chars(name: str) -> None:
+    assert sanitize_unicode_emojis_and_symbols(name) == name
+
+
+@pytest.mark.skipif(not (IS_WINDOWS or IS_MACOS))
+def test_unicode9_filename_raise_os_error(tmp_path: Path) -> None:
+    with pytest.raises(OSError):
+        tmp_path.joinpath(INVALID_UNICODE9_STRING).with_suffix(".txt").write_text("OK")
+
+
+@pytest.mark.parametrize("name", [INVALID_UNICODE9_STRING, *FOREIGN_LENGUAGE_STRINGS])
+def test_sanitized_filename_do_not_raise_os_errors(name: str, tmp_path: Path) -> None:
+    filename = sanitize_filename(name)
+    tmp_path.joinpath(filename).with_suffix(".txt").write_text("OK")
