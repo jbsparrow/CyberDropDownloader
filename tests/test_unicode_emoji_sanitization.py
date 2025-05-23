b263b2d63d7ddf4efed771070f38efe7d1bf00ef
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2025-05-23 15:14:05 -0500

    test: only run error test in macOS

--- tests/test_unicode_emoji_sanitization.py
+++ tests/test_unicode_emoji_sanitization.py
@@ -1,12 +1,11 @@
-import os
+import sys
 from pathlib import Path
 
 import pytest
 
 from cyberdrop_dl.utils.utilities import sanitize_filename, sanitize_unicode_emojis_and_symbols
 
-IS_WINDOWS = os.name == "nt"
-IS_MACOS = os.name == "darwin"
+IS_MACOS = sys.platform == "darwin"
 
 
 FOREIGN_LENGUAGE_STRINGS = ["これは単なるテストです", "это всего лишь тест", "این فقط یک آزمایش است", "هذا مجرد اختبار"]
@@ -22,7 +21,7 @@ def test_sanitization_must_keep_other_languages_chars(name: str) -> None:
     assert sanitize_unicode_emojis_and_symbols(name) == name
 
 
-@pytest.mark.skipif(not (IS_WINDOWS or IS_MACOS), reason="linux has support utf8 with any unicode version")
+@pytest.mark.skipif(not IS_MACOS, reason="Only fails in macOS with APFS")
 def test_unicode9_filename_raise_os_error(tmp_path: Path) -> None:
     with pytest.raises(OSError):
         tmp_path.joinpath(INVALID_UNICODE9_STRING).with_suffix(".txt").write_text("OK")
