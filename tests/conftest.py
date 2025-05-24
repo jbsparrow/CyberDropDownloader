47ad500c62b8a1dc29bfa2fd1f71188b08074ae9
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2025-05-23 22:02:43 -0500

    test: check log file

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,14 +5,14 @@ from typing import TYPE_CHECKING
 
 import pytest
 
+from cyberdrop_dl.managers.manager import Manager
+from cyberdrop_dl.scraper.scrape_mapper import ScrapeMapper
+
 if TYPE_CHECKING:
     from collections.abc import AsyncGenerator, Generator
     from pathlib import Path
 
     from _pytest.nodes import Node  # type: ignore
-
-    from cyberdrop_dl.managers.manager import Manager
-    from cyberdrop_dl.scraper.scrape_mapper import ScrapeMapper
 
 
 @pytest.fixture(autouse=True)
@@ -34,8 +34,6 @@ def custom_sys_argv(request: pytest.FixtureRequest) -> list[str]:
 
 @pytest.fixture
 def bare_manager(tmp_path: Path, custom_sys_argv: list[str]) -> Generator[Manager]:
-    from cyberdrop_dl.managers.manager import Manager
-
     with pytest.MonkeyPatch.context() as mocker:
         mocker.chdir(tmp_path)
         mocker.setattr("sys.argv", custom_sys_argv)
@@ -48,6 +46,7 @@ def sync_manager(bare_manager: Manager) -> Generator[Manager]:
     bare_manager.startup()
     bare_manager.path_manager.startup()
     bare_manager.log_manager.startup()
+    assert not bare_manager.log_manager.main_log.exists()
     yield bare_manager
 
 
@@ -60,8 +59,6 @@ async def async_manager(sync_manager: Manager) -> AsyncGenerator[Manager]:
 
 @pytest.fixture
 async def scrape_and_manager(async_manager: Manager) -> AsyncGenerator[tuple[ScrapeMapper, Manager]]:
-    from cyberdrop_dl.scraper.scrape_mapper import ScrapeMapper
-
     async_manager.states.RUNNING.set()
     scrape_mapper = ScrapeMapper(async_manager)
     async with asyncio.TaskGroup() as task_group:
