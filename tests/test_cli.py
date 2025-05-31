ed13ffa7a360cd939dd497e92c7a0300e7dd1fa8
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2025-05-30 20:16:39 -0500

    tests: add more cli tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -5,11 +5,18 @@ import pytest
 from cyberdrop_dl.main import run
 
 
-def test_help(tmp_cwd: Path, capsys: pytest.CaptureFixture[str]) -> None:
+@pytest.mark.parametrize(
+    "command, text",
+    [
+        ("--help", "Bulk asynchronous downloader for multiple file hosts"),
+        ("--show-supported-sites", "for a details about supported paths"),
+    ],
+)
+def test_command_by_console_output(tmp_cwd: Path, capsys: pytest.CaptureFixture[str], command: str, text: str) -> None:
     try:
-        run(("--help",))
+        run(tuple(command.split()))
     except SystemExit:
         pass
     captured = capsys.readouterr()
     output = captured.out
-    assert "Bulk asynchronous downloader for multiple file hosts" in output
+    assert text in output
