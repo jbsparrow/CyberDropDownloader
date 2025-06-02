42a89b5f47c05fcf9db4fc005d4ffcd82fea12ee
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2025-06-02 12:59:42 -0500

    fix: register supported domains

--- cyberdrop_dl/crawlers/bunkrr.py
+++ cyberdrop_dl/crawlers/bunkrr.py
@@ -16,7 +16,7 @@ from aiohttp import ClientConnectorError
 from cyberdrop_dl.constants import FILE_FORMATS
 from cyberdrop_dl.crawlers.crawler import Crawler
 from cyberdrop_dl.exceptions import DDOSGuardError, NoExtensionError, ScrapeError
-from cyberdrop_dl.types import AbsoluteHttpURL, SupportedPaths
+from cyberdrop_dl.types import AbsoluteHttpURL, SupportedDomains, SupportedPaths
 from cyberdrop_dl.utils import css
 from cyberdrop_dl.utils.utilities import (
     error_handling_wrapper,
@@ -122,6 +122,7 @@ class AlbumItem:
 
 
 class BunkrrCrawler(Crawler):
+    SUPPORTED_DOMAINS: ClassVar[SupportedDomains] = "bunkr", "bunkrr"
     SUPPORTED_PATHS: ClassVar[SupportedPaths] = {
         "Album": "/a/...",
         "Video": "/v/...",
@@ -130,7 +131,7 @@ class BunkrrCrawler(Crawler):
     }
     DATABASE_PRIMARY_HOST: ClassVar[str] = "bunkr.site"
     PRIMARY_URL: ClassVar[AbsoluteHttpURL] = AbsoluteHttpURL(f"https://{DATABASE_PRIMARY_HOST}")
-    DOMAIN: ClassVar[str] = "bunkr"
+    DOMAIN: ClassVar[str] = "bunkrr"
 
     def __post_init__(self) -> None:
         self.known_good_host: str = ""
