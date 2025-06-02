7b4b0df05adc5f1101286ff2cbb4622d580636f3
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2025-06-02 13:16:34 -0500

    style: apply ruff fixes

--- cyberdrop_dl/managers/path_manager.py
+++ cyberdrop_dl/managers/path_manager.py
@@ -1,10 +1,10 @@
 from __future__ import annotations
 
+import os
 from dataclasses import Field, field
 from datetime import datetime
 from pathlib import Path
 from typing import TYPE_CHECKING
-import os
 
 from cyberdrop_dl import env
 from cyberdrop_dl.utils.utilities import purge_dir_tree
@@ -88,11 +88,10 @@ class PathManager:
 
         def replace(path: Path) -> Path:
             path_w_config = str(path).replace("{config}", current_config)
-            if os.name == "nt": 
+            if os.name == "nt":
                 return self.cwd.joinpath(Path(path_w_config))
-            else:
-                normalized_path_str = path_w_config.replace("\\", "/")
-                return self.cwd.joinpath(Path(normalized_path_str))
+            normalized_path_str = path_w_config.replace("\\", "/")
+            return self.cwd.joinpath(Path(normalized_path_str))
 
         self.download_folder = replace(settings_data.files.download_folder)
         self.sorted_folder = replace(settings_data.sorting.sort_folder)
