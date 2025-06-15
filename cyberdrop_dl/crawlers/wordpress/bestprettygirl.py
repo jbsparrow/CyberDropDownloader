c7e4761214bd3fd6cfaf7365d518979540a59d15
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2025-06-15 15:52:13 -0500

    fix:  use the API

--- cyberdrop_dl/crawlers/wordpress/bestprettygirl.py
+++ cyberdrop_dl/crawlers/wordpress/bestprettygirl.py
@@ -4,10 +4,10 @@ from typing import ClassVar
 
 from cyberdrop_dl.data_structures.url_objects import AbsoluteHttpURL
 
-from ._wordpress import WordPressBaseCrawler
+from ._wordpress import WordPressAPICrawler
 
 
-class BestPrettyGirlCrawler(WordPressBaseCrawler):
+class BestPrettyGirlCrawler(WordPressAPICrawler):
     DOMAIN: ClassVar[str] = "bestprettygirl.com"
     FOLDER_DOMAIN: ClassVar[str] = "BestPrettyGirl"
     PRIMARY_URL: ClassVar[AbsoluteHttpURL] = AbsoluteHttpURL("https://bestprettygirl.com/")
