6d1899038817ab02346d5f7cacb5095a50408813
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2025-07-07 17:48:39 -0500

    refactor: simplify

--- cyberdrop_dl/crawlers/hotleak_vip.py
+++ cyberdrop_dl/crawlers/hotleak_vip.py
@@ -3,24 +3,15 @@ from __future__ import annotations
 import json
 from typing import TYPE_CHECKING, Any, ClassVar
 
-from cyberdrop_dl.crawlers.leakedzone import LeakedZoneCrawler, decode_video_url
+from cyberdrop_dl.crawlers.leakedzone import LeakedZoneCrawler
 from cyberdrop_dl.data_structures.url_objects import AbsoluteHttpURL
 from cyberdrop_dl.utils import css
-from cyberdrop_dl.utils.m3u8 import M3U8Media
-from cyberdrop_dl.utils.utilities import error_handling_wrapper
 
 if TYPE_CHECKING:
     from bs4 import BeautifulSoup
 
-    from cyberdrop_dl.data_structures.url_objects import ScrapeItem
 
-
-class Selectors:
-    LIGHT_GALLERY_ITEM = "div.light-gallery-item"
-    MODEL_NAME = "div.actor-name > h1"
-
-
-_SELECTORS = Selectors()
+LIGHT_GALLERY_ITEM_SELECTOR = "div.light-gallery-item"
 PRIMARY_URL = AbsoluteHttpURL("https://hotleak.vip")
 IMAGES_CDN = AbsoluteHttpURL("https://image-cdn.hotleak.vip")
 
@@ -31,19 +22,8 @@ class HotLeakVipCrawler(LeakedZoneCrawler):
     PRIMARY_URL: ClassVar[AbsoluteHttpURL] = PRIMARY_URL
     IMAGES_CDN: ClassVar[AbsoluteHttpURL] = IMAGES_CDN
 
-    @error_handling_wrapper
-    async def video(self, scrape_item: ScrapeItem, video_id: str) -> None:
-        if await self.check_complete_from_referer(scrape_item.url):
-            return
-
-        async with self.request_limiter:
-            soup: BeautifulSoup = await self.client.get_soup(self.DOMAIN, scrape_item.url)
-
-        video_info_text = css.select_one_get_attr(soup, _SELECTORS.LIGHT_GALLERY_ITEM, "data-video")
+    @classmethod
+    def get_encoded_video_url(cls, soup: BeautifulSoup) -> str:
+        video_info_text = css.select_one_get_attr(soup, LIGHT_GALLERY_ITEM_SELECTOR, "data-video")
         video_data: dict[str, Any] = json.loads(video_info_text)
-        url = self.parse_url(decode_video_url(video_data["source"][0]["src"]))
-        m3u8_media = M3U8Media(await self._get_m3u8(url))
-        model_name = css.select_one_get_text(soup, _SELECTORS.MODEL_NAME)
-        ext = ".mp4"
-        filename = self.create_custom_filename(model_name, ext, file_id=video_id)
-        await self.handle_file(scrape_item.url, scrape_item, filename, ext, m3u8_media=m3u8_media)
+        return video_data["source"][0]["src"]
--- cyberdrop_dl/crawlers/leakedzone.py
+++ cyberdrop_dl/crawlers/leakedzone.py
@@ -9,7 +9,6 @@ from aiolimiter import AsyncLimiter
 from cyberdrop_dl.compat import IntEnum
 from cyberdrop_dl.crawlers.crawler import Crawler, SupportedPaths
 from cyberdrop_dl.data_structures.url_objects import AbsoluteHttpURL
-from cyberdrop_dl.exceptions import ScrapeError
 from cyberdrop_dl.utils import css
 from cyberdrop_dl.utils.m3u8 import M3U8Media
 from cyberdrop_dl.utils.utilities import error_handling_wrapper, get_text_between
@@ -27,12 +26,12 @@ class PostType(IntEnum):
 
 class Selectors:
     JS_PLAYER = "script:contains('playerInstance.setup')"
-    MODEL_NAME_COLLECTION = "div.actor-name > h1"
-    MODEL_NAME = "h2.actor-title-port"
+    MODEL_NAME_FROM_PROFILE = "div.actor-name > h1"
+    MODEL_NAME_FROM_VIDEO = "h2.actor-title-port"
+    MODEL_NAME = f"{MODEL_NAME_FROM_VIDEO}, {MODEL_NAME_FROM_PROFILE}"
 
 
 _SELECTORS = Selectors()
-
 PRIMARY_URL = AbsoluteHttpURL("https://leakedzone.com")
 IMAGES_CDN = AbsoluteHttpURL("https://image-cdn.leakedzone.com/storage/")
 
@@ -53,13 +52,13 @@ class LeakedZoneCrawler(Crawler):
     async def fetch(self, scrape_item: ScrapeItem) -> None:
         if len(scrape_item.url.parts) >= 4 and "video" in scrape_item.url.parts:
             return await self.video(scrape_item, video_id=scrape_item.url.parts[-1])
-        return await self.collection(scrape_item)
+        return await self.model(scrape_item)
 
     @error_handling_wrapper
-    async def collection(self, scrape_item: ScrapeItem) -> None:
+    async def model(self, scrape_item: ScrapeItem) -> None:
         async with self.request_limiter:
             soup = await self.client.get_soup(self.DOMAIN, scrape_item.url)
-        model_name: str = css.select_one_get_text(soup, _SELECTORS.MODEL_NAME_COLLECTION)
+        model_name: str = css.select_one_get_text(soup, _SELECTORS.MODEL_NAME_FROM_PROFILE)
         title = self.create_title(model_name)
         scrape_item.setup_as_profile(title)
 
@@ -77,21 +76,23 @@ class LeakedZoneCrawler(Crawler):
                     await self.handle_gallery_image(scrape_item, post)
 
     async def handle_gallery_video(self, scrape_item: ScrapeItem, post: dict[str, Any], model_name: str) -> None:
-        video_id: str = str(post["id"])
-        canonical_url: AbsoluteHttpURL = PRIMARY_URL / scrape_item.url.parts[1] / "video" / video_id
+        video_id = str(post["id"])
+        canonical_url = PRIMARY_URL / scrape_item.url.parts[1] / "video" / video_id
         if await self.check_complete_from_referer(canonical_url):
             return
         scrape_item.url = canonical_url
-        url: AbsoluteHttpURL = self.parse_url(decode_video_url(post["stream_url_play"]))
+        url = self.parse_url(_decode_video_url(post["stream_url_play"]))
         m3u8_media = M3U8Media(await self._get_m3u8(url))
-        filename, ext = self.get_filename_and_ext(f"{model_name} [{video_id}].mp4")
+        ext = ".mp4"
+        filename = self.create_custom_filename(model_name, ext, file_id=video_id)
         await self.handle_file(scrape_item.url, scrape_item, filename, ext, m3u8_media=m3u8_media)
 
     async def handle_gallery_image(self, scrape_item: ScrapeItem, post: dict[str, Any]) -> None:
         image_url: AbsoluteHttpURL = self.IMAGES_CDN / post["image"].replace("_thumb", "")
+        image_web_url = PRIMARY_URL / "photo" / str(post["id"])
         filename, ext = self.get_filename_and_ext(image_url.name)
-        new_scrape_item = scrape_item.create_child(image_url)
-        await self.handle_file(new_scrape_item.url, new_scrape_item, filename, ext)
+        new_scrape_item = scrape_item.create_child(image_web_url)
+        await self.handle_file(image_url, new_scrape_item, filename, ext)
 
     @error_handling_wrapper
     async def video(self, scrape_item: ScrapeItem, video_id: str) -> None:
@@ -101,23 +102,21 @@ class LeakedZoneCrawler(Crawler):
         async with self.request_limiter:
             soup: BeautifulSoup = await self.client.get_soup(self.DOMAIN, scrape_item.url)
 
-        player = soup.select_one(_SELECTORS.JS_PLAYER)
-        if not player:
-            raise ScrapeError(422)
-
-        url: AbsoluteHttpURL = self.parse_url(decode_video_url(get_encoded_video_url(player.text)))
+        encoded_url = self.get_encoded_video_url(soup)
+        url = self.parse_url(_decode_video_url(encoded_url))
         m3u8_media = M3U8Media(await self._get_m3u8(url))
         model_name = css.select_one_get_text(soup, _SELECTORS.MODEL_NAME)
         ext = ".mp4"
         filename = self.create_custom_filename(model_name, ext, file_id=video_id)
         await self.handle_file(scrape_item.url, scrape_item, filename, ext, m3u8_media=m3u8_media)
 
+    @classmethod
+    def get_encoded_video_url(cls, soup: BeautifulSoup) -> str:
+        js_text = css.select_one_get_text(soup, _SELECTORS.JS_PLAYER)
+        return get_text_between(js_text, 'file: f("', '"),')
 
-def get_encoded_video_url(script_text: str) -> str:
-    return get_text_between(script_text, 'file: f("', '"),')
 
-
-def decode_video_url(url: str) -> str:
+def _decode_video_url(url: str) -> str:
     # cut first and last 16 characters, reverse, base64 decode
     # TODO: Research if this work on any JW Player
     return binascii.a2b_base64(url[-17:15:-1]).decode()
