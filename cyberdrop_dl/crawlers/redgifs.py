c71e40d6084bb8a1bb9aa936fe7112042828d41e
Barbarella6666666 <155651358+Barbarella6666666@users.noreply.github.com>
2025-07-07 17:55:49 -0300

    fix: handling host with "www." prefix (redgifs) (#1143)

--- cyberdrop_dl/crawlers/redgifs.py
+++ cyberdrop_dl/crawlers/redgifs.py
@@ -27,7 +27,7 @@ class RedGifsCrawler(Crawler):
         await self.get_auth_token(API_ENTRYPOINT / "v2/auth/temporary")
 
     async def fetch(self, scrape_item: ScrapeItem) -> None:
-        if scrape_item.url.host != self.PRIMARY_URL.host:
+        if scrape_item.url.host.removeprefix("www.") != self.PRIMARY_URL.host:
             raise ValueError
         if "users" in scrape_item.url.parts:
             return await self.user(scrape_item)
