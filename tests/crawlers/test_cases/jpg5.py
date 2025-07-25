604d2bd954ce6d77b56a9dbcee0bea825d3fb68a
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2025-07-25 12:38:55 -0500

    tests: add crawler tests

---
+++ tests/crawlers/test_cases/jpg5.py
@@ -0,0 +1,45 @@
+DOMAIN = "jpg5.su"
+TEST_CASES = [
+    (
+        "https://jpg6.su/img/00612-3170878818-raw-photo-nude-woman-naked-braless-no-cloth.Yvjr1u7",
+        [
+            {
+                "url": "https://simp4.jpg5.su/00612-3170878818-RAW-photo-of-a-nude-woman-naked_-braless-no-clothes-highly-detailed-skin-8k-uhd-medium-breast-size-detailed-nipples6b05c0ef6025d0a0.jpg",
+                "filename": "00612-3170878818-RAW-photo-of-a-nude-woman-naked_-braless-no-clothes-highly-detailed-skin-8k-uh.jpg",
+                "referer": "https://jpg6.su/img/Yvjr1u7",
+                "album_id": None,
+                "datetime": 1693206885,
+            }
+        ],
+    ),
+    (
+        "https://jpg6.su/img/2520x3360-ab219efe5d20f99740d58188edd20c440659035b8a5c979d.N3iJfxa",
+        [
+            {
+                "url": "https://simp6.selti-delivery.ru/images3/2520x3360_ab219efe5d20f99740d58188edd20c440659035b8a5c979d2f40a0e086780c7d.jpg",
+                "filename": "2520x3360_ab219efe5d20f99740d58188edd20c440659035b8a5c979d2f40a0e086780c7d.jpg",
+                "referer": "https://jpg6.su/img/N3iJfxa",
+                "datetime": 1753502011,
+            }
+        ],
+    ),
+    (
+        "https://jpg6.su/a/testalbum.YnfD4p",
+        [
+            {
+                "url": "https://simp6.selti-delivery.ru/images3/landscape-mountains-nature-5564166880341c46b7abbe.jpg",
+                "filename": "landscape-mountains-nature-5564166880341c46b7abbe.jpg",
+                "referer": "https://jpg6.su/img/N3TGEr1",
+                "datetime": None,
+                "album_id": "YnfD4p",
+            },
+            {
+                "url": "https://simp6.selti-delivery.ru/images3/47749882ae05d0349d87f2d.jpg",
+                "filename": "47749882ae05d0349d87f2d.jpg",
+                "referer": "https://jpg6.su/img/N3TGCdA",
+                "datetime": None,
+                "album_id": "YnfD4p",
+            },
+        ],
+    ),
+]
