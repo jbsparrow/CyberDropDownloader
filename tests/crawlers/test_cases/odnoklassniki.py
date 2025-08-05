bfe0131e232aef19ef37937a64cfcacb41835e32
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2025-08-05 13:10:48 -0500

    test: add tests

---
+++ tests/crawlers/test_cases/odnoklassniki.py
@@ -0,0 +1,16 @@
+DOMAIN = "odnoklassniki"
+TEST_CASES = [
+    (
+        "https://ok.ru/video/1416667269705",
+        [
+            {
+                "url": "https://m.ok.ru/video/1416667269705",
+                "filename": "73 Réplika_x264 [1416667269705][480p].mp4",
+                "original_filename": "1416667269705.mp4",
+                "referer": "https://ok.ru/video/1416667269705",
+                "album_id": None,
+                "datetime": 1566236730,
+            }
+        ],
+    ),
+]
