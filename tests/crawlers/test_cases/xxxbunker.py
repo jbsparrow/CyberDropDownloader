886ca6f9785e3dba083ab587dbb4d7cbbd093054
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2025-08-06 21:41:11 -0500

    test: add tests

---
+++ tests/crawlers/test_cases/xxxbunker.py
@@ -0,0 +1,16 @@
+DOMAIN = "xxxbunker"
+TEST_CASES = [
+    (
+        "https://xxxbunker.com/11634231",
+        [
+            {
+                "url": "https://xxxbunker.com/player/11634231",
+                "filename": "Stepbrothers Wife Exposes Big Tits For Lactation Scene 1 [11634231].mp4",
+                "referer": "https://xxxbunker.com/11634231",
+                "debrid_link": "ANY",
+                "album_id": None,
+                "datetime": None,
+            }
+        ],
+    ),
+]
