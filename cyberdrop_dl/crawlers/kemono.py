4970169bfd2552e489736fe2d26501362bb32008
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2025-08-07 23:18:02 -0500

    refactor: simplify
    - Add missing error_handling_wrapper for discord methods
    - Add missing no api fallbacks for favorites

--- cyberdrop_dl/crawlers/kemono.py
+++ cyberdrop_dl/crawlers/kemono.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from typing import ClassVar
 
-from cyberdrop_dl.data_structures.url_objects import AbsoluteHttpURL, ScrapeItem
+from cyberdrop_dl.data_structures.url_objects import AbsoluteHttpURL
 
 from ._kemono_base import KemonoBaseCrawler
 
@@ -10,7 +10,7 @@ from ._kemono_base import KemonoBaseCrawler
 class KemonoCrawler(KemonoBaseCrawler):
     SUPPORTED_PATHS: ClassVar = KemonoBaseCrawler.SUPPORTED_PATHS | {
         "Discord Server": "/discord/<server_id>",
-        "Discord Server Channel": "/discord/server/...#...",
+        "Discord Server Channel": "/discord/server/<server_id>/<channel_id>#...",
     }
     PRIMARY_URL: ClassVar[AbsoluteHttpURL] = AbsoluteHttpURL("https://kemono.cr")
     API_ENTRYPOINT: ClassVar[AbsoluteHttpURL] = AbsoluteHttpURL("https://kemono.cr/api/v1")
@@ -26,8 +26,3 @@ class KemonoCrawler(KemonoBaseCrawler):
         "subscribestar",
     )
     OLD_DOMAINS: ClassVar[tuple[str, ...]] = "kemono.party", "kemono.su"
-
-    async def fetch(self, scrape_item: ScrapeItem) -> None:
-        if "discord" in scrape_item.url.parts:
-            return await self.discord(scrape_item)
-        return await super().fetch(scrape_item)
--- cyberdrop_dl/crawlers/nekohouse.py
+++ cyberdrop_dl/crawlers/nekohouse.py
@@ -1,33 +1,18 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, ClassVar
+from typing import ClassVar
 
 from cyberdrop_dl.data_structures.url_objects import AbsoluteHttpURL
 
 from ._kemono_base import KemonoBaseCrawler
 
-if TYPE_CHECKING:
-    from cyberdrop_dl.data_structures.url_objects import ScrapeItem
-
 
 class NekohouseCrawler(KemonoBaseCrawler):
     SUPPORTED_PATHS: ClassVar[dict[str, str]] = {
-        "Model": "/<service>/user/<user>",
-        "Individual Post": "/<service>/<user>/post/...",
-        "Direct links": "",
+        "Model": "/<service>/user/<user_id>",
+        "Individual Post": "/<service>/user/<user_id>/post/<post_id>",
+        "Direct links": "/(data|thumbnails)/...",
     }
     PRIMARY_URL: ClassVar[AbsoluteHttpURL] = AbsoluteHttpURL("https://nekohouse.su")
     DOMAIN: ClassVar[str] = "nekohouse"
     SERVICES = "fanbox", "fantia", "fantia_products", "subscribestar", "twitter"
-
-    async def fetch(self, scrape_item: ScrapeItem) -> None:
-        if "thumbnails" in scrape_item.url.parts:
-            return await self.handle_direct_link(scrape_item)
-        if "post" in scrape_item.url.parts:
-            return await self.post_w_no_api(scrape_item)
-        if any(x in scrape_item.url.parts for x in self.SERVICES):
-            return await self.profile_w_no_api(scrape_item)
-        if any(x in scrape_item.url.parts for x in ("posts", "discord")):
-            raise ValueError
-
-        await self.handle_direct_link(scrape_item)
