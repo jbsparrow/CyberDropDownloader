34e9160083dcd59e554cce5b5e4ac32cf7aa850e
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2025-08-08 01:26:21 -0500

    test: add nekohouse tests

---
+++ tests/crawlers/test_cases/nekohouse.py
@@ -0,0 +1,42 @@
+DOMAIN = "nekohouse"
+TEST_CASES = [
+    (
+        "https://nekohouse.su/fanbox/user/705370/post/9605906",
+        [
+            {
+                "url": "https://nekohouse.su/data/33/7f/337f1f97e0414358092fe9de88046614e35aab417d030da820e4816e3cab3e42.jpg",
+                "filename": "337f1f97e0414358092fe9de88046614e35aab417d030da820e4816e3cab3e42.jpg",
+                "referer": "https://nekohouse.su/fanbox/user/705370/post/9605906",
+                "download_folder": r"re:shiratamaco \(Nekohouse\)",
+                "album_id": "705370",
+                "datetime": 1743063817,
+            },
+            {
+                "url": "https://nekohouse.su/data/ec/7b/ec7bf13520488ea9ed19e766eeac6033547e69eca88e395f12c9e503feaa71a5.jpg",
+                "filename": "ec7bf13520488ea9ed19e766eeac6033547e69eca88e395f12c9e503feaa71a5.jpg",
+                "referer": "https://nekohouse.su/fanbox/user/705370/post/9605906",
+                "album_id": "705370",
+                "datetime": 1743063817,
+            },
+            {
+                "url": "https://nekohouse.su/data/fa/66/fa66c78e0fefaa748baeee23dafc5392236b3a194aa0f2b3040dced30a617b7f.jpg",
+                "filename": "fa66c78e0fefaa748baeee23dafc5392236b3a194aa0f2b3040dced30a617b7f.jpg",
+                "referer": "https://nekohouse.su/fanbox/user/705370/post/9605906",
+                "album_id": "705370",
+                "datetime": 1743063817,
+            },
+        ],
+    ),
+    (
+        "https://nekohouse.su/thumbnail/data/33/7f/337f1f97e0414358092fe9de88046614e35aab417d030da820e4816e3cab3e42.jpg?type=preview",
+        [
+            {
+                "url": "https://nekohouse.su/data/33/7f/337f1f97e0414358092fe9de88046614e35aab417d030da820e4816e3cab3e42.jpg",
+                "filename": "337f1f97e0414358092fe9de88046614e35aab417d030da820e4816e3cab3e42.jpg",
+                "referer": "https://nekohouse.su/data/33/7f/337f1f97e0414358092fe9de88046614e35aab417d030da820e4816e3cab3e42.jpg",
+                "album_id": None,
+                "datetime": None,
+            },
+        ],
+    ),
+]
