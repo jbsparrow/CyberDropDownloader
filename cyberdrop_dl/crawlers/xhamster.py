078ad9ee309f7a852f13b6231a8a26b20ae6f35a
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2025-08-09 02:56:11 -0500

    fix: handle videos without http sources

--- cyberdrop_dl/crawlers/xhamster.py
+++ cyberdrop_dl/crawlers/xhamster.py
@@ -1,25 +1,27 @@
 from __future__ import annotations
 
+import dataclasses
+import itertools
 import json
 from functools import partial
 from typing import TYPE_CHECKING, Annotated, Any, ClassVar, NamedTuple
 
-from pydantic import AliasPath, Field, PlainValidator
+from pydantic import Field, PlainValidator
 
 from cyberdrop_dl.crawlers.crawler import Crawler, SupportedPaths
 from cyberdrop_dl.data_structures.url_objects import AbsoluteHttpURL
 from cyberdrop_dl.models import AliasModel
-from cyberdrop_dl.utils.logger import log_debug
+from cyberdrop_dl.utils import css
 from cyberdrop_dl.utils.utilities import error_handling_wrapper, get_text_between, parse_url
 
 if TYPE_CHECKING:
-    from collections.abc import Generator
-
-    from bs4 import BeautifulSoup
+    from collections.abc import Iterable
 
     from cyberdrop_dl.data_structures.url_objects import ScrapeItem
 
 PRIMARY_URL = AbsoluteHttpURL("https://xhamster.com/")
+ALLOW_HLS = False
+ALLOW_AV1 = False
 
 
 class Selectors:
@@ -32,7 +34,9 @@ class Selectors:
 _SELECTORS = Selectors()
 
 
-HttpURL = Annotated[AbsoluteHttpURL, PlainValidator(partial(parse_url, relative_to=PRIMARY_URL))]
+_parse_url = partial(parse_url, relative_to=PRIMARY_URL)
+
+HttpURL = Annotated[AbsoluteHttpURL, PlainValidator(_parse_url)]
 
 
 class XhamsterCrawler(Crawler):
@@ -40,7 +44,7 @@ class XhamsterCrawler(Crawler):
         "Video": "/videos/<video_title>",
         "User": "/users/<user_name>",
         "Creator": "/creatos/<creator_name>",
-        "Gallery": "photos/gallery/<gallery_name>",
+        "Gallery": "/photos/gallery/<gallery_name>",
     }
     PRIMARY_URL: ClassVar[AbsoluteHttpURL] = PRIMARY_URL
     NEXT_PAGE_SELECTOR: ClassVar[str] = _SELECTORS.NEXT_PAGE
@@ -48,13 +52,17 @@ class XhamsterCrawler(Crawler):
     FOLDER_DOMAIN: ClassVar[str] = "xHamster"
 
     async def fetch(self, scrape_item: ScrapeItem) -> None:
-        if "gallery" in scrape_item.url.parts:
-            return await self.gallery(scrape_item)
-        if any(p in scrape_item.url.parts for p in ("creators", "users")):
-            return await self.profile(scrape_item)
-        if any(p in scrape_item.url.parts for p in ("movies", "videos")):
-            return await self.video(scrape_item)
-        raise ValueError
+        match scrape_item.url.parts[1:]:
+            case ["photos", "gallery", _]:
+                return await self.gallery(scrape_item)
+            case ["movies" | "videos", _]:
+                return await self.video(scrape_item)
+            case ["creators" | "users", _]:
+                return await self.profile(scrape_item)
+            case ["gallery"]:
+                return await self.gallery(scrape_item)
+            case _:
+                raise ValueError
 
     @error_handling_wrapper
     async def profile(self, scrape_item: ScrapeItem) -> None:
@@ -84,7 +92,7 @@ class XhamsterCrawler(Crawler):
 
     @error_handling_wrapper
     async def gallery(self, scrape_item: ScrapeItem) -> None:
-        json_info = await self.get_model_details(scrape_item.url, "photosGalleryModel", "galleryModel")
+        json_info = await self._get_window_initials(scrape_item.url, "photosGalleryModel", "galleryModel")
         gallery = Gallery(**json_info)
         title = f"{gallery.title} [gallery]"
         scrape_item.setup_as_album(title, album_id=gallery.id)
@@ -105,52 +113,32 @@ class XhamsterCrawler(Crawler):
         if await self.check_complete_from_referer(scrape_item):
             return
 
-        json_info = await self.get_model_details(scrape_item.url, "videoModel")
-        video = Video(**json_info)
+        initials = await self._get_window_initials(scrape_item.url)
 
-        scrape_item.url = video.page_url
+        video = _parse_video(initials)
         scrape_item.possible_datetime = video.created
-        _, resolution, download_url = max(video.get_formats())
-        link = PRIMARY_URL / "movies" / video.id / "download" / resolution
-        filename, ext = self.get_filename_and_ext(f"{video.id}.mp4")
-        custom_filename = self.create_custom_filename(video.title, ext, file_id=video.id, resolution=resolution)
+        custom_filename = self.create_custom_filename(
+            video.title,
+            ".mp4",
+            file_id=video.id,
+            video_codec=video.best_mp4.codec,
+            resolution=video.best_mp4.resolution,
+        )
         await self.handle_file(
-            link, scrape_item, filename, ext, custom_filename=custom_filename, debrid_link=download_url
+            video.url,
+            scrape_item,
+            filename=f"{video.id}.mp4",
+            custom_filename=custom_filename,
+            debrid_link=video.best_mp4.url,
         )
 
-    async def get_model_details(self, url: AbsoluteHttpURL, *model_name_choices: str) -> dict:
-        model_names = model_name_choices or []
-
+    async def _get_window_initials(self, url: AbsoluteHttpURL, *model_name_choices: str) -> dict[str, Any]:
         async with self.request_limiter:
-            soup: BeautifulSoup = await self.client.get_soup(self.DOMAIN, url)
+            soup = await self.client.get_soup(self.DOMAIN, url)
 
-        json_info = get_window_initials_json(soup)
-        del soup
-
-        def get_model_value() -> dict[str, Any] | None:
-            for model_name in model_names:
-                if (value := json_info.get(model_name)) is not None:
-                    return value
-
-        json_info = get_model_value() or json_info
-        log_debug(json_info)
-        return json_info
-
-
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-
-def get_window_initials_json(soup: BeautifulSoup) -> dict[str, dict]:
-    js_script = soup.select_one(_SELECTORS.JS_VIDEO_INFO)
-    js_code: str = str(js_script)
-    json_text: str = get_text_between(js_code, "window.initials=", "</script>").removesuffix(";").strip()
-    return json.loads(json_text)
-
-
-class Format(NamedTuple):
-    height: int
-    resolution: str
-    url: AbsoluteHttpURL
+        js_script = css.select_one(soup, _SELECTORS.JS_VIDEO_INFO)
+        json_text = get_text_between(str(js_script), "window.initials=", ";</script>")
+        return json.loads(json_text)
 
 
 class XHamsterItem(AliasModel):
@@ -169,15 +157,101 @@ class Gallery(XHamsterItem):
     quantity: int
 
 
-class Video(XHamsterItem):
+@dataclasses.dataclass(frozen=True, slots=True)
+class Video:
+    id: str
     title: str
-    mp4_file: HttpURL = Field(alias="mp4File")
-    mp4_sources: dict[str, Any] = Field({}, validation_alias=AliasPath("sources", "mp4"))
+    created: int
+    url: AbsoluteHttpURL
+    best_hls: Format | None
+    best_mp4: Format
 
-    def get_formats(self) -> Generator[Format]:
-        yield Format(0, "", self.mp4_file)
-        for resolution, details in self.mp4_sources.items():
-            height = int(resolution.removesuffix("p"))
-            link: str = details["link"] if isinstance(details, dict) else details
-            url = parse_url(link, relative_to=PRIMARY_URL)
-            yield Format(height, f"{height}p", url)
+
+def _parse_video(initials: dict[str, Any]):
+    video: dict[str, Any] = initials["videoModel"]
+
+    hls_sources: list[Format] = []
+    mp4_sources: list[Format] = []
+
+    sources = itertools.chain(_parse_http_sources(initials), _parse_xplayer_sources(initials))
+
+    for src in sources:
+        if src.codec == "av1" and not ALLOW_AV1:
+            continue
+        if src.url.suffix == ".m3u8":
+            if not ALLOW_HLS:
+                continue
+            hls_sources.append(src)
+        else:
+            mp4_sources.append(src)
+
+    return Video(
+        id=video["idHashSlug"],
+        title=video["title"],
+        created=video["created"],
+        url=_parse_url(video["pageURL"]),
+        best_hls=max(hls_sources, default=None),
+        best_mp4=max(mp4_sources),
+    )
+
+
+class Format(NamedTuple):
+    codec: str  # av1 > h264
+    resolution: int
+    url: AbsoluteHttpURL
+
+
+def _parse_http_sources(initials: dict[str, Any]) -> Iterable[Format]:
+    seen_urls: set[AbsoluteHttpURL] = set()
+
+    http_sources: dict[str, dict[str, str]] = initials["videoModel"].get("sources") or {}
+    if not http_sources:
+        return
+
+    for codec, formats_dict in http_sources.items():
+        for quality, url in formats_dict.items():
+            if codec == "download":
+                continue
+
+            url = _parse_url(url)
+            if url in seen_urls:
+                continue
+
+            seen_urls.add(url)
+            yield Format(codec, int(quality.removesuffix("p")), url)
+
+
+def _parse_xplayer_sources(initials: dict[str, Any]) -> Iterable[Format]:
+    xplayer_sources: dict[str, Any] = initials.get("xplayerSettings", {}).get("sources", {})
+    if not xplayer_sources:
+        return
+
+    seen_urls: set[AbsoluteHttpURL] = set()
+
+    def parse_format_dict(format_dict: dict[str, str], codec: str):
+        for key in ("url", "fallback"):
+            url = format_dict.get(key)
+            if not url:
+                continue
+
+            url = _parse_url(url)
+            if url in seen_urls:
+                continue
+
+            seen_urls.add(url)
+            if url.suffix == ".m3u8":
+                resolution = 0
+            else:
+                quality: str = format_dict.get("quality") or format_dict["label"]
+                resolution = int(quality.removesuffix("p"))
+
+            yield Format(codec, resolution, url)
+
+    hls_sources: dict[str, dict[str, str]] = xplayer_sources.get("hls") or {}
+    for codec, format_dict in hls_sources.items():
+        yield from parse_format_dict(format_dict, codec)
+
+    standard_sources: dict[str, list[dict[str, Any]]] = xplayer_sources.get("standard") or {}
+    for codec, formats_list in standard_sources.items():
+        for format_dict in formats_list:
+            yield from parse_format_dict(format_dict, codec)
