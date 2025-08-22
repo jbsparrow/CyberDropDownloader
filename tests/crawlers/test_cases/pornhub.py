990c790090cbcafe8185f896427e93a924072076
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2025-08-22 16:35:44 +0000

    refactor: better geo blocked log msgs (pornhub) (#1248)

--- tests/crawlers/test_cases/pornhub.py
+++ tests/crawlers/test_cases/pornhub.py
@@ -15,4 +15,32 @@ TEST_CASES = [
         ],
         8,
     ),
+    (
+        # mp4 available
+        "https://www.pornhub.com/view_video.php?viewkey=ph5d530fd885a81",
+        [
+            {
+                "url": "https://www.pornhub.com/embed/ph5d530fd885a81",
+                "filename": "masturbating with my tail butt plug in - Ally Blake [ph5d530fd885a81][1080p].mp4",
+                "original_filename": "ph5d530fd885a81.mp4",
+                "referer": "https://www.pornhub.com/view_video.php?viewkey=ph5d530fd885a81",
+                "album_id": None,
+                "datetime": 1565724764,
+            }
+        ],
+    ),
+    (
+        # m3u8 download
+        "https://www.pornhub.com/embed/6890ddc0b4b11",
+        [
+            {
+                "url": "https://www.pornhub.com/embed/6890ddc0b4b11",
+                "filename": "MILF Celebrates Victory in Public Parking Ramp by Flashing Her Titti [6890ddc0b4b11][1080p].mp4",
+                "original_filename": "6890ddc0b4b11.mp4",
+                "referer": "https://www.pornhub.com/view_video.php?viewkey=6890ddc0b4b11",
+                "album_id": None,
+                "datetime": 1754324606,
+            }
+        ],
+    ),
 ]
