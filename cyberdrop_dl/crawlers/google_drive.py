af0450828a2c07b79ee169800eba8be562720064
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2025-08-27 12:08:47 -0500

    docs: add original header

--- cyberdrop_dl/crawlers/google_drive.py
+++ cyberdrop_dl/crawlers/google_drive.py
@@ -1,3 +1,35 @@
+# SPDX-License-Identifier: GPL-3.0
+#
+# Licensed under GPL-3.0 as detailed in the root LICENSE file
+#
+# The code in this file has been adapted from an MIT-licensed source.
+# See the MIT License section below for details.
+#
+# Original code from https://github.com/wkentaro/gdown by Kentaro Wada (wkentaro)
+#
+# ----------------------------------------------------------------------
+# MIT License
+# ----------------------------------------------------------------------
+#
+# Copyright (c) 2015 Kentaro Wada
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 from __future__ import annotations
 
 import asyncio
