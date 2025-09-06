e40fc2c5e06cc5251a85fced5676b89349d6fb34
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2025-09-06 05:51:53 -0500

    fix: handle encoded URLs (mediaFire)
    - Download URL are now base64 encoded
    - All request sto the actual website require HTTP2 or they return 404, so we need to use impersonate

--- cyberdrop_dl/crawlers/mediafire.py
+++ cyberdrop_dl/crawlers/mediafire.py
@@ -1,16 +1,16 @@
 from __future__ import annotations
 
+import base64
 import itertools
 from typing import TYPE_CHECKING, Any, ClassVar
 
-from aiolimiter import AsyncLimiter
 from mediafire import MediaFireApi, api
 
 from cyberdrop_dl.crawlers.crawler import Crawler, SupportedPaths
 from cyberdrop_dl.data_structures.url_objects import AbsoluteHttpURL
 from cyberdrop_dl.exceptions import MediaFireError, ScrapeError
 from cyberdrop_dl.utils import css
-from cyberdrop_dl.utils.utilities import error_handling_wrapper
+from cyberdrop_dl.utils.utilities import error_handling_wrapper, is_blob_or_svg
 
 if TYPE_CHECKING:
     from bs4 import BeautifulSoup
@@ -20,6 +20,7 @@ if TYPE_CHECKING:
 DOWNLOAD_BUTTON_SELECTOR = "a[id=downloadButton]"
 DATE_SELECTOR = "ul[class=details] li span"
 PRIMARY_URL = AbsoluteHttpURL("https://www.mediafire.com/")
+API_URL = PRIMARY_URL / "api/1.4"
 
 
 class MediaFireCrawler(Crawler):
@@ -29,10 +30,10 @@ class MediaFireCrawler(Crawler):
     }
     PRIMARY_URL: ClassVar[AbsoluteHttpURL] = PRIMARY_URL
     DOMAIN: ClassVar[str] = "mediafire"
+    _RATE_LIMIT = 5, 1
 
     def __post_init__(self) -> None:
         self.api = MediaFireApi()
-        self.request_limiter = AsyncLimiter(5, 1)
 
     async def fetch(self, scrape_item: ScrapeItem) -> None:
         if "folder" in scrape_item.url.parts:
@@ -73,17 +74,24 @@ class MediaFireCrawler(Crawler):
         if await self.check_complete_from_referer(scrape_item):
             return
 
-        async with self.request_limiter:
-            soup: BeautifulSoup = await self.client.get_soup(self.DOMAIN, scrape_item.url)
-
-        link_tag = soup.select_one(DOWNLOAD_BUTTON_SELECTOR)
-        if not link_tag:
-            if "Something appears to be missing" in soup.get_text():
-                raise ScrapeError(410)
-            raise ScrapeError(422)
-
+        soup = await self.request_soup(scrape_item.url, impersonate=True)
         scrape_item.possible_datetime = self.parse_iso_date(soup.select(DATE_SELECTOR)[-1].get_text())
-        link_str: str = css.get_attr(link_tag, "href")
-        link = self.parse_url(link_str)
+        link = self.parse_url(_extract_download_link(soup))
         filename, ext = self.get_filename_and_ext(link.name)
         await self.handle_file(link, scrape_item, filename, ext)
+
+
+def _extract_download_link(soup: BeautifulSoup) -> str:
+    link_tag = soup.select_one(DOWNLOAD_BUTTON_SELECTOR)
+    if not link_tag:
+        if "Something appears to be missing" in soup.get_text():
+            raise ScrapeError(410)
+        raise ScrapeError(422)
+
+    if encoded_url := css.get_attr_or_none(link_tag, "data-scrambled-url"):
+        return base64.urlsafe_b64decode(encoded_url).decode()
+
+    url = css.get_attr(link_tag, "data-scrambled-url")
+    if is_blob_or_svg(url):
+        raise ScrapeError(422)
+    return url
