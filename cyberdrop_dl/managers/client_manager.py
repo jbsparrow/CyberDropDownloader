1900d6bb71ce64bfcafc8e1e19ba2ca4622e8be8
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2025-09-18 16:50:15 +0000

    feat: allow a null socket read timeout (#1313)

--- cyberdrop_dl/managers/client_manager.py
+++ cyberdrop_dl/managers/client_manager.py
@@ -274,7 +274,7 @@ class ClientManager:
             impersonate="chrome",
             verify=bool(self.ssl_context),
             proxy=proxy_or_none,
-            timeout=self.rate_limiting_options._timeout,
+            timeout=self.rate_limiting_options._curl_timeout,
             cookies={cookie.key: cookie.value for cookie in self.cookies},
         )
 
@@ -300,11 +300,11 @@ class ClientManager:
         self, cached: bool = False, trace_configs: list[aiohttp.TraceConfig] | None = None
     ) -> CachedSession | ClientSession:
         if cached:
-            timeout = self.rate_limiting_options._scrape_timeout
+            timeout = self.rate_limiting_options._aiohttp_timeout
             session_cls = CachedSession
             kwargs: dict[str, Any] = {"cache": self.manager.cache_manager.request_cache}
         else:
-            timeout = self.rate_limiting_options._download_timeout
+            timeout = self.rate_limiting_options._aiohttp_timeout
             session_cls = ClientSession
             kwargs = {}
         return session_cls(
