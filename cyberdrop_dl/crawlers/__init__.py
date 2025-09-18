829544858e9dc6efecdd5b8920c433c0dfe88612
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2025-09-18 05:51:29 -0500

    feat: add voe.sx support

--- cyberdrop_dl/crawlers/__init__.py
+++ cyberdrop_dl/crawlers/__init__.py
@@ -102,6 +102,7 @@ from .twitter_images import TwimgCrawler
 from .twpornstars import TwPornstarsCrawler
 from .vbulletin import VBULLETIN_CRAWLERS
 from .vipr_dot_im import ViprImCrawler
+from .voe_sx import VoeSxCrawler
 from .wetransfer import WeTransferCrawler
 from .wordpress import WP_CRAWLERS, WordPressHTMLCrawler, WordPressMediaCrawler
 from .xbunkr import XBunkrCrawler
---
+++ tests/crawlers/test_cases/voe_sx.py
@@ -0,0 +1,43 @@
+DOMAIN = "voe.sx"
+TEST_CASES = [
+    (
+        "https://jilliandescribecompany.com/bhrc4degufg9",
+        [
+            {
+                "url": "https://jilliandescribecompany.com/e/bhrc4degufg9",
+                "debrid_link": "ANY",
+                "filename": "Dr4gr4c3Fr4nc143x7Pv [bhrc4degufg9].mp4",
+                "referer": "https://jilliandescribecompany.com/e/bhrc4degufg9",
+                "album_id": None,
+                "datetime": None,
+            },
+            {
+                "url": "https://jilliandescribecompany.com/srt/14316/bhrc4degufg9_en.vtt",
+                "filename": "Dr4gr4c3Fr4nc143x7Pv [bhrc4degufg9].en.vtt",
+            },
+            {
+                "url": "https://jilliandescribecompany.com/srt/14316/bhrc4degufg9_pt.vtt",
+                "filename": "Dr4gr4c3Fr4nc143x7Pv [bhrc4degufg9].pt.vtt",
+            },
+            {
+                "url": "https://jilliandescribecompany.com/srt/14316/bhrc4degufg9_es.vtt",
+                "filename": "Dr4gr4c3Fr4nc143x7Pv [bhrc4degufg9].es.vtt",
+            },
+        ],
+    ),
+    (
+        "https://voe.sx/ueof3kkmzcal",
+        [
+            {
+                "url": "https://jilliandescribecompany.com/e/ueof3kkmzcal",
+                "debrid_link": "ANY",
+                "filename": "wlkng.dd.ct.1x02.m720p.es [ueof3kkmzcal].mp4",
+                "referer": "https://jilliandescribecompany.com/e/ueof3kkmzcal",
+                "album_id": None,
+                "datetime": None,
+            }
+        ],
+    ),
+    ("https://voe.sx/bhrc4degufg9", [], 4),
+    ("https://voe.sx/e/bhrc4degufg9", [], 4),
+]
