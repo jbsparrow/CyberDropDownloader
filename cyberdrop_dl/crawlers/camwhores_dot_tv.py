bfbf62d3d345b65e2e245de9bdaa114d719d6ff7
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2025-09-18 17:25:21 +0000

    fix: parse 1440p and 4K videos (KVS) (#1314)

--- cyberdrop_dl/crawlers/camwhores_dot_tv.py
+++ cyberdrop_dl/crawlers/camwhores_dot_tv.py
@@ -2,18 +2,15 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING, ClassVar
 
-from cyberdrop_dl.crawlers._kvs import KernelVideoSharingCrawler, Video
+from cyberdrop_dl.crawlers._kvs import KernelVideoSharingCrawler
 from cyberdrop_dl.crawlers.porntrex import PorntrexCrawler
 from cyberdrop_dl.data_structures.url_objects import AbsoluteHttpURL, ScrapeItem
-from cyberdrop_dl.utils import css, open_graph
+from cyberdrop_dl.utils import css
 
 if TYPE_CHECKING:
-    from bs4 import BeautifulSoup
-
     from cyberdrop_dl.crawlers.crawler import SupportedPaths
 
 
-PRIMARY_URL = AbsoluteHttpURL("https://www.camwhores.tv")
 LAST_PAGE_SELECTOR = "div.pagination-holder li.page"
 
 
@@ -26,7 +23,7 @@ class CamwhoresTVCrawler(KernelVideoSharingCrawler):
         "Members": "/members/<member_id>",
     }
 
-    PRIMARY_URL: ClassVar[AbsoluteHttpURL] = PRIMARY_URL
+    PRIMARY_URL: ClassVar[AbsoluteHttpURL] = AbsoluteHttpURL("https://www.camwhores.tv")
     DOMAIN: ClassVar[str] = "camwhores.tv"
 
     async def fetch(self, scrape_item: ScrapeItem) -> None:
@@ -34,10 +31,6 @@ class CamwhoresTVCrawler(KernelVideoSharingCrawler):
         if scrape_item.url.name:
             scrape_item.url = scrape_item.url / ""
         await super().fetch(scrape_item)
-
-    def get_video_info(self, soup: BeautifulSoup) -> Video:
-        video = super().get_video_info(soup)
-        return video._replace(title=open_graph.title(soup))
 
     def parse_url(
         self, link_str: str, relative_to: AbsoluteHttpURL | None = None, *, trim: bool | None = None
