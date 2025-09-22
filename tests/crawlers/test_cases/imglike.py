6ff092ef7fd733d6ac0b7622737ff0e78cb07203
Barbarella <barbarella6666666@protonmail.com>
2025-09-22 07:58:12 -0300

    fix: correcting image urls

--- tests/crawlers/test_cases/imglike.py
+++ tests/crawlers/test_cases/imglike.py
@@ -4,7 +4,7 @@ TEST_CASES = [
         "https://imglike.com/image/Palm-Desert-Resuscitation-Education-%28YourCPRMD.com%29.L1xHc",
         [
             {
-                "url": "https://imglike.com/image/Palm-Desert-Resuscitation-Education-%28YourCPRMD.com%29.L1xHc",
+                "url": "https://imglike.com/images/2022/10/20/Palm-Desert-Resuscitation-Education-YourCPRMD.com.png",
                 "filename": "Palm-Desert-Resuscitation-Education-YourCPRMD.com.png",
                 "referer": "http://imglike.com/image/L1xHc",
                 "album_id": None,
@@ -16,42 +16,42 @@ TEST_CASES = [
         "https://imglike.com/album/Kara-Del-Toro-Naked.cG7l",
         [
             {
-                "url": "https://imglike.com/image/Nude-Kara-Del-Tori-%286%29.LGXgd",
+                "url": "https://imglike.com/images/2022/06/23/Nude-Kara-Del-Tori-6.jpg",
                 "filename": "Nude-Kara-Del-Tori-6.jpg",
                 "referer": "https://imglike.com/image/LGXgd",
                 "datetime": 1655983031,
                 "album_id": "cG7l",
             },
             {
-                "url": "https://imglike.com/image/Nude-Kara-Del-Tori-%285%29.LGxFT",
+                "url": "https://imglike.com/images/2022/06/23/Nude-Kara-Del-Tori-5.jpg",
                 "filename": "Nude-Kara-Del-Tori-5.jpg",
                 "referer": "https://imglike.com/image/LGxFT",
                 "datetime": 1655983031,
                 "album_id": "cG7l",
             },
             {
-                "url": "https://imglike.com/image/Nude-Kara-Del-Tori-%283%29.LGB1z",
+                "url": "https://imglike.com/images/2022/06/23/Nude-Kara-Del-Tori-3.jpg",
                 "filename": "Nude-Kara-Del-Tori-3.jpg",
                 "referer": "https://imglike.com/image/LGB1z",
                 "datetime": 1655983030,
                 "album_id": "cG7l",
             },
             {
-                "url": "https://imglike.com/image/Nude-Kara-Del-Tori-%284%29.LG6lR",
+                "url": "https://imglike.com/images/2022/06/23/Nude-Kara-Del-Tori-4.jpg",
                 "filename": "Nude-Kara-Del-Tori-4.jpg",
                 "referer": "https://imglike.com/image/LG6lR",
                 "datetime": 1655983030,
                 "album_id": "cG7l",
             },
             {
-                "url": "https://imglike.com/image/Nude-Kara-Del-Tori-%282%29.LG4Jg",
+                "url": "https://imglike.com/images/2022/06/23/Nude-Kara-Del-Tori-2.jpg",
                 "filename": "Nude-Kara-Del-Tori-2.jpg",
                 "referer": "https://imglike.com/image/LG4Jg",
                 "datetime": 1655983028,
                 "album_id": "cG7l",
             },
             {
-                "url": "https://imglike.com/image/Nude-Kara-Del-Tori-%286%29.LGXgd",
+                "url": "https://imglike.com/images/2022/06/23/Nude-Kara-Del-Tori-1.jpg",
                 "filename": "Nude-Kara-Del-Tori-1.jpg",
                 "referer": "https://imglike.com/image/LGP7s",
                 "datetime": 1655983028,
