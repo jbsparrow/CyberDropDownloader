ee92c284e099313b169d5cff589b4fa50adfe463
Barbarella <barbarella6666666@protonmail.com>
2025-09-24 08:09:22 -0300

    test: adding test_case

---
+++ tests/crawlers/test_cases/thisvid.py
@@ -0,0 +1,15 @@
+DOMAIN = "thisvid"
+TEST_CASES = [
+    (
+        "https://thisvid.com/videos/a-wedding-photo-to-remember/",
+        [
+            {
+                "url": "https://thisvid.com/videos/a-wedding-photo-to-remember",
+                "filename": "A wedding photo to remember [12797267].mp4",
+                "referer": "https://thisvid.com/videos/a-wedding-photo-to-remember",
+                "album_id": None,
+                "datetime": 1753326000,
+            }
+        ],
+    ),
+]
