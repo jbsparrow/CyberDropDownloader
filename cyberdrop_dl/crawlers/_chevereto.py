c506aab68f90ccc10fe05d8513855359ff336798
Barbarella <barbarella6666666@protonmail.com>
2025-09-24 07:12:03 -0300

    refactor: moving request_soup to ImgLikeCrawler

--- cyberdrop_dl/crawlers/_chevereto.py
+++ cyberdrop_dl/crawlers/_chevereto.py
@@ -4,7 +4,7 @@ import base64
 from typing import TYPE_CHECKING, ClassVar
 
 from cyberdrop_dl.crawlers.crawler import Crawler
-from cyberdrop_dl.data_structures.url_objects import AbsoluteHttpURL, copy_signature
+from cyberdrop_dl.data_structures.url_objects import AbsoluteHttpURL
 from cyberdrop_dl.exceptions import PasswordProtectedError
 from cyberdrop_dl.utils import css, open_graph
 from cyberdrop_dl.utils.utilities import error_handling_wrapper, xor_decrypt
@@ -164,11 +164,6 @@ class CheveretoCrawler(Crawler, is_generic=True):
     ) -> None:
         link = _thumbnail_to_src(url or scrape_item.url)
         await super().direct_file(scrape_item, link, assume_ext)
-
-    @copy_signature(Crawler.request_soup)
-    async def request_soup(self, url: AbsoluteHttpURL, *args, impersonate: bool = False, **kwargs) -> BeautifulSoup:
-        impersonate = impersonate or "image" in url.parts
-        return await super().request_soup(url, *args, impersonate=impersonate, **kwargs)
 
     @error_handling_wrapper
     async def media(self, scrape_item: ScrapeItem) -> None:
