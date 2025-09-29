6cccd25f1f3635bf33edb892daa074d09b61172a
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2025-09-29 20:29:17 +0000

    refactor: save the database path as an attribute of `MediaItem` (#1354)
    We use the db path for every db lookup. We compute it every time before a lookup but this path will never changes after the item is created
    
    This will allow us to simplify all db calls in the future since the only argument they will ever need is the MediaItem object.

--- tests/test_database.py
+++ tests/test_database.py
@@ -1,12 +1,17 @@
+from __future__ import annotations
+
 from datetime import datetime
 from pathlib import Path
-from typing import cast
+from typing import TYPE_CHECKING, cast
 
-import aiosqlite
 import pytest
 
-from cyberdrop_dl.data_structures.url_objects import AbsoluteHttpURL, ScrapeItem
+from cyberdrop_dl.data_structures.url_objects import AbsoluteHttpURL, MediaItem, ScrapeItem
 from cyberdrop_dl.scraper.scrape_mapper import _create_item_from_row
+from cyberdrop_dl.utils.utilities import parse_url
+
+if TYPE_CHECKING:
+    import aiosqlite
 
 _MOCK_ROW = {
     "referer": "https://drive.google.com/file/d/1F0YBsnQRvrMbK0p9UlnyLu88kqQ0j_F6/edit",
@@ -81,3 +86,28 @@ def test_invalid_date_format(row) -> None:
 
     with pytest.raises(ValueError):
         _create_item_from_row(row)
+
+
+@pytest.mark.parametrize(
+    "url, expected",
+    [
+        (
+            "https://megacloud.blog/embed-2/v3/e-1/TZb4gRkOQ642?k=1&autoPlay=1&oa=0&asi=1",
+            "/embed-2/v3/e-1/TZb4gRkOQ642",
+        ),
+        ("https://www.mediafire.com/file/ctppmpm7giofsgv/ADOFAI.vpk", "ADOFAI.vpk"),
+        ("https://mega.nz/#!Ue5VRSIQ!kC2E4a4JwfWWCWYNJovGFHlbz8F", "/#!Ue5VRSIQ!kC2E4a4JwfWWCWYNJovGFHlbz8F"),
+        (
+            "https://mega.nz/folder/oZZxyBrY#oU4jASLPpJVvqGHJIMRcgQ/file/IYZABDGY",
+            "/folder/oZZxyBrY#oU4jASLPpJVvqGHJIMRcgQ/file/IYZABDGY",
+        ),
+        (
+            "https://c.bunkr-cache.se/HwdRnHMUiWOQevCg/1df93418-5063-4e1b-851e-9470cb8fc5c6.mp4",
+            "/HwdRnHMUiWOQevCg/1df93418-5063-4e1b-851e-9470cb8fc5c6.mp4",
+        ),
+    ],
+)
+def test_create_db_path(url: str, expected: str) -> None:
+    url_ = parse_url(url)
+    path = MediaItem.create_db_path(url_, url_.host)
+    assert path == expected
