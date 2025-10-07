7340afa40b76d97f92bdc627f7ade60c6d58c956
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2025-10-07 01:57:30 +0000

    chore: update dependencies (#1369)
    * chore: update dependencies
    
    * chore: remove `browser_cookie3` dep
    
    * chore: update poetry lock
    
    * chore: remove `wmi` warning catch
    
    * chore: update dependencies
    
    * chore: update poetry lock

--- cyberdrop_dl/dependencies/__init__.py
+++ cyberdrop_dl/dependencies/__init__.py
@@ -1,7 +1,3 @@
-try:
-    import browser_cookie3
-except ImportError:
-    import _browser_cookie3 as browser_cookie3
-
+from . import _browser_cookie3 as browser_cookie3
 
 __all__ = ["browser_cookie3"]
