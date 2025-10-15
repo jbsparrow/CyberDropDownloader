249bf354f4ec383b069ef9acaa1d2c104701ecc8
Barbarella <barbarella6666666@protonmail.com>
2025-10-15 16:48:32 -0300

    test: adding test for spankbang

---
+++ tests/crawlers/test_cases/spankbang.py
@@ -0,0 +1,15 @@
+DOMAIN = "spankbang"
+TEST_CASES = [
+    (
+        "https://spankbang.com/9qwc4/video/wife+for+free+use+devil+khloee",
+        [
+            {
+                "url": r"re:https://vdownload.+16371508-1080p.mp4\?.+_tid=16371508",
+                "filename": "WIFE FOR FREE USE . Devil Khloee [16371508][1080p].mp4",
+                "referer": "https://spankbang.com/16371508/video",
+                "album_id": None,
+                "datetime": None,
+            }
+        ],
+    ),
+]
