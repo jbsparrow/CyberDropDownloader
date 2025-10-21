abc9ce003c9611319ba8ae014c277665b45d1aec
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2025-10-21 20:04:01 +0000

    feat: add support for images (RedGifs) (#1415)

---
+++ tests/crawlers/test_cases/redgifs.py
@@ -0,0 +1,15 @@
+DOMAIN = "redgifs"
+TEST_CASES = [
+    (
+        "https://i.redgifs.com/i/tidyshowybream.jpg",
+        [
+            {
+                "url": "https://media.redgifs.com/TidyShowyBream-large.jpg",
+                "filename": "TidyShowyBream-large.jpg",
+                "referer": "https://www.redgifs.com/watch/tidyshowybream",
+                "album_id": None,
+                "datetime": 1734826081,
+            }
+        ],
+    )
+]
