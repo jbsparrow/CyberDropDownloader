b03e7e8a82ac559a5e5f918fef46ef9eb2410552
Barbarella <barbarella6666666@protonmail.com>
2025-10-22 08:48:30 -0300

    ruff: fixing code format

--- cyberdrop_dl/crawlers/transflix.py
+++ cyberdrop_dl/crawlers/transflix.py
@@ -53,9 +53,7 @@ class TransflixCrawler(Crawler):
         scrape_item.possible_datetime = int(filename.split(".")[0])
         custom_filename = self.create_custom_filename(title, ext, file_id=video_id)
 
-        return await self.handle_file(
-            link, scrape_item, filename, ext, custom_filename=custom_filename
-        )
+        return await self.handle_file(link, scrape_item, filename, ext, custom_filename=custom_filename)
 
     @error_handling_wrapper
     async def search(self, scrape_item: ScrapeItem, query: str) -> None:
