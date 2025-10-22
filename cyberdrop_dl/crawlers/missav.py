8e3a66e73379b10e2bca4c5274f3405f7975c608
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2025-10-22 11:13:10 -0500

    refactor: simplify

--- cyberdrop_dl/crawlers/missav.py
+++ cyberdrop_dl/crawlers/missav.py
@@ -13,24 +13,30 @@ if TYPE_CHECKING:
     from cyberdrop_dl.data_structures.url_objects import ScrapeItem
 
 
-M3U8_SERVER = AbsoluteHttpURL("https://surrit.com/")
-PRIMARY_URL = AbsoluteHttpURL("https://missav.ws")
-DATE_SELECTOR = "div > span:-soup-contains('Release date:') + time"
-DVD_CODE_SELECTOR = "div > span:-soup-contains('Code:') + span"
+_M3U8_SERVER = AbsoluteHttpURL("https://surrit.com/")
+_PRIMARY_URL = AbsoluteHttpURL("https://missav.ws")
+
+
+class Selector:
+    UUID = "script:-soup-contains('m3u8|')"
+    DATE = "div > span:-soup-contains('Release date:') + time"
+    DVD_CODE = "div > span:-soup-contains('Code:') + span"
 
 
 class MissAVCrawler(Crawler):
     SUPPORTED_PATHS: ClassVar[SupportedPaths] = {"Video": "/..."}
-    PRIMARY_URL: ClassVar[AbsoluteHttpURL] = PRIMARY_URL
+    PRIMARY_URL: ClassVar[AbsoluteHttpURL] = _PRIMARY_URL
     DOMAIN: ClassVar[str] = "missav"
     FOLDER_DOMAIN: ClassVar[str] = "MissAV"
 
     async def fetch(self, scrape_item: ScrapeItem) -> None:
+        if "search" in scrape_item.url.parts:
+            return await self.video(scrape_item)
         return await self.video(scrape_item)
 
     @error_handling_wrapper
     async def video(self, scrape_item: ScrapeItem) -> None:
-        canonical_url = PRIMARY_URL / "en" / scrape_item.url.name
+        canonical_url = _PRIMARY_URL / "en" / scrape_item.url.name
         scrape_item.url = canonical_url
         if await self.check_complete_from_referer(canonical_url):
             return
@@ -38,32 +44,32 @@ class MissAVCrawler(Crawler):
         soup = await self.request_soup(scrape_item.url, impersonate=True)
 
         title, date_str = open_graph.title(soup), open_graph.get("video_release_date", soup)
-        if dvd_code_tag := soup.select_one(DVD_CODE_SELECTOR):
-            title = fix_title(title, dvd_code_tag)
+        if dvd_code_tag := soup.select_one(Selector.DVD_CODE):
+            title = _fix_title(title, dvd_code_tag)
 
         if date_str:
             scrape_item.possible_datetime = self.parse_iso_date(date_str)
-        elif date_tag := soup.select_one(DATE_SELECTOR):
+        elif date_tag := soup.select_one(Selector.DATE):
             scrape_item.possible_datetime = self.parse_date(css.get_attr(date_tag, "datetime"))
         else:
             _ = self.parse_date("")  # Trigger warning
 
-        uuid = get_uuid(soup)
-        m3u8_playlist_url = M3U8_SERVER / uuid / "playlist.m3u8"
+        uuid = _get_uuid(soup)
+        m3u8_playlist_url = _M3U8_SERVER / uuid / "playlist.m3u8"
         m3u8, info = await self.get_m3u8_from_playlist_url(m3u8_playlist_url)
         ext = ".mp4"
         filename = self.create_custom_filename(title, ext, resolution=info.resolution)
         await self.handle_file(m3u8_playlist_url, scrape_item, filename, ext, m3u8=m3u8)
 
 
-def get_uuid(soup: BeautifulSoup) -> str:
-    js_text = css.select_one_get_text(soup, "script:-soup-contains('m3u8|')")
+def _get_uuid(soup: BeautifulSoup) -> str:
+    js_text = css.select_one_get_text(soup, Selector.UUID)
     uuid_joined_parts = js_text.split("m3u8|", 1)[-1].split("|com|surrit", 1)[0]
     uuid_parts = reversed(uuid_joined_parts.split("|"))
     return "-".join(uuid_parts)
 
 
-def fix_title(title: str, dvd_code_tag: Tag) -> str:
+def _fix_title(title: str, dvd_code_tag: Tag) -> str:
     dvd_code = css.get_text(dvd_code_tag).upper()
     uncensored = "UNCENSORED" in dvd_code
     leak = "LEAK" in dvd_code
