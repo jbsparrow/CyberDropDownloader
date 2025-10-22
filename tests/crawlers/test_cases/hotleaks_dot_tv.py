75f8add1b42121269f253996c942c1e8980a099f
Barbarella <barbarella6666666@protonmail.com>
2025-10-22 16:19:25 -0300

    ci: adding test case

---
+++ tests/crawlers/test_cases/hotleaks_dot_tv.py
@@ -0,0 +1,15 @@
+DOMAIN = "hotleaks.tv"
+TEST_CASES = [
+    (
+        "https://hotleaks.tv/sexythangyang/video/10714544",
+        [
+            {
+                "url": "https://hotleaks.tv/sexythangyang/video/10714544",
+                "filename": "10714544.mp4",
+                "referer": "https://hotleaks.tv/sexythangyang/video/10714544",
+                "album_id": None,
+                "datetime": None,
+            }
+        ],
+    ),
+]
