f3a17fbf3be443205229d4c3011ad1c3485bfce7
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2025-10-24 11:11:03 -0500

    tests: update tests

--- tests/crawlers/test_cases/gofile.py
+++ tests/crawlers/test_cases/gofile.py
@@ -23,15 +23,7 @@ TEST_CASES = [
                 "referer": "https://gofile.io/d/0272Cc#386c2d54-548a-4fd1-abde-6f7c91b25a5d",
                 "album_id": "0272Cc",
                 "datetime": 1759603311,
-            },
-            {
-                "url": "ANY",
-                "filename": "Boeboe - Drift (Bass Boosted).mp4",
-                "download_folder": r"re:CDL_test \(GoFile\)/folder_B",
-                "referer": "https://gofile.io/d/OP2VyC#229e787c-22e2-4f77-90a5-eb9802788aad",
-                "album_id": "OP2VyC",
-                "datetime": 1759603323,
-            },
+            }
         ],
     ),
     ("https://gofile.io/d/tZeyhP?password=wrong_password", []),
@@ -44,6 +36,19 @@ TEST_CASES = [
                 "download_folder": r"re:CDL_test_password \(GoFile\)/folder_A",
                 "referer": "https://gofile.io/d/T3gI1u#26617578-4770-41fb-9352-cad172b10b35",
                 "album_id": "T3gI1u",
+            },
+        ],
+    ),
+    (
+        "https://store6.gofile.io/download/69b4c1e4-a80a-45d5-ad76-061e48105bd0/MORE_IN90.mp4",
+        [
+            {
+                "url": "https://store3.gofile.io/download/web/69b4c1e4-a80a-45d5-ad76-061e48105bd0/MORE_IN90.mp4",
+                "filename": "MORE_IN90.mp4",
+                "download_folder": r"re:01 \(GoFile\)",
+                "referer": "https://gofile.io/d/9d83784e-2be6-472c-909f-0377704cfde3#69b4c1e4-a80a-45d5-ad76-061e48105bd0",
+                "album_id": "9d83784e-2be6-472c-909f-0377704cfde3",
+                "datetime": 1701541586,
             },
         ],
     ),
