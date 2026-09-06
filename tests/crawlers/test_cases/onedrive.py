3834aa06dc84bbdd9b49b378b5859473a0bfff68
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2026-09-06 04:22:43 -0500

    test: add test case

---
+++ tests/crawlers/test_cases/onedrive.py
@@ -0,0 +1,20 @@
+DOMAIN = "onedrive"
+TEST_CASES = [
+    {
+        "url": "https://1drv.ms/v/s!AtOM4nXMml6LhVtwL9LELS4JOEN7?e=294Cws",
+        "results": [
+            {
+                "url": r"re:my\.microsoftpersonalcontent\.com/_api/v2.0/shares/(.*)/driveitem",
+                "filename": "LG New York HDR UHD 4K Demo.ts",
+                "debrid_url": "re:https://my.microsoftpersonalcontent.com/personal/8b5e9acc75e28cd3/_layouts/15/download.aspx?UniqueId=75e28cd3-9acc-205e-808b-db0200000000",
+                "original_filename": "LG New York HDR UHD 4K Demo.ts",
+                "referer": "https://1drv.ms/v/s!AtOM4nXMml6LhVtwL9LELS4JOEN7?e=294Cws",
+                "album_id": None,
+                "uploaded_at": 1678604476,
+                "download_folder": "re:Loose Files (OneDrive)",
+                "thumbnail": None,
+            }
+        ],
+        "count": 1,
+    }
+]
