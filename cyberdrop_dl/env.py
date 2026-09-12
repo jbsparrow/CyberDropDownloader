c04179f507c062872f7e8069727c1d4d670616f4
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2026-09-12 17:15:47 -0500

    fix: ffmpeg not running by default
    fixes: https://github.com/Cyberdrop-DL/cyberdrop-dl/issues/2135

--- cyberdrop_dl/env.py
+++ cyberdrop_dl/env.py
@@ -44,7 +44,7 @@ ENABLE_DEBUG_CRAWLERS = (
 
 APPDATA_FOLDER = _env("APPDATA_FOLDER")
 WRITE_JSON_UI = int(_env("WRITE_JSON_UI") or 0) or None
-FFMPEG_FIX_HLS = _cast_bool(_env("FFMPEG_FIX_HLS"))
+FFMPEG_FIX_HLS = _cast_bool(_val if (_val := _env("FFMPEG_FIX_HLS")) is not None else True)
 EDITOR = os.getenv("EDITOR")
 CI = _cast_bool(os.getenv("CI"))
 TERMUX = {
