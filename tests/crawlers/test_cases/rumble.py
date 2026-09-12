2391ff2d8e3c386c433e324e6f53878d3acf1be2
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2026-09-12 15:53:00 -0500

    test: update tests

--- tests/crawlers/test_cases/rumble.py
+++ tests/crawlers/test_cases/rumble.py
@@ -5,7 +5,7 @@ TEST_CASES = [
         "description": "short",
         "results": [
             {
-                "url": "https://hugh.cdn.rumble.cloud/video/fww1/1b/s8/2/c/v/9/X/cv9Xz.aaa.mp4",
+                "url": "re:/video/fww1/1b/s8/2/c/v/9/X/cv9Xz.aaa.mp4",
                 "filename": "Why Bad Bunny Was a DISASTER for NFL Fans [v75ijpe][1920p].mp4",
                 "debrid_url": None,
                 "original_filename": "Why Bad Bunny Was a DISASTER for NFL Fans.mp4",
@@ -13,7 +13,7 @@ TEST_CASES = [
                 "album_id": None,
                 "uploaded_at": 1770642606,
                 "download_folder": "re:Loose Files (Rumble)",
-                "thumbnail": "https://hugh.cdn.rumble.cloud/video/fww1/1b/s8/6/c/v/9/X/cv9Xz.adyb.jpg",
+                "thumbnail": "re:/video/fww1/1b/s8/6/c/v/9/X/cv9Xz.adyb.jpg",
             }
         ],
         "count": 1,
@@ -22,7 +22,7 @@ TEST_CASES = [
         "url": "https://rumble.com/vwu8gp-residente-bzrp-music-sessions-49.html?e9s=src_v1_s",
         "results": [
             {
-                "url": "https://1a-1791.com/video/s8/2/z/z/A/s/zzAsd.caa.mp4",
+                "url": "re:/video/s8/2/z/z/A/s/zzAsd.caa.mp4",
                 "filename": "RESIDENTE -- BZRP Music Sessions #49 [vu82er][360p].mp4",
                 "original_filename": "RESIDENTE -- BZRP Music Sessions #49.mp4",
                 "referer": "https://rumble.com/vwu8gp-residente-bzrp-music-sessions-49.html",
@@ -35,19 +35,19 @@ TEST_CASES = [
         "url": "https://rumble.com/c/RumbleEvents",
         "results": [
             {
-                "url": "https://1a-1791.com/video/vo/47ww/m03gQsmi4GE/Px13ulRwHFM.mp4",
+                "url": "re:/video/vo/47ww/m03gQsmi4GE/Px13ulRwHFM.mp4",
                 "filename": "CLOSING OUT Dave Rubin & Benny Johnson LIVE - Rumble Exclusive Live at TPU [v1at1kt][1080p].mp4",
                 "referer": "https://rumble.com/v1df7i1-rumble-exclusive-live-at-tpusa.html",
                 "album_id": None,
                 "download_folder": "re:RumbleEvents (Rumble)",
             },
             {
-                "url": "https://1a-1791.com/video/vo/y7pe/dPdI3HbQ11Q/kkxZS05zoL0.mp4",
+                "url": "re:/video/vo/y7pe/dPdI3HbQ11Q/kkxZS05zoL0.mp4",
                 "filename": "Turning Point USA 2022 Student Action Summit DAY 1  - Rumble Exclusive Liv [v1amumr][1080p].mp4",
                 "referer": "https://rumble.com/v1d90jz-rumble-exclusive-live-at-tpusa.html",
             },
             {
-                "url": "https://1a-1791.com/video/vo/5j7n/fsuRaNtWx5A/K9ZKZmzRXAg.mp4",
+                "url": "re:/video/vo/5j7n/fsuRaNtWx5A/K9ZKZmzRXAg.mp4",
                 "filename": "President Donald J. Trump LIVE at Turning Point - Rumble Exclusive Live at [v1apryd][1080p].mp4",
                 "referer": "https://rumble.com/v1dbxvl-rumble-exclusive-live-at-tpusa.html",
             },
