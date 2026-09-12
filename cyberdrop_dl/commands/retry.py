543733c79386c77165f40d243fadae6c76179ba8
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2026-09-12 11:42:01 -0500

    refactor: add prompt

--- cyberdrop_dl/commands/retry.py
+++ cyberdrop_dl/commands/retry.py
@@ -7,7 +7,6 @@ from cyberdrop_dl.commands import CLIarguments
 from cyberdrop_dl.commands.scrape import prepare_manager, scrape
 from cyberdrop_dl.config import Config
 from cyberdrop_dl.constants import USE_RETRY_PATH
-from cyberdrop_dl.exceptions import CDLConfigRuntimeErrorsGroup
 from cyberdrop_dl.models import ConfigModel
 from cyberdrop_dl.scrape_source import RetryScrapeSource, RetrySource
 
@@ -37,8 +36,6 @@ def create_retry_src(retry: RetrySource, args: RetryArgs | None = None) -> Retry
     args = args or RetryArgs()
     if args.force_original_path:
         USE_RETRY_PATH.set(True)
-        error = RuntimeError("Support for '--force-original-path' has been temporarily removed")
-        raise CDLConfigRuntimeErrorsGroup("Unsupported option", (error,))
 
     return RetryScrapeSource(retry, after=args.from_, before=args.to or _tomorrow())
 
