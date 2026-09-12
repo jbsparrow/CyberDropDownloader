1ba9b34522ddf4b39fa7d7661e5b9bc14528672d
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2026-09-12 02:09:25 -0500

    refactor: rewrite URLs (cyberdrop)

--- cyberdrop_dl/crawlers/cyberdrop.py
+++ cyberdrop_dl/crawlers/cyberdrop.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, ClassVar
+from typing import TYPE_CHECKING, ClassVar, override
 
 from cyberdrop_dl import aio
 from cyberdrop_dl.clients.http import HTTPConfig
@@ -43,15 +43,23 @@ class CyberdropCrawler(Crawler):
     async def fetch(self, scrape_item: ScrapeItem) -> None:
         match scrape_item.url.parts[1:]:
             case ["a", album_id, *_]:
-                return await self.album(scrape_item, album_id)
-            case ["api", "file" | "proxy", "d" | "auth" | "thumb", file_id, *_]:
-                return await self.file(scrape_item, file_id)
-            case ["f" | "e", file_id]:
-                return await self.file(scrape_item, file_id)
+                await self.album(scrape_item, album_id)
+            case ["f", file_id]:
+                await self.file(scrape_item, file_id)
             case [_]:
-                return await self.follow_redirect(scrape_item)
+                await self.follow_redirect(scrape_item)
             case _:
                 raise ValueError
+
+    @classmethod
+    @override
+    def transform_url(cls, url: AbsoluteHttpURL) -> AbsoluteHttpURL:
+        url = super().transform_url(url)
+        match url.parts[1:]:
+            case ["e", file_id] | ["api", "file" | "proxy", "d" | "auth" | "thumb", file_id, *_]:
+                return url.origin() / "f" / file_id
+            case _:
+                return url
 
     @error_handling_wrapper
     async def album(self, scrape_item: ScrapeItem, album_id: str) -> None:
